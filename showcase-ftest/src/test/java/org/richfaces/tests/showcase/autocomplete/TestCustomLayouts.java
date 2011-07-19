943cdb40e533c29f3aaf717302f951da2a768e0e
Juraj Huska <jhuska@redhat.com>
2011-07-19 12:53:48 +0200

    Tests for autocomplete samples custom layouts and modes addedo

---
+++ showcase-ftest/src/test/java/org/richfaces/tests/showcase/autocomplete/TestCustomLayouts.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
+package org.richfaces.tests.showcase.autocomplete;
+
+import static org.jboss.arquillian.ajocado.Ajocado.guardXhr;
+import static org.jboss.arquillian.ajocado.locator.LocatorFactory.jq;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.awt.event.KeyEvent;
+
+import org.jboss.arquillian.ajocado.dom.Event;
+import org.jboss.arquillian.ajocado.locator.JQueryLocator;
+import org.richfaces.tests.showcase.AbstractShowcaseTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:jhuska@redhat.com">Juraj Huska</a>
+ * @version $Revision$
+ */
+public class TestCustomLayouts extends AbstractShowcaseTest {
+
+	/* *********************************************************************************************************************
+	 * Locators
+	 **********************************************************************************************************************/
+	
+	private JQueryLocator inputWithTableLayout = jq("input[type=text]:eq(0)");
+	private JQueryLocator inputWithDivLayout = jq("input[type=text]:eq(1)");
+	private JQueryLocator firstRowOfSelectOfFirstInput = jq("tr.rf-au-itm:eq(0):visible");
+	private JQueryLocator firstRowOfSelectOfSecondInput = jq("div.rf-au-itm:eq(0):visible");
+
+	/* ********************************************************************************************************************
+	 * Constants
+	 **********************************************************************************************************************/
+	
+	private final String EXP_SUGG_AFTER_V_FST_INPUT = "Vermont Montpelier";
+	private final String EXP_SUGG_AFTER_ALA_FST_INPUT = "Alabama Montgomery";
+	private final String EXP_SUGG_AFTER_V_SC_INPUT = "Vermont - (Montpelier)";
+	private final String EXP_SUGG_AFTER_ALA_SC_INPUT = "Alabama - (Montgomery)";
+
+	/* **********************************************************************************************************************
+	 * Tests
+	 ***********************************************************************************************************************/
+	
+	@Test
+	public void testAutocompletionOfInputWithTableLayout() {
+
+		typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(inputWithTableLayout, firstRowOfSelectOfFirstInput, "v", EXP_SUGG_AFTER_V_FST_INPUT,
+				EXP_SUGG_AFTER_V_FST_INPUT.split(" ")[0]);
+
+		typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(inputWithTableLayout, firstRowOfSelectOfFirstInput, "ala", EXP_SUGG_AFTER_ALA_FST_INPUT,
+				EXP_SUGG_AFTER_ALA_FST_INPUT.split(" ")[0]);
+	}
+	
+	@Test
+	public void testAutocompletionOfInputWithDivlayput() {
+		
+		typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(inputWithDivLayout, firstRowOfSelectOfSecondInput, "v", 
+				EXP_SUGG_AFTER_V_SC_INPUT, EXP_SUGG_AFTER_V_SC_INPUT.split(" ")[0]);
+		
+		typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue(inputWithDivLayout, firstRowOfSelectOfSecondInput, "ala", 
+				EXP_SUGG_AFTER_ALA_SC_INPUT, EXP_SUGG_AFTER_ALA_SC_INPUT.split(" ")[0]);
+
+	}
+	
+	@Test
+	public void testFirstInputPopupIsCreatedFromTable() {
+		
+		selenium.type(inputWithTableLayout, "a");
+		guardXhr(selenium).fireEvent(inputWithTableLayout, Event.KEYPRESS);
+
+		assertTrue(selenium.isElementPresent(firstRowOfSelectOfFirstInput), "The poppup with suggestions should be visible!");
+
+		JQueryLocator tdState = jq(firstRowOfSelectOfFirstInput.getRawLocator() + " > td:eq(1)");
+		
+		assertTrue( selenium.isElementPresent(tdState), "The popup should be built from table!");
+		
+		eraseInput(inputWithTableLayout);
+	}
+	
+
+	/* ******************************************************************************************************************
+	 * Help methods
+	 ********************************************************************************************************************/
+	
+	private void typeSomethingToInputCheckThePoppupPressEnterCheckTheInputValue( JQueryLocator input, JQueryLocator firstRow, String whatTotype, 
+			String expectedValueInPopup, String expectedValueInInputAfterEnter) {
+		
+		selenium.type(input, whatTotype);
+		guardXhr(selenium).fireEvent(input, Event.KEYPRESS);
+
+		assertTrue(selenium.isElementPresent(firstRow));
+
+		String firstRowContent = selenium.getText(firstRow).trim();
+
+		assertTrue(expectedValueInPopup.equals(firstRowContent),
+				"The first row of popup should suggest "
+						+ expectedValueInPopup + " " + "when " + whatTotype
+						+ " is typed in input");
+
+		guardXhr(selenium).keyPressNative(KeyEvent.VK_ENTER);
+
+		String valueInInput = selenium.getValue(input);
+
+		assertEquals(valueInInput, expectedValueInInputAfterEnter,
+				"The value in input should be different!");
+
+		eraseInput(input);
+	}
+
+}
---
+++ showcase-ftest/src/test/java/org/richfaces/tests/showcase/autocomplete/TestModes.java
@@ -0,0 +1,119 @@
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
+package org.richfaces.tests.showcase.autocomplete;
+
+import static org.jboss.arquillian.ajocado.Ajocado.guardXhr;
+import static org.jboss.arquillian.ajocado.locator.LocatorFactory.jq;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.awt.event.KeyEvent;
+
+import org.jboss.arquillian.ajocado.locator.JQueryLocator;
+import org.richfaces.tests.showcase.AbstractShowcaseTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:jhuska@redhat.com">Juraj Huska</a>
+ * @version $Revision$
+ */
+public class TestModes  extends AbstractShowcaseTest {
+	
+	/* **************************************************************************************
+	 * Locators
+	 ****************************************************************************************/
+	
+	private JQueryLocator minCharInput = jq("input[type=text]:eq(0)");
+	private JQueryLocator multipleSelectionInput = jq("input[type=text]:eq(1)");
+	private JQueryLocator selectFirstFalseInput = jq("input[type=text]:eq(2)");
+	private JQueryLocator selection = jq("div.rf-au-itm");
+	
+	/* **************************************************************************************
+	 * Tests
+	 ****************************************************************************************/
+	
+	@Test
+	public void testMinCharInput() {
+		
+		char key = 'a';
+		selenium.keyPress(minCharInput, 'a');
+		
+		assertFalse( selenium.isElementPresent(selection), "The selection should not be visible, since there is only one char!");
+		
+		key = 'r';
+		selenium.focus(minCharInput);
+		guardXhr(selenium).keyPress(minCharInput, key);
+		
+		assertTrue( selenium.isVisible(selection), "The selection should be visible, since there are two chars!");
+		
+		String actualArizona = selenium.getText( jq(selection.getRawLocator() + ":eq(0)") );
+		assertEquals( actualArizona, "Arizona", "The provided option should be Arizona");
+		
+		String actualArkansas = selenium.getText( jq(selection.getRawLocator() + ":eq(1)") );
+		assertEquals( actualArkansas, "Arkansas", "The provided option should be Arkansas");
+		
+	}
+	
+	@Test
+	public void testMultipleSelectionInput() {
+		
+		char key = 'a';
+		selenium.focus(multipleSelectionInput);
+		guardXhr(selenium).keyPress( multipleSelectionInput, key);
+		
+		assertTrue( selenium.isVisible(selection), "The selection should be visible, since there is correct starting char!");
+		
+		selenium.keyPressNative( KeyEvent.VK_ENTER );
+		
+		key = ' ';
+		selenium.keyPress( multipleSelectionInput, key);
+		
+		key = 'w';
+		
+		selenium.focus(multipleSelectionInput);
+		guardXhr(selenium).keyPress( multipleSelectionInput, key);
+		
+		assertTrue( selenium.isVisible(selection), "The selection should be visible, since there is correct starting char!");
+		
+		selenium.keyPressNative( KeyEvent.VK_ENTER );
+		
+		String actualContentOfInput = selenium.getValue(multipleSelectionInput);
+		assertEquals(actualContentOfInput, "Alabama Washington", "The input should contain something else!");
+	}
+	
+	@Test
+	public void testSelectFirstFalseInput() {
+		
+		char key = 'a';
+		selenium.focus( selectFirstFalseInput);
+		guardXhr(selenium).keyPress( selectFirstFalseInput, key);
+		
+		assertTrue( selenium.isVisible(selection), "The selection should be visible, since there is correct starting char!");
+		
+		selenium.keyPressNative( KeyEvent.VK_ENTER );
+		
+		String actualValueOfInput = selenium.getValue(selectFirstFalseInput);
+		assertEquals(actualValueOfInput, "a", "The content should be the letter which was types and not something else, since selectFirst is false!");
+	}
+
+}
