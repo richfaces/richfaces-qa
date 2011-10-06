6ee0d55ca7d78e04d0a2c45ac49fefd93ed59201
Juraj Huska <jhuska@redhat.com>
2011-10-06 18:10:38 +0200

    tests altered to accept white spaces as correct chars

--- showcase-ftest/src/test/java/org/richfaces/tests/showcase/togglePanel/TestWizard.java
+++ showcase-ftest/src/test/java/org/richfaces/tests/showcase/togglePanel/TestWizard.java
@@ -31,28 +31,34 @@ import org.testng.annotations.Test;
 
 import com.thoughtworks.selenium.SeleniumException;
 
-
-
 /**
  * @author <a href="mailto:jhuska@redhat.com">Juraj Huska</a>
  * @version $Revision$
  */
 public class TestWizard extends AbstractAjocadoTest {
-	
+
 	/* ***********************************************************************************
-	 * Constants 
-	 *************************************************************************************/
-	
+	 * Constants
+	 * *****************************************************************
+	 * ******************
+	 */
+
 	protected final String ERROR_MSG_VALUE_REQUIRED = "Validation Error: Value is required.";
-	protected final String ERROR_MSG_FIRST_NAME = "First Name: " + ERROR_MSG_VALUE_REQUIRED;
-	protected final String ERROR_MSG_LAST_NAME = "Last Name: " + ERROR_MSG_VALUE_REQUIRED;
-	protected final String ERROR_MSG_COMPANY = "Company: " + ERROR_MSG_VALUE_REQUIRED;
-	protected final String ERROR_MSG_NOTES = "Notes: " + ERROR_MSG_VALUE_REQUIRED;
-	
+	protected final String ERROR_MSG_FIRST_NAME = "First Name: "
+			+ ERROR_MSG_VALUE_REQUIRED;
+	protected final String ERROR_MSG_LAST_NAME = "Last Name: "
+			+ ERROR_MSG_VALUE_REQUIRED;
+	protected final String ERROR_MSG_COMPANY = "Company: "
+			+ ERROR_MSG_VALUE_REQUIRED;
+	protected final String ERROR_MSG_NOTES = "Notes: "
+			+ ERROR_MSG_VALUE_REQUIRED;
+
 	/* ***********************************************************************************
 	 * Locators
-	 *************************************************************************************/
-	
+	 * ******************************************************************
+	 * *****************
+	 */
+
 	JQueryLocator firstNameInput = jq("input[type=text]:eq(0)");
 	JQueryLocator lastNameInput = jq("input[type=text]:eq(1)");
 	JQueryLocator companyInput = jq("input[type=text]:eq(2)");
@@ -65,51 +71,53 @@ public class TestWizard extends AbstractAjocadoTest {
 	JQueryLocator errorMessageCompany = jq("span[class=rf-msg-det]:contains('Company: Validation Error'):visible");
 	JQueryLocator errorMessageNotes = jq("span[class=rf-msg-det]:contains('Notes: Validation Error: Value is required.'):visible");
 	JQueryLocator errorMessage = jq("span[class=rf-msg-det]:contains('Validation Error'):visible");
-		
-	/* *************************************************************************** 
+
+	/* ***************************************************************************
 	 * Tests
-	 *****************************************************************************/
-	
+	 * *********************************************************************
+	 * ******
+	 */
+
 	@Test
 	public void testStep1ValidationEmptyInputs() {
-		
+
 		guardXhr(selenium).click(nextButton);
-		
+
 		checkForAllErrorMessagesFromFirstStep(true);
-		
+
 		fillAnyInput(firstNameInput, " ");
 		fillAnyInput(lastNameInput, " ");
 		fillAnyInput(companyInput, " ");
-		
+
 		guardXhr(selenium).click(nextButton);
-		
+
 		try {
-			
-			checkForAllErrorMessagesFromFirstStep(true);
-		
-			isThereErrorMessage(errorMessage, ERROR_MSG_VALUE_REQUIRED, true);
-		
-		} catch ( SeleniumException ex ) {
-			
-			fail("The string containing just white spaces should be considered as empty string during validation!");
+
+			checkForAllErrorMessagesFromFirstStep(false);
+
+			isThereErrorMessage(errorMessage, ERROR_MSG_VALUE_REQUIRED, false);
+
+		} catch (SeleniumException ex) {
+
+			fail("Has to change the test, since last time JSF considered white spaces as characters when there was restriction required!");
 		}
 	}
-	
+
 	@Test
 	public void testStep1ValidationCorrectValues() {
-		
+
 		fillInputWithStringOfLength(firstNameInput, 5);
 		fillInputWithStringOfLength(lastNameInput, 5);
 		fillInputWithStringOfLength(companyInput, 5);
-		
+
 		guardXhr(selenium).click(nextButton);
-		
+
 		checkForAllErrorMessagesFromFirstStep(false);
 	}
-	
+
 	@Test
 	public void testStep2ValidationEmptyInput() {
-		
+
 		fillInputWithStringOfLength(firstNameInput, 5);
 		fillInputWithStringOfLength(lastNameInput, 5);
 		fillInputWithStringOfLength(companyInput, 5);
@@ -126,36 +134,42 @@ public class TestWizard extends AbstractAjocadoTest {
 		
 		try {
 			
-			isThereErrorMessage(errorMessageNotes, ERROR_MSG_NOTES, true);
+			isThereErrorMessage(errorMessageNotes, ERROR_MSG_NOTES, false);
 		
 		} catch ( SeleniumException ex ) {
 			
-			fail("The string containing just white spaces should be considered as empty string during validation!");
+			fail("Has to change the test, since last time JSF considered white spaces as characters when there was restriction required!");
 		}
-		
+
 	}
-	
-	//@Test
+
+	// @Test
 	public void testStep2ValidationCorrectValue() {
-		//TODO
+		// TODO
 	}
-	
-	//@Test
+
+	// @Test
 	public void testStep3CheckSummaryOfPreviousSteps() {
-		//TODO
+		// TODO
 	}
-	
+
 	/* ***************************************************************************************
 	 * Help methods
-	 *****************************************************************************************/
-	
-	private void checkForAllErrorMessagesFromFirstStep( boolean shouldErrorMessagePresented ) {
-		
-		isThereErrorMessage(errorMessageFirstName, ERROR_MSG_FIRST_NAME, shouldErrorMessagePresented);
-		
-		isThereErrorMessage(errorMessageLastName, ERROR_MSG_LAST_NAME, shouldErrorMessagePresented);
-		
-		isThereErrorMessage(errorMessageCompany, ERROR_MSG_COMPANY, shouldErrorMessagePresented);
+	 * **************************************************************
+	 * *************************
+	 */
+
+	private void checkForAllErrorMessagesFromFirstStep(
+			boolean shouldErrorMessagePresented) {
+
+		isThereErrorMessage(errorMessageFirstName, ERROR_MSG_FIRST_NAME,
+				shouldErrorMessagePresented);
+
+		isThereErrorMessage(errorMessageLastName, ERROR_MSG_LAST_NAME,
+				shouldErrorMessagePresented);
+
+		isThereErrorMessage(errorMessageCompany, ERROR_MSG_COMPANY,
+				shouldErrorMessagePresented);
 	}
-	
+
 }
