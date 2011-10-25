e7ffa9c6ab18600d517de9639e251e73405012cc
papousek <jan.papousek@gmail.com>
2011-10-25 13:39:31 +0200

    fixed scripts for starting a new android device

--- showcase-webdriver-ftest/command-line-tools/config.sh
+++ showcase-webdriver-ftest/command-line-tools/config.sh
@@ -3,14 +3,14 @@ SCRIPT_DIR=`dirname $BASH_SOURCE`;
 
 # Device properties
 DEVICE_MEMORY=500M;
-DEVICE_NAME_DEFAULT=metamer;
+DEVICE_NAME_DEFAULT=showcase-ftest-device;
 DEVICE_SERIAL=emulator-5554;
-DEVICE_VERSION_DEFAULT=10;
+DEVICE_VERSION_DEFAULT=8;
 
 # WebDriver properties
 WEBDRIVER_FILE_PREFIX=android-server-;
 WEBDRIVER_FILE_SUFFIX=.apk;
-WEBDRIVER_FILE_VERSION_DEFAULT=2.9.0;
+WEBDRIVER_FILE_VERSION_DEFAULT=2.6.0;
 WEBDRIVER_DEVICE_PORT_DEFAULT=8080;
 WEBDRIVER_HOST_PORT_DEFAULT=4444;
 WEBDRIVER_DOWNLOAD_URL=http://selenium.googlecode.com/files;
@@ -22,16 +22,12 @@ ANDROID_DOWNLOAD_URL=http://dl.google.com/android;
 # Workspace
 WORKSPACE_DEFAULT=$SCRIPT_DIR/android-workspace;
 
-# Argument loading
+# Functions
 
-ARG_ANDROID_SDK=;
-ARG_DEVICE_NAME=$DEVICE_NAME_DEFAULT;
-ARG_DEVICE_PORT=$WEBDRIVER_DEVICE_PORT_DEFAULT;
-ARG_DEVICE_VERSION=$DEVICE_VERSION_DEFAULT;
-ARG_INSTALL_WEBDRIVER=1;
-ARG_HOST_PORT=$WEBDRIVER_HOST_PORT_DEFAULT;
-ARG_WEBDRIVER_FILE_VERSION=$WEBDRIVER_FILE_VERSION_DEFAULT;
-ARG_WORKSPACE=$WORKSPACE_DEFAULT;
+function msg()
+{
+	echo "### $1";
+}
 
 usage()
 {
@@ -43,41 +39,66 @@ This script runs android emulator
 OPTIONS:
 	-d 	device port (defaultly set to 8080)
 	-h	host port (defaultly set to 4444)
+	-i	unable installing android server to the device
+	-l 	skip starting a new device
 	-n	device name
 	-p	workspace
 	-s	android SDK directory (defaultly set to 2.9.0)
-	-u	unable installing android server to the device
 	-v 	device verison (defaultly set to 11)
 	-w	android selenium server version
 EOF
 }
 
-while getopts "d:h:s:v:w:" OPTION
+# Argument loading
+
+ARG_ANDROID_SDK=;
+ARG_DEVICE_NAME=$DEVICE_NAME_DEFAULT;
+ARG_DEVICE_PORT=$WEBDRIVER_DEVICE_PORT_DEFAULT;
+ARG_DEVICE_VERSION=$DEVICE_VERSION_DEFAULT;
+ARG_INSTALL_WEBDRIVER=1;
+ARG_HOST_PORT=$WEBDRIVER_HOST_PORT_DEFAULT;
+ARG_START_DEVICE=1;
+ARG_WEBDRIVER_FILE_VERSION=$WEBDRIVER_FILE_VERSION_DEFAULT;
+ARG_WORKSPACE=$WORKSPACE_DEFAULT;
+
+while getopts "d:h:iln:p:s:v:w:" OPTION
 do
 	case $OPTION in
 		d)
 			ARG_DEVICE_PORT=$OPTARG;
+			msg "device port has been set to [$OPTARG]";
 			;;
 		h)
 			ARG_HOST_PORT=$OPTARG;
+			msg "host port has been set to [$OPTARG]";
+			;;
+		i)
+			ARG_INSTALL_WEBDRIVER=0;
+			msg "skipping installation of android selenium server";
+			;;
+		l)
+			ARG_START_DEVICE=0;
+			msg "skipping starting device";
 			;;
 		n)
 			ARG_DEVICE_NAME=$OPTARG;
+			msg "device name has been set to [$OPTARG]";
 			;;
 		p)
 			ARG_WORKSPACE=$OPTARG;
+			msg "workspace has been set to [$OPTARG]";
 			;;
 		s)
 			ARG_ANDROID_SDK=$OPTARG;
-			;;	
-		u)
-			ARG_INSTALL_WEBDRIVER=0;
-			;;	
+			msg "android sdk directory has been set to [$OPTARG]";
+			;;		
 		v)
 			ARG_DEVICE_VERSION=$OPTARG;
+			msg "device version has been set to [$OPTARG]";
 			;;
 		w)
-			ARG_WEBDRIVER_VERSION=$OPTARG;
+			ARG_WEBDRIVER_FILE_VERSION=$OPTARG;
+			msg "android selenium server version has been set to [$OPTARG]";
 			;;
 		?)
 			usage;
@@ -88,11 +109,6 @@ done
 
 # Argument validation
 
-function msg()
-{
-	echo "### $1";
-}
-
 if [ ! -d "$ARG_ANDROID_SDK" ]; then
 	msg "set android sdk [$ARG_ANDROID_SDK] is not a directory";
 	usage;
@@ -101,5 +117,5 @@ fi
 
 if [ ! -d "$ARG_WORKSPACE" ]; then
 	msg "creating workspace [$ARG_WORKSPACE]";
-	mkdir "$ARG_WORKSPACE";
+	mkdir -p "$ARG_WORKSPACE";
 fi
--- showcase-webdriver-ftest/command-line-tools/device.sh
+++ showcase-webdriver-ftest/command-line-tools/device.sh
@@ -2,25 +2,30 @@
 SCRIPT_DIR=`dirname $BASH_SOURCE`;
 
 source "$SCRIPT_DIR/config.sh";
+if [ $ARG_START_DEVICE == 1 ]; then
+	msg "deleting old devices";
+	$ARG_ANDROID_SDK/tools/android delete avd -n $ARG_DEVICE_NAME;
 
-msg "deleting old devices";
-$ARG_ANDROID_SDK/tools/android delete avd -n $ARG_DEVICE_NAME;
+	msg "creating new device";
+	$ARG_ANDROID_SDK/tools/android create avd -n $ARG_DEVICE_NAME -t $ARG_DEVICE_VERSION -c $DEVICE_MEMORY;
 
-msg "creating new device";
-$ARG_ANDROID_SDK/tools/android create avd -n $ARG_DEVICE_NAME -t $ARG_DEVICE_VERSION -c $DEVICE_MEMORY;
+	msg "starting device";
+	($ARG_ANDROID_SDK/tools/emulator -avd $ARG_DEVICE_NAME -no-audio -no-boot-anim &);
+	while [[ "$GREPPED" != $DEVICE_SERIAL*[d]* ]];
+	do
+		GREPPED=`$ARG_ANDROID_SDK/platform-tools/adb devices | grep $DEVICE_SERIAL`;
+	done
+fi
 
-msg "starting device";
-($ARG_ANDROID_SDK/tools/emulator -avd my_android -no-audio -no-boot-anim &);
-while [[ "$GREPPED" != $DEVICE_SERIAL*[d]* ]];
-do
-	GREPPED=`$ARG_ANDROID_SDK/platform-tools/adb devices | grep $DEVICE_SERIAL`;
-done
 
 if [ $ARG_INSTALL_WEBDRIVER == 1 ]; then
 	WEBDRIVER=${WEBDRIVER_FILE_PREFIX}${ARG_WEBDRIVER_FILE_VERSION}${WEBDRIVER_FILE_SUFFIX};
-	msg "installing web driver [$WEBDRIVER]";
-	wget $WEBDRIVER_DOWNLOAD_URL/$WEBDRIVER --output-document $ARG_WORKSPACE/$WEBDRIVER;
-	$ARG_ANDROID_SDK/platform-tools/adb -s $DEVICE_SERIAL -e install -r $WORKSPACE/$WEBDRIVER;
+	if [ ! -e $ARG_WORKSPACE/$WEBDRIVER ]; then
+		msg "downloading webdriver to [$ARG_WORKSPACE/$WEBDRIVER]";
+		wget $WEBDRIVER_DOWNLOAD_URL/$WEBDRIVER --output-document $ARG_WORKSPACE/$WEBDRIVER;
+	fi	
+	msg "installing web driver [$WEBDRIVER]";	
+	$ARG_ANDROID_SDK/platform-tools/adb -s $DEVICE_SERIAL -e install -r $ARG_WORKSPACE/$WEBDRIVER;
 	msg "forwarding tcp: host port $ARG_HOST_PORT -> device port $ARG_DEVICE_PORT";
 	$ARG_ANDROID_SDK/platform-tools/adb -s $DEVICE_SERIAL forward tcp:$ARG_HOST_PORT tcp:$ARG_DEVICE_PORT;
 fi
