1c49d8fc478467bb902e3a663b61719cd059d0f9
papousek <jan.papousek@gmail.com>
2011-11-10 15:55:08 +0100

    RFPL-1676: tests for rich:panelMenu

---
+++ showcase-webdriver-ftest/src/test/java/org/richfaces/tests/showcase/ftest/webdriver/page/richPanelMenu/PanelMenu.java
@@ -0,0 +1,98 @@
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2011, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
+package org.richfaces.tests.showcase.ftest.webdriver.page.richPanelMenu;
+
+import org.jboss.test.selenium.android.ToolKit;
+import org.jboss.test.selenium.android.support.ui.AbstractComponent;
+import org.jboss.test.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+/**
+ * @author <a href="mailto:jpapouse@redhat.com">Jan Papousek</a>
+ */
+public class PanelMenu extends AbstractComponent{
+    
+    public PanelMenu(WebDriver webDriver, ToolKit toolKit, WebElement webElement) {
+        super(webDriver, toolKit, webElement);
+    }
+
+    public int getNumberOfFirstLevelGroups() {
+        return getWebElement().findElements(By.xpath("div[@class='rf-pm-top-gr']")).size();
+    }
+    
+    public boolean isFirstLevelGroupExpanded(int index) {
+        try {
+            getWebElement().findElement(By.xpath("div[@class='rf-pm-top-gr'][" + (index+1) + "]/div[contains(@class,'rf-pm-hdr-exp')]"));
+            return true;
+        }
+        catch(NoSuchElementException ignored) {}
+        catch(StaleElementReferenceException ignored) {}
+        return false;
+    }
+    
+    public void toggleFirstLevelGroup(final int index) {
+        boolean expanded = isFirstLevelGroupExpanded(index);
+        getWebElement().findElement(By.xpath("div[@class='rf-pm-top-gr'][" + (index+1) + "]/div[1]")).click();
+        ExpectedCondition<Boolean> condition;
+        if (expanded) {
+            condition = new ExpectedCondition<Boolean>() {
+                @Override
+                public Boolean apply(WebDriver arg0) {
+                    return !isFirstLevelGroupExpanded(index);
+                }
+            };
+        } else {
+            condition = new ExpectedCondition<Boolean>() {
+                @Override
+                public Boolean apply(WebDriver arg0) {
+                    return isFirstLevelGroupExpanded(index);
+                }
+            };
+        }
+        new WebDriverWait(getWebDriver())
+            .failWith("The group <" + index + "> in panel menu can't be " + (expanded ? "collapsed" : "expanded") + ".")
+            .until(condition);
+    }
+    
+    public void selectSecondLevelItem(int groupIndex, int itemIndex) {
+        final String itemLocator = "div[@class='rf-pm-top-gr'][" + (groupIndex+1) + "]//div[contains(@class, 'rf-pm-itm')][" + (itemIndex+1) + "]";
+        getWebElement().findElement(By.xpath(itemLocator + "//td[@class='rf-pm-itm-lbl']")).click();
+        new WebDriverWait(getWebDriver())
+            .failWith("The option <" + groupIndex + "><" + itemIndex +"> can't be selected.")
+            .until(new ExpectedCondition<Boolean>() {
+                @Override
+                public Boolean apply(WebDriver webDriver) {
+                    try {
+                        return getWebElement().findElement(By.xpath(itemLocator)).getAttribute("class").contains("rf-pm-itm-sel");
+                    } catch(Exception ignored) {
+                        return false;
+                    }
+                }
+            });
+    }
+    
+}
---
+++ showcase-webdriver-ftest/src/test/java/org/richfaces/tests/showcase/ftest/webdriver/page/richPanelMenu/PanelMenuPage.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2011, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
+package org.richfaces.tests.showcase.ftest.webdriver.page.richPanelMenu;
+
+import org.jboss.test.selenium.android.ToolKit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.richfaces.tests.showcase.ftest.webdriver.page.AbstractAndroidPage;
+
+/**
+ * @author <a href="mailto:jpapouse@redhat.com">Jan Papousek</a>
+ */
+public class PanelMenuPage extends AbstractAndroidPage {
+
+    @FindBy(xpath = "//*[@class='example-cnt']//div[@class='rf-pm']")
+    private WebElement panelMenuElement;
+    private PanelMenu panelMenu;
+    @FindBy(xpath = "//*[@class='example-cnt']//span[contains(@id, 'current')]")
+    private WebElement selectionElement;
+    
+    public PanelMenuPage(WebDriver webDriver, ToolKit toolKit) {
+        super(webDriver, toolKit);
+    }
+
+    @Override
+    public String getDemoName() {
+        return "panelMenu";
+    }
+
+    @Override
+    public String getSampleName() {
+        return "panelMenu";
+    }
+
+    public String getCurrentSelection() {
+        return selectionElement.getText();
+    }
+    
+    public PanelMenu getPanelMenu() {
+        if (panelMenu == null) {
+            panelMenu = new PanelMenu(getWebDriver(), getToolKit(), panelMenuElement);
+        }
+        return panelMenu;
+    }
+    
+}
