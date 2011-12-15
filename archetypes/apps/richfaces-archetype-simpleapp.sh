4b7dec176c47dd2469bce5219f511c04697c8112
papousek <jan.papousek@gmail.com>
2011-12-15 11:04:10 +0100

    RFPL-1948: version chnaged to 4.1.1-SNAPSHOT and fixed shell script for generating tested application

--- archetypes/apps/richfaces-archetype-simpleapp.sh
+++ archetypes/apps/richfaces-archetype-simpleapp.sh
@@ -2,4 +2,10 @@
 SCRIPT_DIR=`dirname $BASH_SOURCE`;
 source "$SCRIPT_DIR/version.sh";
 
+if [ -d "$SCRIPT_DIR/richfaces-simpleapp" ]; then
+    rm -rf "$SCRIPT_DIR/richfaces-simpleapp";
+fi
+cd $SCRIPT_DIR;
 mvn archetype:generate -U -DarchetypeGroupId=org.richfaces.archetypes -DarchetypeArtifactId=richfaces-archetype-simpleapp -DarchetypeVersion=${VERSION} -DgroupId=org.richfaces.tests.archetypes -DartifactId=richfaces-simpleapp -Dversion=${VERSION} -Dpackage=org.richfaces.tests.archetypes.simpleapp -DinteractiveMode=false -DarchetypeRepository=https://repository.jboss.org/nexus/content/groups/public/;
+cd richfaces-simpleapp;
+mvn clean package -Prelease;
