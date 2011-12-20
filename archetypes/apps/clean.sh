e002d1d086c1d3d5a6052a9682a3792f1689447d
papousek <jan.papousek@gmail.com>
2011-12-20 13:34:53 +0100

    another try to fix maven in bash scripts on jenkins

--- archetypes/apps/clean.sh
+++ archetypes/apps/clean.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=`readlink -m $SCRIPT_DIR`;
 rm -rf $SCRIPT_DIR/richfaces-simpleapp;
 rm -rf $SCRIPT_DIR/richfaces-gae;
 rm -rf $SCRIPT_DIR/appengine-java-sdk;
-rm -rf $SCRIPT_DIR/*.sh~
+rm -rf $SCRIPT_DIR/*.sh~;
 
