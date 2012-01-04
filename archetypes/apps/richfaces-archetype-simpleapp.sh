518f2ecce9760a40c7d766592580a0c739684d6d
papousek <jan.papousek@gmail.com>
2012-01-04 10:09:53 +0100

    updating dependencies when generating simpleapp from archetype

--- archetypes/apps/richfaces-archetype-simpleapp.sh
+++ archetypes/apps/richfaces-archetype-simpleapp.sh
@@ -9,7 +9,11 @@ if [ -d "$SCRIPT_DIR/richfaces-simpleapp" ]; then
     rm -rf "$SCRIPT_DIR/richfaces-simpleapp";
 fi
 
+echo "--------------------------------------------------------------------------------";
+echo "RichFaces version: ${RICHFACES_VERSION}" 
+echo "--------------------------------------------------------------------------------";
+
 cd $SCRIPT_DIR;
-${MAVEN} ${MAVEN_ARGS} archetype:generate -Dbasedir=${SCRIPT_DIR} -DarchetypeGroupId=org.richfaces.archetypes -DarchetypeArtifactId=richfaces-archetype-simpleapp -DarchetypeVersion=${RICHFACES_VERSION} -DgroupId=org.richfaces.tests.archetypes -DartifactId=richfaces-simpleapp -Dversion=${RICHFACES_VERSION} -Dpackage=org.richfaces.tests.archetypes.simpleapp -DinteractiveMode=false $@;
-${MAVEN} ${MAVEN_ARGS} -f $SCRIPT_DIR/richfaces-simpleapp/pom.xml clean package -Prelease;
+${MAVEN} ${MAVEN_ARGS} -U archetype:generate -Dbasedir=${SCRIPT_DIR} -DarchetypeGroupId=org.richfaces.archetypes -DarchetypeArtifactId=richfaces-archetype-simpleapp -DarchetypeVersion=${RICHFACES_VERSION} -DgroupId=org.richfaces.tests.archetypes -DartifactId=richfaces-simpleapp -Dversion=${RICHFACES_VERSION} -Dpackage=org.richfaces.tests.archetypes.simpleapp -DinteractiveMode=false $@;
+${MAVEN} ${MAVEN_ARGS} -U -f $SCRIPT_DIR/richfaces-simpleapp/pom.xml clean package -Prelease;
 cd $WORKING_DIR;
