4a080a73b04c3e257bb65d4c03b771ee1184a159
papousek <jan.papousek@gmail.com>
2012-01-09 14:22:50 +0100

    removed duplicate annotation

--- showcase-webdriver-ftest/src/test/java/org/richfaces/tests/showcase/ftest/webdriver/ftest/a4jMediaOutput/TestA4jMediaOutputSimple.java
+++ showcase-webdriver-ftest/src/test/java/org/richfaces/tests/showcase/ftest/webdriver/ftest/a4jMediaOutput/TestA4jMediaOutputSimple.java
@@ -38,7 +38,6 @@ import org.testng.annotations.Test;
  */
 public class TestA4jMediaOutputSimple extends AbstractAndroidTest<ImgUsagePage> {
 
-    @Test
     @Test(groups = { "4.2" })
     public void testStates() throws Exception {
         Select leftColor = new Select(getWebDriver(), getToolKit(), getPage().getSelectLeftColor());
