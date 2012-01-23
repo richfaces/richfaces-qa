af848f542a5c2706b5b8568dc065eba3a6d0b524
papousek <jan.papousek@gmail.com>
2012-01-23 10:16:29 +0100

    fixed checkstyle violations in showcase-webdriver-ftest

--- showcase-webdriver-ftest/src/test/java/org/jboss/test/selenium/android/Key.java
+++ showcase-webdriver-ftest/src/test/java/org/jboss/test/selenium/android/Key.java
@@ -23,13 +23,14 @@ package org.jboss.test.selenium.android;
 
 /**
  * {@link http://thecodeartist.blogspot.com/2011/03/simulating-keyevents-on-android-device.html}
- * 
+ *
  * @author <a href="mailto:jpapouse@redhat.com">Jan Papousek</a>
  */
 public enum Key {
 
-    UP_ARROW(19), DOWN_ARROW(20), LEFT_ARROW(21), RIGHT_ARROW(22), A(29), B(30), C(31), D(32), E(33), F(34), G(35), H(
-        36), I(37), J(38), K(39), L(40), ENTER(66), DELETE(67);
+    UP_ARROW(19), DOWN_ARROW(20), LEFT_ARROW(21), RIGHT_ARROW(22),
+    A(29), B(30), C(31), D(32), E(33), F(34), G(35), H(36), I(37),
+    J(38), K(39), L(40), ENTER(66), DELETE(67);
 
     private int code;
 
@@ -39,9 +40,7 @@ public enum Key {
 
     /**
      * Returns a key code which is used by Android SDK
-     * (https://lh4.googleusercontent.com/-kb19V3eCj1g/TYIaEklPgmI/AAAAAAAAAXQ
-     * /cz1mEj9Wr_s/s1600/Android-input-keycodes.png)
-     * 
+     *
      * @return a key code
      */
     public int getCode() {
