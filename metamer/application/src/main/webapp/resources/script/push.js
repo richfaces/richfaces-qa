5d1bc3082840bd3aa09fbb5b0c3e957018bb3fa3
Juraj Huska <jhuska@redhat.com>
2012-01-25 14:40:38 +0100

    metamer app trunk migration

--- metamer/application/src/main/webapp/resources/script/push.js
+++
@@ -1,6 +0,0 @@
-function updateList(data) {
-    author = '<span style="font-weight:bold" class="author">' + data.author + '</span>: ';
-    message = '<span class="message">' + data.text + '</span>';
-    timestamp = ' <span>[' + data.timestamp + ']</span>';
-    jQuery("ul.push-list").append('<li>' + author + message + timestamp + '</li>');
-}
