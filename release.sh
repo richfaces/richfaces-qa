458b276258f48512644cde6a4674865b377889ed
Pavol Pitonak <ppitonak@redhat.com>
2012-01-27 18:49:59 +0100

    release script created

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-RICHFACES_VERSION=`grep '<version.richfaces>' pom.xml | sed -r 's#.*>([^<]+)<.*#\1#'`
+RICHFACES_VERSION=`grep '<version.richfaces>' metamer/pom.xml | sed -r 's#.*>([^<]+)<.*#\1#'`
 
 echo "Project defines following version of RichFaces: ${RICHFACES_VERSION}"
-echo $RICHFACES_VERSION | egrep -q '\-SNAPSHOT$' && { echo "The project cannot depend on SNAPSHOT version, correct the version first and try again."; exit 2; }
+#echo $RICHFACES_VERSION | egrep -q '\-SNAPSHOT$' && { echo "The project cannot depend on SNAPSHOT version, correct the version first and try again."; exit 2; }
 #perl -e 'open (FILE, "<", "pom.xml"); local $/; $file=<FILE>; close (FILE); if ($file !~ m#\>'$RICHFACES_VERSION'\<\/version\>\s+\<\/parent\>#sg) { exit 1 }' || { echo "The project parent have to be same version like RichFaces version"; exit 3; }
 echo "Is this version correct?"
 read -p 'Press ENTER to continue or Ctrl+C for exit...'
@@ -10,7 +10,7 @@ read -p 'Enter release version: ' RELEASE
 read -p 'Enter new development version: ' DEVELOPMENT
 read -p 'Enter path to the settings.xml: ' MVN_SETTINGS
 
-CONF="--batch-mode -Dtag=metamer-${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${DEVELOPMENT}"
+CONF="--batch-mode -Dtag=${RELEASE} -DreleaseVersion=${RELEASE} -DdevelopmentVersion=${DEVELOPMENT}"
 
 if [ -n "$MVN_SETTINGS" ]; then
    CONF="$CONF -s $MVN_SETTINGS"; 
