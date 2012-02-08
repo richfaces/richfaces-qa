3ec049f969aabad422a0f4aa79f3a48b4b32f9e9
Ján Jamrich <jjamrich@redhat.com>
2012-02-08 02:27:49 +0100

    RFPL-2037: Add resourceMapping enabling functionality
    After moved to Arquillian, legacy code (maven plugins) for managing context-params in web.xml is no longer working. Implemented new way how to manage these context-params using ShrinkWrap Descriptors

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/AbstractMetamerTest.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/AbstractMetamerTest.java
@@ -23,9 +23,7 @@ package org.richfaces.tests.metamer.ftest;
 
 import static org.jboss.arquillian.ajocado.Ajocado.id;
 import static org.jboss.arquillian.ajocado.Ajocado.retrieveText;
-
 import static org.jboss.arquillian.ajocado.javascript.JavaScript.js;
-
 import static org.jboss.test.selenium.locator.utils.LocatorEscaping.jq;
 
 import java.io.File;
@@ -41,7 +39,11 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.cheiron.retriever.ScriptEvaluationRetriever;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.spec.servlet.web.WebAppDescriptor;
+import org.jboss.shrinkwrap.impl.base.path.BasicPath;
 import org.jboss.test.selenium.locator.reference.LocatorReference;
 import org.richfaces.tests.metamer.TemplatesList;
 import org.richfaces.tests.metamer.ftest.annotations.Inject;
@@ -63,6 +65,13 @@ public abstract class AbstractMetamerTest extends Arquillian {
      * The path to the metamer application.
      */
     public static final String WEBAPP_SRC = "../application/src/main/webapp";
+
+    /** Key to manage resourceMapping enabling context-param in web.xml */
+    public static final String RESOURCE_MAPPING_ENABLED = "org.richfaces.resourceMapping.enabled";
+    /** Key to manage compressedStages context-param in web.xml */
+    public static final String RESOURCE_MAPPING_COMPRESSED_STAGES = "org.richfaces.resourceMapping.compressedStages";
+    /** Key to manage packedStages context-param in web.xml */
+    public static final String RESOURCE_MAPPING_PACKED_STAGES = "org.richfaces.resourceMapping.packedStages";
 
     /**
      * timeout in miliseconds
@@ -101,7 +110,49 @@ public abstract class AbstractMetamerTest extends Arquillian {
     public static WebArchive createTestArchive() {
 
         WebArchive war = ShrinkWrap.createFromZipFile(WebArchive.class, new File("target/metamer.war"));
+
+        /* If value on system property "org.richfaces.resourceMapping.enabled" is set to true,
+         * modify context-params in web.xml. For more info see https://issues.jboss.org/browse/RFPL-1682
+         */
+        // Note that following code verify value of system property with given key
+        if (Boolean.getBoolean(RESOURCE_MAPPING_ENABLED)) {
+            Boolean compressedStages = Boolean.getBoolean(RESOURCE_MAPPING_COMPRESSED_STAGES);
+            Boolean packedStages = Boolean.getBoolean(RESOURCE_MAPPING_PACKED_STAGES);
+            war = updateArchiveWebXml(war, compressedStages, packedStages);
+        }
+
         return war;
+    }
+
+    /*
+     * Update contex-param values in web.xml
+     * Call this function cause set org.richfaces.resourceMapping.enabled to true,
+     * and remain 2 context-params according to function params values
+     */
+    private static WebArchive updateArchiveWebXml(WebArchive defaultWar, Boolean compressedStages, Boolean packedStages) {
+        // 1. load existing web.xml from metamer.war
+        WebAppDescriptor webXmlDefault = Descriptors.importAs(WebAppDescriptor.class)
+            .from(defaultWar.get(new BasicPath("WEB-INF/web.xml")).getAsset().openStream());
+
+        // 2. modify web.xml according to requirements
+        webXmlDefault.contextParam(RESOURCE_MAPPING_ENABLED, "true");
+        if (compressedStages != null && compressedStages) {
+            webXmlDefault.contextParam(RESOURCE_MAPPING_COMPRESSED_STAGES, "All");
+        }
+        if (packedStages != null && packedStages) {
+            webXmlDefault.contextParam(RESOURCE_MAPPING_PACKED_STAGES, "All");
+        }
+
+        // 3. create second archive (war). Set here modified web.xml
+        WebArchive modifiedWar = ShrinkWrap.create(WebArchive.class);
+        modifiedWar.setWebXML(new StringAsset(webXmlDefault.exportAsString()));
+
+        // 4. merge newly created war with metamer.war (this is way how to change descriptor within archive)
+        // war.merge(tempWar); -- this way doesn't work
+        modifiedWar.merge(defaultWar);
+
+        // 5. return modified archive
+        return modifiedWar;
     }
 
     /**
