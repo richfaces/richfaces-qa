181fec35c5f86cda28daf13da8200acf66593662
Pavol Pitonak <ppitonak@redhat.com>
2012-03-26 14:32:57 +0200

    7 tests for rich:menuItem and rich:menuGroup fixed

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richMenuGroup/TestRichMenuGroup.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richMenuGroup/TestRichMenuGroup.java
@@ -21,16 +21,12 @@
  *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.richMenuGroup;
 
-import static org.jboss.arquillian.ajocado.Ajocado.elementVisible;
-import static org.jboss.arquillian.ajocado.Ajocado.guardNoRequest;
-import static org.jboss.arquillian.ajocado.Ajocado.waitGui;
-
+import static org.jboss.arquillian.ajocado.Graphene.elementVisible;
+import static org.jboss.arquillian.ajocado.Graphene.guardNoRequest;
+import static org.jboss.arquillian.ajocado.Graphene.waitGui;
 import static org.jboss.arquillian.ajocado.locator.option.OptionLocatorFactory.optionLabel;
-
 import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
-
 import static org.jboss.test.selenium.locator.utils.LocatorEscaping.jq;
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
@@ -61,8 +57,8 @@ public class TestRichMenuGroup extends AbstractAjocadoTest {
     private JQueryLocator fileMenuList = pjq("div[id$=menu1_list]");
     private JQueryLocator group = pjq("div[id$=menuGroup4]");
     private JQueryLocator groupList = pjq("div[id$=menuGroup4_list]");
-    private JQueryLocator icon = group.getDescendant(jq("> div.rf-ddm-itm-ic img"));
-    private JQueryLocator emptyIcon = group.getDescendant(jq("> div.rf-ddm-itm-ic > div.rf-ddm-emptyIcon"));
+    private JQueryLocator icon = group.getDescendant(jq("> span.rf-ddm-itm-ic img"));
+    private JQueryLocator emptyIcon = group.getDescendant(jq("> span.rf-ddm-itm-ic > span.rf-ddm-emptyIcon"));
     private JQueryLocator label = group.getDescendant(jq("span.rf-ddm-itm-lbl"));
     private JQueryLocator menuItem41 = pjq("div[id$=menuItem41]");
 
