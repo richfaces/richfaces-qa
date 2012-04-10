af2a2b1ec0b2fc3af80f157faad607a8930b20df
Jiri Stefek <jstefek@redhat.com>
2012-04-10 09:43:26 +0200

    RFPL-2143.

--- metamer/application/src/main/java/org/richfaces/tests/metamer/bean/a4j/A4JAjaxBean.java
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/a4j/A4JAjaxBean.java
@@ -1,28 +1,30 @@
-/*******************************************************************************
- * JBoss, Home of Professional Open Source
- * Copyright 2010-2012, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+/**
+ * *****************************************************************************
+ * JBoss, Home of Professional Open Source Copyright 2010-2012, Red Hat, Inc.
+ * and individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *******************************************************************************/
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ * *****************************************************************************
+ */
 package org.richfaces.tests.metamer.bean.a4j;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -53,6 +55,22 @@ public class A4JAjaxBean implements Serializable {
     private static Logger logger;
     private Attributes attributes;
     private String input;
+    private String simpleText1;
+    private String simpleText2;
+    private String autocomplete1;
+    private String autocomplete2;
+    private String editor1;
+    private String editor2;
+    private String inplaceInput1;
+    private String inplaceInput2;
+    private String inplaceSelect1;
+    private String inplaceSelect2;
+    private String inputNumberSlider1;
+    private String inputNumberSlider2;
+    private String inputNumberSpinner1;
+    private String inputNumberSpinner2;
+    private Date calendar1;
+    private Date calendar2;
     private List<?> inputs;
     private List<String> cars;
     private String car;
@@ -94,7 +112,8 @@ public class A4JAjaxBean implements Serializable {
     /**
      * Getter for attributes.
      *
-     * @return A map containing all attributes of tested component. Name of the component is key in the map.
+     * @return A map containing all attributes of tested component. Name of the
+     * component is key in the map.
      */
     public Attributes getAttributes() {
         return attributes;
@@ -103,8 +122,8 @@ public class A4JAjaxBean implements Serializable {
     /**
      * Setter for attributes.
      *
-     * @param attributes
-     *            map containing all attributes of tested component. Name of the component is key in the map.
+     * @param attributes map containing all attributes of tested component. Name
+     * of the component is key in the map.
      */
     public void setAttributes(Attributes attributes) {
         this.attributes = attributes;
@@ -112,6 +131,7 @@ public class A4JAjaxBean implements Serializable {
 
     /**
      * Getter for input.
+     *
      * @return content of the input field on the page
      */
     public String getInput() {
@@ -120,6 +140,7 @@ public class A4JAjaxBean implements Serializable {
 
     /**
      * Setter for input.
+     *
      * @param input new content of the input field on the page
      */
     public void setInput(String input) {
@@ -151,10 +172,11 @@ public class A4JAjaxBean implements Serializable {
     }
 
     /**
-     * An action listener that takes the first six characters from input and stores it to input.
+     * An action listener that takes the first six characters from input and
+     * stores it to input.
      *
-     * @param event
-     *            an event representing the activation of a user interface component (not used)
+     * @param event an event representing the activation of a user interface
+     * component (not used)
      */
     public void first6CharsListener(AjaxBehaviorEvent event) {
         if (input == null) {
@@ -166,10 +188,11 @@ public class A4JAjaxBean implements Serializable {
     }
 
     /**
-     * An action listener that takes user's input, doubles it and stores it to input.
+     * An action listener that takes user's input, doubles it and stores it to
+     * input.
      *
-     * @param event
-     *            an event representing the activation of a user interface component (not used)
+     * @param event an event representing the activation of a user interface
+     * component (not used)
      */
     public void doubleStringListener(AjaxBehaviorEvent event) {
         if (input == null) {
@@ -180,10 +203,11 @@ public class A4JAjaxBean implements Serializable {
     }
 
     /**
-     * An action listener that takes user's input, converts it to upper case and stores it to input3.
+     * An action listener that takes user's input, converts it to upper case and
+     * stores it to input3.
      *
-     * @param event
-     *            an event representing the activation of a user interface component (not used)
+     * @param event an event representing the activation of a user interface
+     * component (not used)
      */
     public void toUpperCaseListener(AjaxBehaviorEvent event) {
         if (input == null) {
@@ -195,7 +219,9 @@ public class A4JAjaxBean implements Serializable {
 
     /**
      * An action listener causing error. Suitable for testing onerror attribute.
-     * @param event an event representing the activation of a user interface component (not used)
+     *
+     * @param event an event representing the activation of a user interface
+     * component (not used)
      */
     public void causeErrorListener(AjaxBehaviorEvent event) {
         throw new FacesException("Ajax request caused an error. This is intentional behavior.");
@@ -207,5 +233,133 @@ public class A4JAjaxBean implements Serializable {
 
     public void setInputs(List<?> inputs) {
         this.inputs = inputs;
+    }
+
+    public String getAutocomplete1() {
+        return autocomplete1;
+    }
+
+    public void setAutocomplete1(String autocomplete1) {
+        this.autocomplete1 = autocomplete1;
+    }
+
+    public String getAutocomplete2() {
+        return autocomplete2;
+    }
+
+    public void setAutocomplete2(String autocomplete2) {
+        this.autocomplete2 = autocomplete2;
+    }
+
+    public Date getCalendar1() {
+        return calendar1;
+    }
+
+    public void setCalendar1(Date calendar1) {
+        this.calendar1 = calendar1;
+    }
+
+    public Date getCalendar2() {
+        return calendar2;
+    }
+
+    public void setCalendar2(Date calendar2) {
+        this.calendar2 = calendar2;
+    }
+
+    public String getEditor1() {
+        return editor1;
+    }
+
+    public void setEditor1(String editor1) {
+        this.editor1 = editor1;
+    }
+
+    public String getEditor2() {
+        return editor2;
+    }
+
+    public void setEditor2(String editor2) {
+        this.editor2 = editor2;
+    }
+
+    public String getInplaceInput1() {
+        return inplaceInput1;
+    }
+
+    public void setInplaceInput1(String inplaceInput1) {
+        this.inplaceInput1 = inplaceInput1;
+    }
+
+    public String getInplaceInput2() {
+        return inplaceInput2;
+    }
+
+    public void setInplaceInput2(String inplaceInput2) {
+        this.inplaceInput2 = inplaceInput2;
+    }
+
+    public String getInplaceSelect1() {
+        return inplaceSelect1;
+    }
+
+    public void setInplaceSelect1(String inplaceSelect1) {
+        this.inplaceSelect1 = inplaceSelect1;
+    }
+
+    public String getInplaceSelect2() {
+        return inplaceSelect2;
+    }
+
+    public void setInplaceSelect2(String inplaceSelect2) {
+        this.inplaceSelect2 = inplaceSelect2;
+    }
+
+    public String getInputNumberSlider1() {
+        return inputNumberSlider1;
+    }
+
+    public void setInputNumberSlider1(String inputNumberSlider1) {
+        this.inputNumberSlider1 = inputNumberSlider1;
+    }
+
+    public String getInputNumberSlider2() {
+        return inputNumberSlider2;
+    }
+
+    public void setInputNumberSlider2(String inputNumberSlider2) {
+        this.inputNumberSlider2 = inputNumberSlider2;
+    }
+
+    public String getInputNumberSpinner1() {
+        return inputNumberSpinner1;
+    }
+
+    public void setInputNumberSpinner1(String inputNumberSpinner1) {
+        this.inputNumberSpinner1 = inputNumberSpinner1;
+    }
+
+    public String getInputNumberSpinner2() {
+        return inputNumberSpinner2;
+    }
+
+    public void setInputNumberSpinner2(String inputNumberSpinner2) {
+        this.inputNumberSpinner2 = inputNumberSpinner2;
+    }
+
+    public String getSimpleText1() {
+        return simpleText1;
+    }
+
+    public void setSimpleText1(String simpleText1) {
+        this.simpleText1 = simpleText1;
+    }
+
+    public String getSimpleText2() {
+        return simpleText2;
+    }
+
+    public void setSimpleText2(String simpleText2) {
+        this.simpleText2 = simpleText2;
     }
 }
