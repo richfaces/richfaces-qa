e89e62bdb61fd137c7be1f8e5767b5892db96288
Jiri Stefek <jstefek@redhat.com>
2012-05-16 11:08:30 +0200

    RFPL-2203: Metamer: added tests for attribute frozenColumns for EDT.

---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richExtendedDataTable/TestEDTFrozenColumns.java
@@ -0,0 +1,162 @@
+/**
+ * JBoss, Home of Professional Open Source Copyright 2012, Red Hat, Inc. and
+ * individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.richfaces.tests.metamer.ftest.richExtendedDataTable;
+
+import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
+import static org.richfaces.tests.metamer.ftest.webdriver.AttributeList.extendedDataTableAttributes;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.net.URL;
+import java.util.List;
+import org.jboss.test.selenium.support.ui.ElementDisplayed;
+import org.jboss.test.selenium.support.ui.ElementNotPresent;
+import org.jboss.test.selenium.support.ui.ElementPresent;
+import org.jboss.test.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
+import org.richfaces.tests.metamer.ftest.annotations.Inject;
+import org.richfaces.tests.metamer.ftest.annotations.Use;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
+ */
+public class TestEDTFrozenColumns extends AbstractWebDriverTest {
+
+    private FrozenColumnsPage page;
+    @Inject
+    @Use(empty = false)
+    private Integer numberOfColumns;
+    private Point location;//for testScrollerForNotFrozenColumns
+
+    @Override
+    public URL getTestUrl() {
+        return buildUrl(contextPath, "faces/components/richExtendedDataTable/frozenColumns.xhtml");
+    }
+
+    @BeforeMethod
+    public void loadPage() {
+        page = new FrozenColumnsPage();
+        injectWebElementsToPage(page);
+    }
+
+    @Test
+    public void testInit() {
+        Boolean notPresent = ElementNotPresent.getInstance().element(page.frozenColumnsTd).apply(driver);
+        assertTrue(notPresent, "No frozen columns should be in page.");
+    }
+
+    /**
+     * Tests if frozen columns html elements show when frozen columns are
+     * enabled(set to >=1).Checks if they are still there after switching to
+     * another data page.
+     */
+    @Test
+    @Use(field = "numberOfColumns", ints = { 0, 2, 4 })
+    public void testFrozenColumnsShow() {
+        extendedDataTableAttributes.set(ExtendedDataTableAttributes.frozenColumns, numberOfColumns);
+        //test
+        _testFrozenColumnsShow();
+
+        //change page
+        page.nextPage.click();
+        new WebDriverWait(driver, 5).until(ElementDisplayed.getInstance().element(page.secondPageSpan));
+        //test
+        _testFrozenColumnsShow();
+
+        //change page
+        page.nextPage.click();
+        new WebDriverWait(driver, 5).until(ElementDisplayed.getInstance().element(page.thirdPageSpan));
+        //test
+        _testFrozenColumnsShow();
+    }
+
+    public void _testFrozenColumnsShow() {
+        //wait for list of elements with frozen columns with expected size
+        List<WebElement> frozenColumns = guardListSize(page.frozenColumns, numberOfColumns);
+        assertEquals(Integer.valueOf(frozenColumns.size()), numberOfColumns,
+                "The number of frozen columns set is not equal to the number of frozen columns found");
+    }
+
+    /**
+     * Tests if scroller for not frozen columns is moved to another position and
+     * if the default one is removed. Checks if scroller is still there after
+     * switching to another data page.
+     */
+    @Test
+    @Use(field = "numberOfColumns", ints = { 1, 3 })
+    //TODO https://issues.jboss.org/browse/RF-12236 , when numberOfColumns=4
+    public void testScrollerForNotFrozenColumns() {
+        //check if default scroller is present and get its location
+        Boolean present = ElementPresent.getInstance().element(page.defaultScroller).apply(driver);
+        assertTrue(present, "Default scroller is not in the page.");
+        location = page.defaultScroller.getLocation();
+        extendedDataTableAttributes.set(ExtendedDataTableAttributes.frozenColumns, numberOfColumns);
+        //test
+        _testScrollerForNotFrozenColumns();
+
+        //change page
+        page.nextPage.click();
+        new WebDriverWait(driver, 5).until(ElementDisplayed.getInstance().element(page.secondPageSpan));
+        //test
+        _testScrollerForNotFrozenColumns();
+
+        //change page
+        page.nextPage.click();
+        new WebDriverWait(driver, 5).until(ElementDisplayed.getInstance().element(page.thirdPageSpan));
+        //test
+        _testScrollerForNotFrozenColumns();
+    }
+
+    private void _testScrollerForNotFrozenColumns() {
+        //check if there is default scroller
+        Boolean present = ElementNotPresent.getInstance().element(page.defaultScroller).apply(driver);
+        assertTrue(present, "Default scroller should not be in the page.");
+        //check if there is scroller for not frozen columns
+        present = ElementPresent.getInstance().element(page.movedScroller).apply(driver);
+        assertTrue(present, "Scroller for the not frozen columns should be in the page.");
+        //check if the location of scroller moved
+        assertNotEquals(location, page.movedScroller.getLocation(), "The position of scroller has not been changed.");
+    }
+
+    private class FrozenColumnsPage {
+
+        @FindBy(xpath = "//td[@class='rf-edt-ftr-fzn']//div//table//tr//td")
+        List<WebElement> frozenColumns;
+        @FindBy(xpath = "//td[@class='rf-edt-ftr-fzn']")
+        WebElement frozenColumnsTd;
+        @FindBy(xpath = "//div[@class='rf-edt-ftr']//table//tbody//tr//td[@colspan=1][1]//div[@class='rf-edt-scrl']")
+        WebElement defaultScroller;
+        @FindBy(xpath = "//div[@class='rf-edt-ftr']//table//tbody//tr//td[@colspan=1][2]//div[@class='rf-edt-scrl']")
+        WebElement movedScroller;
+        @FindBy(css = "a.rf-ds-btn-next")
+        WebElement nextPage;
+        @FindBy(xpath = "//span[contains(@id, 'scroller2_ds_2')][contains(text(), '2')]")
+        WebElement secondPageSpan;
+        @FindBy(xpath = "//span[contains(@id, 'scroller2_ds_3')][contains(text(), '3')]")
+        WebElement thirdPageSpan;
+    }
+}
