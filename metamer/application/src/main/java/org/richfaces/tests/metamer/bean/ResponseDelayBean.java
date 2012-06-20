4ea1af5e3fb18220bd5c985207b9f4d63e06935a
Pavol Pitonak <ppitonak@redhat.com>
2012-06-20 16:47:13 +0200

    ResponseDelayBean made serializable

--- metamer/application/src/main/java/org/richfaces/tests/metamer/bean/ResponseDelayBean.java
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/ResponseDelayBean.java
@@ -1,24 +1,27 @@
-/**
- * JBoss, Home of Professional Open Source Copyright 2012, Red Hat, Inc. and
- * individual contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2012, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
- */
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
 package org.richfaces.tests.metamer.bean;
+
+import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -29,8 +32,9 @@ import javax.faces.bean.ViewScoped;
  */
 @ManagedBean(name = "responseDelayBean")
 @ViewScoped
-public class ResponseDelayBean {
+public class ResponseDelayBean implements Serializable {
 
+    private static final long serialVersionUID = -8116123004872799500L;
     private int delay;
 
     public int getDelay() {
