3eb433a23ff4871f5ea8f1254c67d39ae26f0479
Jiri Stefek <jstefek@redhat.com>
2012-07-03 10:38:23 +0200

    RFPL-1744: Metamer: created samples and tests for validation messages.

---
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/validation/ValidationMessagesBean.java
@@ -0,0 +1,273 @@
+/**
+ * JBoss, Home of Professional Open Source Copyright 2012, Red Hat, Inc. and
+ * individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.richfaces.tests.metamer.validation;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.model.SelectItem;
+import javax.faces.validator.ValidatorException;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+
+/**
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
+ */
+@ManagedBean
+@ViewScoped
+public class ValidationMessagesBean implements Serializable {
+
+    private final long pastTime = System.currentTimeMillis() - 4 * 24 * 3600 * 1000;
+    private final long futureTime = System.currentTimeMillis() + 4 * 24 * 3600 * 1000;
+    private Date pastDate = new Date(pastTime);
+    private Date futureDate = new Date(futureTime);
+    public static final String MSG = "Custom validator error message from bean.";
+    //JSR
+    @Min(value = 2, message = MSG)
+    @Max(value = 9, message = MSG)
+    private Integer valueWithMessagesJSR = new Integer(5);
+    @Min(2)
+    @Max(9)
+    private Integer valueWithoutMessagesJSR = new Integer(5);
+    @Min(2)
+    @Max(9)
+    private Integer valueWithoutMessages2JSR = new Integer(5);
+    @Past(message = MSG)
+    private Date dateWithMessagesJSR = new Date(pastTime);
+    @Past
+    private Date dateWithoutMessagesJSR = new Date(pastTime);
+    @Past
+    private Date dateWithoutMessages2JSR = new Date(pastTime);
+    //CSV
+    @Min(value = 2, message = MSG)
+    @Max(value = 9, message = MSG)
+    private Integer valueWithMessagesCSV = new Integer(5);
+    @Min(2)
+    @Max(9)
+    private Integer valueWithoutMessagesCSV = new Integer(5);
+    @Min(2)
+    @Max(9)
+    private Integer valueWithoutMessages2CSV = new Integer(5);
+    @Past(message = MSG)
+    private Date dateWithMessagesCSV = new Date(pastTime);
+    @Past
+    private Date dateWithoutMessagesCSV = new Date(pastTime);
+    @Past
+    private Date dateWithoutMessages2CSV = new Date(pastTime);
+    //JSF
+    private Integer valueJSF = new Integer(5);
+    private Integer value2JSF = new Integer(5);
+    private Date dateJSF = new Date(pastTime);
+    private Date date2JSF = new Date(pastTime);
+    private Converter converter = FacesContext.getCurrentInstance().getApplication().createConverter(Integer.class);
+    private List<SelectItem> validationOptions = null;
+
+    @PostConstruct
+    public void init() {
+        validationOptions = new ArrayList<SelectItem>();
+        validationOptions.add(new SelectItem(5));
+        validationOptions.add(new SelectItem(10));
+    }
+
+    public List<SelectItem> getValidationOptions() {
+        return validationOptions;
+    }
+
+    public Converter getConverter() {
+        return converter;
+    }
+
+    public String getDescription() {
+        return "Integer, from 2 to 9";
+    }
+
+    public void activate() {
+        FacesContext.getCurrentInstance().getApplication().setMessageBundle("CustomErrorMessages");
+    }
+
+    public void deactivate() {
+        FacesContext.getCurrentInstance().getApplication().setMessageBundle("DefaultErrorMessages");
+    }
+
+    public Integer getValueJSF() {
+        return valueJSF;
+    }
+
+    public void setValueJSF(Integer valueJSF) {
+        this.valueJSF = valueJSF;
+    }
+
+    public Integer getValueWithMessagesCSV() {
+        return valueWithMessagesCSV;
+    }
+
+    public void setValueWithMessagesCSV(Integer valueWithMessagesCSV) {
+        this.valueWithMessagesCSV = valueWithMessagesCSV;
+    }
+
+    public Integer getValueWithMessagesJSR() {
+        return valueWithMessagesJSR;
+    }
+
+    public void setValueWithMessagesJSR(Integer valueWithMessagesJSR) {
+        this.valueWithMessagesJSR = valueWithMessagesJSR;
+    }
+
+    public Integer getValueWithoutMessages2CSV() {
+        return valueWithoutMessages2CSV;
+    }
+
+    public void setValueWithoutMessages2CSV(Integer valueWithoutMessages2CSV) {
+        this.valueWithoutMessages2CSV = valueWithoutMessages2CSV;
+    }
+
+    public Integer getValueWithoutMessages2JSR() {
+        return valueWithoutMessages2JSR;
+    }
+
+    public void setValueWithoutMessages2JSR(Integer valueWithoutMessages2JSR) {
+        this.valueWithoutMessages2JSR = valueWithoutMessages2JSR;
+    }
+
+    public Integer getValueWithoutMessagesCSV() {
+        return valueWithoutMessagesCSV;
+    }
+
+    public void setValueWithoutMessagesCSV(Integer valueWithoutMessagesCSV) {
+        this.valueWithoutMessagesCSV = valueWithoutMessagesCSV;
+    }
+
+    public Integer getValueWithoutMessagesJSR() {
+        return valueWithoutMessagesJSR;
+    }
+
+    public void setValueWithoutMessagesJSR(Integer valueWithoutMessagesJSR) {
+        this.valueWithoutMessagesJSR = valueWithoutMessagesJSR;
+    }
+
+    public Integer getValue2JSF() {
+        return value2JSF;
+    }
+
+    public void setValue2JSF(Integer value2JSF) {
+        this.value2JSF = value2JSF;
+    }
+
+    public String printCurrentResourceBundle() {
+        return FacesContext.getCurrentInstance().getApplication().getMessageBundle();
+    }
+
+    public Date getDate2JSF() {
+        return date2JSF;
+    }
+
+    public void setDate2JSF(Date date2JSF) {
+        this.date2JSF = date2JSF;
+    }
+
+    public Date getDateJSF() {
+        return dateJSF;
+    }
+
+    public void setDateJSF(Date dateJSF) {
+        this.dateJSF = dateJSF;
+    }
+
+    public Date getDateWithMessagesCSV() {
+        return dateWithMessagesCSV;
+    }
+
+    public void setDateWithMessagesCSV(Date dateWithMessagesCSV) {
+        this.dateWithMessagesCSV = dateWithMessagesCSV;
+    }
+
+    public Date getDateWithMessagesJSR() {
+        return dateWithMessagesJSR;
+    }
+
+    public void setDateWithMessagesJSR(Date dateWithMessagesJSR) {
+        this.dateWithMessagesJSR = dateWithMessagesJSR;
+    }
+
+    public Date getDateWithoutMessages2CSV() {
+        return dateWithoutMessages2CSV;
+    }
+
+    public void setDateWithoutMessages2CSV(Date dateWithoutMessages2CSV) {
+        this.dateWithoutMessages2CSV = dateWithoutMessages2CSV;
+    }
+
+    public Date getDateWithoutMessages2JSR() {
+        return dateWithoutMessages2JSR;
+    }
+
+    public void setDateWithoutMessages2JSR(Date dateWithoutMessages2JSR) {
+        this.dateWithoutMessages2JSR = dateWithoutMessages2JSR;
+    }
+
+    public Date getDateWithoutMessagesCSV() {
+        return dateWithoutMessagesCSV;
+    }
+
+    public void setDateWithoutMessagesCSV(Date dateWithoutMessagesCSV) {
+        this.dateWithoutMessagesCSV = dateWithoutMessagesCSV;
+    }
+
+    public Date getDateWithoutMessagesJSR() {
+        return dateWithoutMessagesJSR;
+    }
+
+    public void setDateWithoutMessagesJSR(Date dateWithoutMessagesJSR) {
+        this.dateWithoutMessagesJSR = dateWithoutMessagesJSR;
+    }
+
+    public Date getFutureDate() {
+        return futureDate;
+    }
+
+    public Date getPastDate() {
+        return pastDate;
+    }
+
+    public void setFutureDate(Date futureDate) {
+        this.futureDate = futureDate;
+    }
+
+    public void setPastDate(Date pastDate) {
+        this.pastDate = pastDate;
+    }
+
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        Date d = (Date) value;
+        if (d.getTime() > System.currentTimeMillis()) {//must be in past
+            throw new ValidatorException(new FacesMessage("Own default message."));
+        }
+    }
+}
---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/abstractions/AbstractValidationMessagesTest.java
@@ -0,0 +1,288 @@
+/**
+ * JBoss, Home of Professional Open Source Copyright 2012, Red Hat, Inc. and
+ * individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+package org.richfaces.tests.metamer.ftest.abstractions;
+
+import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.net.URL;
+import org.jboss.test.selenium.support.ui.ElementPresent;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
+ */
+public abstract class AbstractValidationMessagesTest extends AbstractWebDriverTest {
+
+    protected ValidationPage page = new ValidationPage();
+    private ElementPresent elementPresent = ElementPresent.getInstance();
+    protected static final String MSG_ATT = "Custom validator error message from attribute of component.";
+    private static final String MSG_BEAN = "Custom validator error message from bean.";
+    private static final String MSG_BUNDLE_JSR = "Custom validator error message from bundle (JSR-303).";
+    private static final String MSG_BUNDLE_JSF_CUSTOM = "Custom validator error message from bundle (JSF).";
+    private static final String MSG_BUNDLE_JSF_DEFAULT = "form:jsf-inBundle: Validation Error: Specified attribute is not between the expected values of 2 and 9.";
+    private final String JS_STATE_VARIABLE = "document.valuesSettingState";
+    private final String JS_COMPLETED_STATE_STRING = "completed";
+    private final String component;
+
+    public AbstractValidationMessagesTest(String component) {
+        this.component = component;
+    }
+
+    @Override
+    public URL getTestUrl() {
+        return buildUrl(contextPath, "faces/components/" + component + "/validationMessages.xhtml");
+    }
+
+    public void testInit() {
+        page.setCorrectValuesAndSubmitJSF();
+        assertTrue(page.noErrorMessagesDisplayed(), "No error messages should now be on page.");
+        page.setCorrectValuesAndSubmitRF();
+        assertTrue(page.noErrorMessagesDisplayed(), "No error messages should now be on page.");
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void setDefault() {
+        page.deactivateCustomMessages();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void pageLoad() {
+        injectWebElementsToPage(page);
+        setDefault();
+        //inits document JavaScript variable, which will hold state of setting of all inputs
+        executeJS(JS_STATE_VARIABLE + "=''");
+    }
+
+    /**
+     * Tests correctness of JSR-303 validation messages using h:commandButton
+     * for submitting. Tests messages set in attribute of component, in
+     * annotations of bean and in message bundle.
+     */
+    public void testJSR303MessagesJSF() {
+        page.setWrongValuesAndSubmitJSF();
+        assertEquals(page.jsr303InAttMsg.getText(), MSG_ATT);
+        assertEquals(page.jsr303InBeanMsg.getText(), MSG_BEAN);
+        assertEquals(page.jsr303InBundleMsg.getText(), MSG_BUNDLE_JSR);
+    }
+
+    /**
+     * Tests correctness of JSR-303 validation messages using a4j:commandButton
+     * for submitting. Tests messages set in attribute of component, in
+     * annotations of bean and in message bundle.
+     */
+    public void testJSR303MessagesRF() {
+        page.setWrongValuesAndSubmitRF();
+        assertEquals(page.jsr303InAttMsg.getText(), MSG_ATT);
+        assertEquals(page.jsr303InBeanMsg.getText(), MSG_BEAN);
+        assertEquals(page.jsr303InBundleMsg.getText(), MSG_BUNDLE_JSR);
+    }
+
+    /**
+     * Tests correctness of CSV validation messages using h:commandButton for
+     * submitting. Tests messages set in attribute of component, in annotations
+     * of bean and in message bundle.
+     */
+    public void testCSVMessagesJSF() {
+        page.setWrongValuesAndSubmitJSF();
+        assertEquals(page.csvInAttMsg.getText(), MSG_ATT);
+        assertEquals(page.csvInBeanMsg.getText(), MSG_BEAN);
+        assertEquals(page.csvInBundleMsg.getText(), MSG_BUNDLE_JSR);
+    }
+
+    /**
+     * Tests correctness of CSV validation messages using a4j:commandButton for
+     * submitting. Tests messages set in attribute of component, in annotations
+     * of bean and in message bundle.
+     */
+    public void testCSVMessagesRF() {
+        page.setWrongValuesAndSubmitRF();
+        assertEquals(page.csvInAttMsg.getText(), MSG_ATT);
+        assertEquals(page.csvInBeanMsg.getText(), MSG_BEAN);
+        assertEquals(page.csvInBundleMsg.getText(), MSG_BUNDLE_JSR);
+    }
+
+    /**
+     * Tests correctness of JSF validation messages using h:commandButton for
+     * submitting. Tests messages set in attribute of component and in message
+     * bundle.
+     */
+    public void testJSFMessagesJSF() {
+        page.setWrongValuesAndSubmitJSF();
+        assertEquals(page.jsfInAttMsg.getText(), MSG_ATT);
+        assertEquals(page.jsfInBundleMsg.getText(), MSG_BUNDLE_JSF_DEFAULT);
+        page.activateCustomMessages();
+        page.setWrongValuesAndSubmitJSF();
+        assertEquals(page.jsfInAttMsg.getText(), MSG_ATT);
+        assertEquals(page.jsfInBundleMsg.getText(), MSG_BUNDLE_JSF_CUSTOM);
+    }
+
+    /**
+     * Tests correctness of JSF validation messages using a4j:commandButton for
+     * submitting. Tests messages set in attribute of component and in message
+     * bundle.
+     */
+    public void testJSFMessagesRF() {
+        page.setWrongValuesAndSubmitRF();
+        assertEquals(page.jsfInAttMsg.getText(), MSG_ATT);
+        assertEquals(page.jsfInBundleMsg.getText(), MSG_BUNDLE_JSF_DEFAULT);
+        page.activateCustomMessages();
+        page.setWrongValuesAndSubmitRF();
+        assertEquals(page.jsfInAttMsg.getText(), MSG_ATT);
+        assertEquals(page.jsfInBundleMsg.getText(), MSG_BUNDLE_JSF_CUSTOM);
+    }
+
+    protected class ValidationPage {
+
+        @FindBy(id = "setCorrectValuesButton")
+        WebElement setCorrectValuesButton;
+        @FindBy(id = "setWrongValuesButton")
+        WebElement setWrongValuesButton;
+        @FindBy(css = "span[id$='jsr-303-inBean-msg'] span[class='rf-msg-det']")
+        WebElement jsr303InBeanMsg;
+        @FindBy(css = "span[id$='jsr-303-inAtt-msg'] span[class='rf-msg-det']")
+        WebElement jsr303InAttMsg;
+        @FindBy(css = "span[id$='jsr-303-inBundle-msg'] span[class='rf-msg-det']")
+        WebElement jsr303InBundleMsg;
+        @FindBy(css = "span[id$='csv-inBean-msg'] span[class='rf-msg-det']")
+        WebElement csvInBeanMsg;
+        @FindBy(css = "span[id$='csv-inAtt-msg'] span[class='rf-msg-det']")
+        WebElement csvInAttMsg;
+        @FindBy(css = "span[id$='csv-inBundle-msg'] span[class='rf-msg-det']")
+        WebElement csvInBundleMsg;
+        @FindBy(css = "span[id$='jsf-inAtt-msg'] span[class='rf-msg-det']")
+        WebElement jsfInAttMsg;
+        @FindBy(css = "span[id$='jsf-inBundle-msg'] span[class='rf-msg-det']")
+        WebElement jsfInBundleMsg;
+        @FindBy(css = "input[id$=hButton]")
+        WebElement jsfSubmitBtn;
+        @FindBy(css = "input[id$=a4jButton]")
+        WebElement rfSubmitBtn;
+        @FindBy(css = "input[id$=activateButton]")
+        WebElement activateButton;
+        @FindBy(css = "input[id$=deactivateButton]")
+        WebElement deactivateButton;
+
+        /**
+         * @return text of validation message for component jsf-inAtt (component
+         * using jsf validator with message set in attribute of component)
+         */
+        public String getJSFInAttMSG() {
+            return this.jsfInAttMsg.getText();
+        }
+
+        /**
+         * Sets correct values to all inputs on page with waiting for all inputs
+         * are set and submits a form with h:commandButton.
+         */
+        public void setCorrectValuesAndSubmitJSF() {
+            setCorrectValuesButton.click();
+            waitForSetting();
+            waitRequest(jsfSubmitBtn, WaitRequestType.HTTP).click();
+        }
+
+        /**
+         * Sets correct values to all inputs on page with waiting for all inputs
+         * are set and submits a form with a4j:commandButton.
+         */
+        public void setCorrectValuesAndSubmitRF() {
+            setCorrectValuesButton.click();
+            waitForSetting();
+            waitRequest(rfSubmitBtn, WaitRequestType.XHR).click();
+        }
+
+        /**
+         * Sets wrong values to all inputs on page with waiting for all inputs
+         * are set and submits a form with h:commandButton.
+         */
+        public void setWrongValuesAndSubmitJSF() {
+            setWrongValuesButton.click();
+            waitForSetting();
+            waitRequest(jsfSubmitBtn, WaitRequestType.HTTP).click();
+        }
+
+        /**
+         * Sets wrong values to all inputs on page with waiting for all inputs
+         * are set and submits a form with a4j:commandButton.
+         */
+        public void setWrongValuesAndSubmitRF() {
+            setWrongValuesButton.click();
+            waitForSetting();
+            waitRequest(rfSubmitBtn, WaitRequestType.XHR).click();
+        }
+
+        /**
+         * Waits for setting of all inputs. Executes JavaScript demanding a
+         * variable with state of setting.
+         */
+        private void waitForSetting() {
+            expectedReturnJS(JS_STATE_VARIABLE, JS_COMPLETED_STATE_STRING);
+        }
+
+        /**
+         * Sets wrong values to all inputs in page via JavaScript enhanced
+         * button.
+         */
+        public void setWrongValues() {
+            setWrongValuesButton.click();
+        }
+
+        /**
+         * Checks if some error messages are displayed on page.
+         *
+         * @return false if some error message is there, else true
+         */
+        public boolean noErrorMessagesDisplayed() {
+            if (elementPresent.element(csvInAttMsg).apply(driver)
+                    || elementPresent.element(csvInBeanMsg).apply(driver)
+                    || elementPresent.element(csvInBundleMsg).apply(driver)
+                    || elementPresent.element(jsfInAttMsg).apply(driver)
+                    || elementPresent.element(jsfInBundleMsg).apply(driver)
+                    || elementPresent.element(jsr303InAttMsg).apply(driver)
+                    || elementPresent.element(jsr303InBeanMsg).apply(driver)
+                    || elementPresent.element(jsr303InBundleMsg).apply(driver)) {
+                return false;
+            }
+            return true;
+        }
+
+        /**
+         * Activates custom validation messages
+         */
+        public void activateCustomMessages() {
+            setCorrectValuesAndSubmitJSF();
+            waitRequest(activateButton, WaitRequestType.XHR).click();
+        }
+
+        /**
+         * Deactivates custom validation messages.
+         */
+        public void deactivateCustomMessages() {
+            setCorrectValuesAndSubmitJSF();
+            waitRequest(deactivateButton, WaitRequestType.XHR).click();
+        }
+    }
+}
