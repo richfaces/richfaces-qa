a9dc6d0b69e34a65892e80555562cabfbaab5387
Pavol Pitonak <ppitonak@redhat.com>
2012-09-07 12:20:47 +0200

    tests for a4j:ajax with h:selectBooleanCheckbox, h:selectManyCheckbox and h:selectOneRadio rewritten to Selenium 2

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/a4jAjax/TestHSelectOneRadio.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/a4jAjax/TestHSelectOneRadio.java
@@ -21,39 +21,22 @@
  *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.a4jAjax;
 
-import static org.richfaces.tests.metamer.ftest.attributes.AttributeList.ajaxAttributes;
-import static org.jboss.arquillian.ajocado.Graphene.guardNoRequest;
-import static org.jboss.arquillian.ajocado.Graphene.guardXhr;
-import static org.jboss.arquillian.ajocado.Graphene.retrieveText;
-import static org.jboss.arquillian.ajocado.Graphene.waitGui;
-
 import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
-
-import static org.jboss.test.selenium.locator.utils.LocatorEscaping.jq;
-
+import static org.richfaces.tests.metamer.ftest.webdriver.AttributeList.ajaxAttributes;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.fail;
 
 import java.net.URL;
 
-import javax.faces.event.PhaseId;
-
-import org.jboss.arquillian.ajocado.javascript.JavaScript;
-import org.jboss.arquillian.ajocado.locator.JQueryLocator;
-import org.richfaces.tests.metamer.ftest.AbstractGrapheneTest;
+import org.jboss.arquillian.graphene.Graphene;
 import org.testng.annotations.Test;
 
 /**
  * Test case for page /faces/components/a4jAjax/hSelectOneRadio.xhtml
  *
- * @author <a href="mailto:ppitonak@redhat.com">Pavol Pitonak</a>
- * @version $Revision: 22054 $
+ * @author <a href="https://community.jboss.org/people/ppitonak">Pavol Pitonak</a>
+ * @since 4.3.0.M2
  */
-public class TestHSelectOneRadio extends AbstractGrapheneTest {
-
-    private JQueryLocator input = pjq("table[id$=selectOneRadio] input[type=radio][value=Audi]");
-    private JQueryLocator output1 = pjq("span[id$=output1]");
-    private JQueryLocator output2 = pjq("span[id$=output2]");
+public class TestHSelectOneRadio extends AbstractAjaxTest {
 
     @Override
     public URL getTestUrl() {
@@ -62,153 +45,82 @@ public class TestHSelectOneRadio extends AbstractGrapheneTest {
 
     @Test(groups = { "screenshot" })
     public void testSimpleClick() {
-        guardXhr(selenium).click(input);
-
-        String outputValue = waitGui.failWith("Page was not updated").waitForChangeAndReturn("[Ferrari, Lexus]",
-            retrieveText.locator(output1));
-
-        assertEquals(outputValue, "Audi", "Wrong output1");
-        assertEquals(selenium.getText(output2), "Audi", "Wrong output2");
+        super.testClick();
     }
 
     @Test
     public void testBypassUpdates() {
-        String reqTime = selenium.getText(time);
-
-        ajaxAttributes.set(AjaxAttributes.listener, "doubleStringListener");
-        ajaxAttributes.set(AjaxAttributes.bypassUpdates, true);
-
-        guardXhr(selenium).click(input);
-        waitGui.failWith("Page was not updated").waitForChange(reqTime, retrieveText.locator(time));
-
-        assertEquals(selenium.getText(output1), "Ferrari", "Output should not change");
-        phaseInfo.assertPhases(PhaseId.RESTORE_VIEW, PhaseId.APPLY_REQUEST_VALUES, PhaseId.PROCESS_VALIDATIONS,
-            PhaseId.RENDER_RESPONSE);
-        phaseInfo.assertListener(PhaseId.PROCESS_VALIDATIONS, "listener invoked");
+        super.testBypassUpdates();
     }
 
     @Test
     public void testData() {
-        ajaxAttributes.set(AjaxAttributes.data, "RichFaces 4 data");
-        ajaxAttributes.set(AjaxAttributes.oncomplete, "data = event.data");
-
-        String reqTime = selenium.getText(time);
-        guardXhr(selenium).click(input);
-        waitGui.failWith("Page was not updated").waitForChange(reqTime, retrieveText.locator(time));
-
-        String data = selenium.getEval(new JavaScript("window.data"));
-        assertEquals(data, "RichFaces 4 data", "Data sent with ajax request");
+        super.testData();
     }
 
     @Test
     public void testDisabled() {
         ajaxAttributes.set(AjaxAttributes.disabled, true);
-
-        guardNoRequest(selenium).click(input);
+        Graphene.guardNoRequest(page.selectOneRadio).click();
     }
 
     @Test
     public void testExecute() {
-        String reqTime = selenium.getText(time);
-
-        ajaxAttributes.set(AjaxAttributes.execute, "input executeChecker");
-
-        guardXhr(selenium).click(input);
-        waitGui.failWith("Page was not updated").waitForChange(reqTime, retrieveText.locator(time));
-
-        JQueryLocator logItems = jq("ul.phases-list li:eq({0})");
-        for (int i = 0; i < 6; i++) {
-            if ("* executeChecker".equals(selenium.getText(logItems.format(i)))) {
-                return;
-            }
-        }
-
-        fail("Attribute execute does not work");
+        super.testExecute();
     }
 
     @Test
     public void testImmediate() {
-        String reqTime = selenium.getText(time);
-
-        ajaxAttributes.set(AjaxAttributes.listener, "doubleStringListener");
-        ajaxAttributes.set(AjaxAttributes.immediate, true);
-
-        guardXhr(selenium).click(input);
-        waitGui.failWith("Page was not updated").waitForChange(reqTime, retrieveText.locator(time));
-
-        assertEquals(selenium.getText(output1), "Audi", "Output should change");
-        phaseInfo.assertPhases(PhaseId.RESTORE_VIEW, PhaseId.APPLY_REQUEST_VALUES, PhaseId.PROCESS_VALIDATIONS,
-            PhaseId.UPDATE_MODEL_VALUES, PhaseId.INVOKE_APPLICATION, PhaseId.RENDER_RESPONSE);
-        phaseInfo.assertListener(PhaseId.APPLY_REQUEST_VALUES, "listener invoked");
+        super.testImmediate();
     }
 
     @Test
     public void testImmediateBypassUpdates() {
-        String reqTime = selenium.getText(time);
-
-        ajaxAttributes.set(AjaxAttributes.listener, "doubleStringListener");
-        ajaxAttributes.set(AjaxAttributes.bypassUpdates, true);
-        ajaxAttributes.set(AjaxAttributes.immediate, true);
-
-        guardXhr(selenium).click(input);
-        waitGui.failWith("Page was not updated").waitForChange(reqTime, retrieveText.locator(time));
-
-        assertEquals(selenium.getText(output1), "Ferrari", "Output should not change");
-        phaseInfo.assertPhases(PhaseId.RESTORE_VIEW, PhaseId.APPLY_REQUEST_VALUES, PhaseId.PROCESS_VALIDATIONS,
-            PhaseId.RENDER_RESPONSE);
-        phaseInfo.assertListener(PhaseId.APPLY_REQUEST_VALUES, "listener invoked");
+        super.testImmediateBypassUpdates();
     }
 
     @Test
     public void testLimitRender() {
-        ajaxAttributes.set(AjaxAttributes.limitRender, true);
-
-        String reqTime = selenium.getText(time);
-
-        guardXhr(selenium).click(input);
-        waitGui.failWith("Page was not updated").waitForChange("", retrieveText.locator(output1));
-
-        assertEquals(selenium.getText(time), reqTime, "Ajax-rendered a4j:outputPanel shouldn't change");
+        super.testLimitRender("Audi");
     }
 
     @Test
     public void testEvents() {
-        ajaxAttributes.set(AjaxAttributes.onbeforesubmit, "metamerEvents += \"beforesubmit \"");
-        ajaxAttributes.set(AjaxAttributes.onbegin, "metamerEvents += \"begin \"");
-        ajaxAttributes.set(AjaxAttributes.onbeforedomupdate, "metamerEvents += \"beforedomupdate \"");
-        ajaxAttributes.set(AjaxAttributes.oncomplete, "metamerEvents += \"complete \"");
-
-        selenium.getEval(new JavaScript("window.metamerEvents = \"\";"));
-
-        guardXhr(selenium).click(input);
-        waitGui.failWith("Page was not updated").waitForChange("", retrieveText.locator(output1));
-
-        String[] events = selenium.getEval(new JavaScript("window.metamerEvents")).split(" ");
-
-        assertEquals(events.length, 4, "4 events should be fired.");
-        assertEquals(events[0], "beforesubmit", "Attribute onbeforesubmit doesn't work");
-        assertEquals(events[1], "begin", "Attribute onbegin doesn't work");
-        assertEquals(events[2], "beforedomupdate", "Attribute onbeforedomupdate doesn't work");
-        assertEquals(events[3], "complete", "Attribute oncomplete doesn't work");
+        super.testEvents();
     }
 
     @Test
     public void testRender() {
-        ajaxAttributes.set(AjaxAttributes.render, "output1");
-
-        guardXhr(selenium).click(input);
-        String outputValue = waitGui.failWith("Page was not updated").waitForChangeAndReturn("", retrieveText.locator(output1));
-
-        assertEquals(outputValue, "Audi", "Wrong output1");
-        assertEquals(selenium.getText(output2), "Ferrari", "Wrong output2");
+        super.testRender();
     }
 
     @Test
     public void testStatus() {
-        ajaxAttributes.set(AjaxAttributes.status, "statusChecker");
+        super.testStatus();
+    }
 
-        String statusCheckerTime = selenium.getText(statusChecker);
-        guardXhr(selenium).click(input);
-        waitGui.failWith("Attribute status doesn't work").waitForChange(statusCheckerTime, retrieveText.locator(statusChecker));
+    @Override
+    public void performAction() {
+        Graphene.guardXhr(page.selectOneRadio).click();
+    }
+
+    @Override
+    public void assertOutput1Changed() {
+        assertEquals(page.output1.getText(), "Audi", "Output1 should change");
+    }
+
+    @Override
+    public void assertOutput1NotChanged() {
+        assertEquals(page.output1.getText(), "Ferrari", "Output1 should not change");
+    }
+
+    @Override
+    public void assertOutput2Changed() {
+        assertEquals(page.output2.getText(), "Audi", "Output2 should change");
+    }
+
+    @Override
+    public void assertOutput2NotChanged() {
+        assertEquals(page.output2.getText(), "Ferrari", "Output2 should not change");
     }
 }
