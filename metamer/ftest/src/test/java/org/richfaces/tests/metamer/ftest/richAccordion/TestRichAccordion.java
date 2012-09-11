d19d15a8fe97e1da92194f182afc93f1af63075a
Pavol Pitonak <ppitonak@redhat.com>
2012-09-11 12:06:57 +0200

    few tests for rich:accordion rewritten to use Selenium 2

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richAccordion/TestRichAccordion.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richAccordion/TestRichAccordion.java
@@ -29,11 +29,6 @@ import static org.jboss.arquillian.ajocado.Graphene.retrieveText;
 import static org.jboss.arquillian.ajocado.Graphene.waitGui;
 import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
 import static org.jboss.test.selenium.locator.utils.LocatorEscaping.jq;
-import static org.richfaces.tests.metamer.ftest.BasicAttributes.itemActiveHeaderClass;
-import static org.richfaces.tests.metamer.ftest.BasicAttributes.itemContentClass;
-import static org.richfaces.tests.metamer.ftest.BasicAttributes.itemDisabledHeaderClass;
-import static org.richfaces.tests.metamer.ftest.BasicAttributes.itemHeaderClass;
-import static org.richfaces.tests.metamer.ftest.BasicAttributes.itemInactiveHeaderClass;
 import static org.richfaces.tests.metamer.ftest.attributes.AttributeList.accordionAttributes;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
@@ -59,7 +54,7 @@ import org.testng.annotations.Test;
 /**
  * Test case for page /faces/components/richAccordion/simple.xhtml
  *
- * @author <a href="mailto:ppitonak@redhat.com">Pavol Pitonak</a>
+ * @author <a href="https://community.jboss.org/people/ppitonak">Pavol Pitonak</a>
  * @version $Revision: 22733 $
  */
 public class TestRichAccordion extends AbstractGrapheneTest {
@@ -69,106 +64,12 @@ public class TestRichAccordion extends AbstractGrapheneTest {
             pjq("div[id$=item3:header]"), pjq("div[id$=item4:header]"), pjq("div[id$=item5:header]") };
     private JQueryLocator[] itemContents = { pjq("div[id$=item1:content]"), pjq("div[id$=item2:content]"),
             pjq("div[id$=item3:content]"), pjq("div[id$=item4:content]"), pjq("div[id$=item5:content]") };
-    private JQueryLocator[] activeHeaders = { pjq("div[id$=item1:header] div.rf-ac-itm-lbl-act"),
-            pjq("div[id$=item2:header] div.rf-ac-itm-lbl-act"), pjq("div[id$=item3:header] div.rf-ac-itm-lbl-act"),
-            pjq("div[id$=item4:header] div.rf-ac-itm-lbl-act"), pjq("div[id$=item5:header] div.rf-ac-itm-lbl-act") };
-    private JQueryLocator[] inactiveHeaders = { pjq("div[id$=item1:header] div.rf-ac-itm-lbl-inact"),
-            pjq("div[id$=item2:header] div.rf-ac-itm-lbl-inact"), pjq("div[id$=item3:header] div.rf-ac-itm-lbl-inact"),
-            pjq("div[id$=item4:header] div.rf-ac-itm-lbl-inact"), pjq("div[id$=item5:header] div.rf-ac-itm-lbl-inact") };
-    private JQueryLocator[] disabledHeaders = { pjq("div[id$=item1:header] div.rf-ac-itm-lbl-dis"),
-            pjq("div[id$=item2:header] div.rf-ac-itm-lbl-dis"), pjq("div[id$=item3:header] div.rf-ac-itm-lbl-dis"),
-            pjq("div[id$=item4:header] div.rf-ac-itm-lbl-dis"), pjq("div[id$=item5:header] div.rf-ac-itm-lbl-dis") };
     private JQueryLocator leftIcon = pjq("div[id$=item{0}] td.rf-ac-itm-ico");
     private JQueryLocator rightIcon = pjq("div[id$=item{0}] td.rf-ac-itm-exp-ico");
 
     @Override
     public URL getTestUrl() {
         return buildUrl(contextPath, "faces/components/richAccordion/simple.xhtml");
-    }
-
-    @Test
-    public void testInit() {
-
-        boolean accordionDisplayed = selenium.isVisible(accordion);
-        assertTrue(accordionDisplayed, "Accordion is not present on the page.");
-
-        for (int i = 0; i < 5; i++) {
-            accordionDisplayed = selenium.isVisible(itemHeaders[i]);
-            assertTrue(accordionDisplayed, "Item" + (i + 1) + "'s header should be visible.");
-        }
-
-        accordionDisplayed = selenium.isVisible(itemContents[0]);
-        assertTrue(accordionDisplayed, "Content of item1 should be visible.");
-
-        for (int i = 1; i < 5; i++) {
-            accordionDisplayed = selenium.isVisible(itemContents[i]);
-            assertFalse(accordionDisplayed, "Item" + (i + 1) + "'s content should not be visible.");
-        }
-    }
-
-    @Test
-    public void testActiveItem() {
-        accordionAttributes.set(AccordionAttributes.activeItem, "item5");
-
-        boolean accordionDisplayed = selenium.isVisible(accordion);
-        assertTrue(accordionDisplayed, "Accordion is not present on the page.");
-
-        for (int i = 0; i < 5; i++) {
-            accordionDisplayed = selenium.isVisible(itemHeaders[i]);
-            assertTrue(accordionDisplayed, "Item" + (i + 1) + "'s header should be visible.");
-        }
-
-        accordionDisplayed = selenium.isVisible(itemContents[4]);
-        assertTrue(accordionDisplayed, "Content of item5 should be visible.");
-
-        for (int i = 0; i < 4; i++) {
-            accordionDisplayed = selenium.isVisible(itemContents[i]);
-            assertFalse(accordionDisplayed, "Item" + (i + 1) + "'s content should not be visible.");
-        }
-
-        accordionAttributes.set(AccordionAttributes.activeItem, "item4");
-
-        for (int i = 0; i < 5; i++) {
-            accordionDisplayed = selenium.isVisible(itemHeaders[i]);
-            assertTrue(accordionDisplayed, "Item" + (i + 1) + "'s header should be visible.");
-        }
-
-        accordionDisplayed = selenium.isVisible(itemContents[0]);
-        assertTrue(accordionDisplayed, "Content of item1 should be visible.");
-
-        for (int i = 1; i < 5; i++) {
-            accordionDisplayed = selenium.isVisible(itemContents[i]);
-            assertFalse(accordionDisplayed, "Item" + (i + 1) + "'s content should not be visible.");
-        }
-    }
-
-    @Test
-    public void testCycledSwitching() {
-        String accordionId = selenium.getEval(new JavaScript("window.testedComponentId"));
-        String result = null;
-
-        // RichFaces.$('form:accordion').nextItem('item4') will be null
-        result = selenium.getEval(new JavaScript("window.RichFaces.$('" + accordionId + "').nextItem('item4')"));
-        assertEquals(result, "null", "Result of function nextItem('item4')");
-
-        // RichFaces.$('form:accordion').prevItem('item1') will be null
-        result = selenium.getEval(new JavaScript("window.RichFaces.$('" + accordionId + "').prevItem('item1')"));
-        assertEquals(result, "null", "Result of function prevItem('item1')");
-
-        accordionAttributes.set(AccordionAttributes.cycledSwitching, true);
-
-        // RichFaces.$('form:accordion').nextItem('item5') will be item1
-        result = selenium.getEval(new JavaScript("window.RichFaces.$('" + accordionId + "').nextItem('item5')"));
-        assertEquals(result, "item1", "Result of function nextItem('item5')");
-
-        // RichFaces.$('form:accordion').prevItem('item1') will be item5
-        result = selenium.getEval(new JavaScript("window.RichFaces.$('" + accordionId + "').prevItem('item1')"));
-        assertEquals(result, "item5", "Result of function prevItem('item1')");
-    }
-
-    @Test
-    public void testDir() {
-        testDir(accordion);
     }
 
     @Test
@@ -206,11 +107,6 @@ public class TestRichAccordion extends AbstractGrapheneTest {
     }
 
     @Test
-    public void testItemActiveHeaderClass() {
-        testStyleClass(activeHeaders[0], itemActiveHeaderClass);
-    }
-
-    @Test
     @RegressionTest("https://issues.jboss.org/browse/RF-10352")
     public void testItemActiveLeftIcon() {
         JQueryLocator icon = leftIcon.format(1).getDescendant(jq("div.rf-ac-itm-ico-act"));
@@ -242,16 +138,6 @@ public class TestRichAccordion extends AbstractGrapheneTest {
     }
 
     @Test
-    public void testItemContentClass() {
-        testStyleClass(itemContents[2], itemContentClass);
-    }
-
-    @Test
-    public void testItemDisabledHeaderClass() {
-        testStyleClass(disabledHeaders[3], itemDisabledHeaderClass);
-    }
-
-    @Test
     public void testItemDisabledLeftIcon() {
         JQueryLocator icon = leftIcon.format(4).getDescendant(jq("div.rf-ac-itm-ico-inact"));
         JQueryLocator input = pjq("select[id$=itemDisabledLeftIconInput]");
@@ -267,16 +153,6 @@ public class TestRichAccordion extends AbstractGrapheneTest {
         JQueryLocator image = rightIcon.format(4).getChild(jq("img"));
 
         verifyStandardIcons(input, icon, image, "-dis");
-    }
-
-    @Test
-    public void testItemHeaderClass() {
-        testStyleClass(itemHeaders[2], itemHeaderClass);
-    }
-
-    @Test
-    public void testItemInactiveHeaderClass() {
-        testStyleClass(inactiveHeaders[1], itemInactiveHeaderClass);
     }
 
     @Test
@@ -296,11 +172,6 @@ public class TestRichAccordion extends AbstractGrapheneTest {
         JQueryLocator image = rightIcon.format(3).getChild(jq("img"));
 
         verifyStandardIcons(input, icon, image, "");
-    }
-
-    @Test
-    public void testLang() {
-        testLang(accordion);
     }
 
     @Test
@@ -378,23 +249,6 @@ public class TestRichAccordion extends AbstractGrapheneTest {
     }
 
     @Test
-    public void testRendered() {
-        accordionAttributes.set(AccordionAttributes.rendered, false);
-
-        assertFalse(selenium.isElementPresent(accordion), "Accordion should not be rendered when rendered=false.");
-    }
-
-    @Test
-    public void testStyle() {
-        testStyle(accordion);
-    }
-
-    @Test
-    public void testStyleClass() {
-        testStyleClass(accordion);
-    }
-
-    @Test
     public void testSwitchTypeNull() {
         for (int i = 2; i >= 0; i--) {
             final int index = i;
@@ -431,11 +285,6 @@ public class TestRichAccordion extends AbstractGrapheneTest {
             guardHttp(selenium).click(itemHeaders[index]);
             waitGui.failWith("Item " + index + " is not displayed.").until(elementVisible.locator(itemContents[index]));
         }
-    }
-
-    @Test
-    public void testTitle() {
-        testTitle(accordion);
     }
 
     @Test
