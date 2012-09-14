cc82d65693308785056f11a2ac5674d55899c086
Pavol Pitonak <ppitonak@redhat.com>
2012-09-14 09:06:44 +0200

    tests for a4j:log rewritten to Selenium 2

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/a4jLog/TestA4JLog.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/a4jLog/TestA4JLog.java
@@ -21,100 +21,89 @@
  *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.a4jLog;
 
-import static org.richfaces.tests.metamer.ftest.attributes.AttributeList.logAttributes;
-import static org.jboss.arquillian.ajocado.Graphene.textEquals;
-import static org.jboss.arquillian.ajocado.Graphene.waitGui;
-
+import static org.richfaces.tests.metamer.ftest.webdriver.AttributeList.logAttributes;
 import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
-
-import static org.jboss.test.selenium.locator.utils.LocatorEscaping.jq;
-
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 import java.net.URL;
 
-import org.jboss.arquillian.ajocado.locator.JQueryLocator;
-import org.richfaces.tests.metamer.ftest.AbstractGrapheneTest;
+import org.jboss.arquillian.graphene.Graphene;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
 import org.testng.annotations.Test;
 
 /**
  * Test case for page /faces/components/a4jLog/simple.xhtml
  *
- * @author <a href="mailto:ppitonak@redhat.com">Pavol Pitonak</a>
- * @version $Revision: 22499 $
+ * @author <a href="https://community.jboss.org/people/ppitonak">Pavol Pitonak</a>
+ * @since 4.3.0.M2
  */
-public class TestA4JLog extends AbstractGrapheneTest {
+public class TestA4JLog extends AbstractWebDriverTest<LogPage> {
 
     /**
      * Enumeration representing all possible levels for a4j:log.
      */
     public enum LogLevel {
-
-        DEBUG,
-        NULL,
-        INFO,
-        WARN,
-        ERROR;
+        DEBUG, NULL, INFO, WARN, ERROR;
     }
-
-    private JQueryLocator input = pjq("input[id$=nameInput]");
-    private JQueryLocator submitButton = pjq("input[id$=submitButton]");
-    private JQueryLocator output = pjq("span[id$=out]");
-    private JQueryLocator log = pjq("div.rf-log");
-    private JQueryLocator levelSelect = pjq("div.rf-log select");
-    private JQueryLocator logMsg = pjq("div.rf-log div.rf-log-contents div");
 
     @Override
     public URL getTestUrl() {
         return buildUrl(contextPath, "faces/components/a4jLog/simple.xhtml");
     }
 
+    @Override
+    protected LogPage createPage() {
+        return new LogPage();
+    }
+
     @Test
     public void testSubmit() {
-        selenium.typeKeys(input, "RichFaces 4");
-        selenium.click(submitButton);
+        page.input.clear();
+        page.input.sendKeys("RichFaces 4");
+        page.submitButton.click();
 
-        waitGui.until(textEquals.locator(output).text("Hello RichFaces 4!"));
+        Graphene.waitGui().until(Graphene.element(page.output).textEquals("Hello RichFaces 4!"));
 
-        int count = selenium.getCount(logMsg);
-        assertTrue(count > 0, "There should be at least one message in log after submit button was clicked.");
+        assertTrue(page.logMsg.size() > 0,
+            "There should be at least one message in log after submit button was clicked.");
     }
 
     @Test
     public void testSubmitUnicode() {
-        selenium.typeKeys(input, "ľščťžýáíéôúäň");
-        selenium.click(submitButton);
+        page.input.clear();
+        page.input.sendKeys("ľščťžýáíéôúäň");
+        page.submitButton.click();
 
-        waitGui.until(textEquals.locator(output).text("Hello ľščťžýáíéôúäň!"));
+        Graphene.waitGui().until(Graphene.element(page.output).textEquals("Hello ľščťžýáíéôúäň!"));
 
-        int count = selenium.getCount(logMsg);
-        assertTrue(count > 0, "There should be at least one message in log after submit button was clicked.");
+        assertTrue(page.logMsg.size() > 0,
+            "There should be at least one message in log after submit button was clicked.");
     }
 
     @Test
     public void testClearButton() {
-        JQueryLocator clearButton = pjq("div.rf-log button");
-
-        selenium.typeKeys(input, "RichFaces 4");
-        selenium.click(submitButton);
-
-        waitGui.until(textEquals.locator(output).text("Hello RichFaces 4!"));
-
-        int count = selenium.getCount(logMsg);
-        assertTrue(count > 0, "There should be at least one message in log after submit button was clicked.");
+        testSubmit();
 
         // test clear
-        selenium.click(clearButton);
-        count = selenium.getCount(logMsg);
-        assertEquals(count, 0, "There should be no messages in log after clear button was clicked.");
+        page.clearButton.click();
+        assertEquals(page.logMsg.size(), 0, "There should be no messages in log after clear button was clicked.");
     }
 
     @Test
     public void testRendered() {
         logAttributes.set(LogAttributes.rendered, false);
-        assertFalse(selenium.isElementPresent(log), "Log should not be displayed.");
+        try {
+            page.log.isDisplayed();
+            fail("Log should not be rendered when rendered=false.");
+        } catch (NoSuchElementException ex) {
+            // expected
+        }
     }
 
     @Test
@@ -218,34 +207,46 @@ public class TestA4JLog extends AbstractGrapheneTest {
     }
 
     private void testLogging(LogLevel logLevel, LogLevel filterLevel) {
-        JQueryLocator logButton = pjq("input[id$=" + logLevel.toString().toLowerCase() + "Button]");
-        JQueryLocator msgType = logMsg.getChild(jq("span.rf-log-entry-lbl"));
-        JQueryLocator msgContent = logMsg.getChild(jq("span.rf-log-entry-msg"));
+        WebElement logButton = driver.findElement(By.cssSelector("input[id$=" + logLevel.toString().toLowerCase()
+            + "Button]"));
 
         if (filterLevel != LogLevel.DEBUG) {
             logAttributes.set(LogAttributes.level, filterLevel.toString().toLowerCase());
         }
 
-        String selectedLevel = selenium.getSelectedLabel(levelSelect);
+        String selectedLevel = (new Select(page.levelSelect)).getFirstSelectedOption().getText();
         if (filterLevel == LogLevel.NULL) {
             assertEquals(selectedLevel, "info", "Log level in select wasn't changed.");
         } else {
             assertEquals(selectedLevel, filterLevel.toString().toLowerCase(), "Log level in select wasn't changed.");
         }
 
-        selenium.typeKeys(input, logLevel.toString());
-        selenium.click(logButton);
+        page.input.clear();
+        page.input.sendKeys(logLevel.toString());
+        switch (logLevel) {
+            case DEBUG:
+                page.debugButton.click();
+                break;
+            case INFO:
+                page.infoButton.click();
+                break;
+            case WARN:
+                page.warnButton.click();
+                break;
+            case ERROR:
+                page.errorButton.click();
+                break;
+        }
 
-        int count = selenium.getCount(logMsg);
-        assertEquals(count, filterLevel.ordinal() <= logLevel.ordinal() ? 1 : 0, "There should be only one message in log.");
+        assertEquals(page.logMsg.size(), filterLevel.ordinal() <= logLevel.ordinal() ? 1 : 0,
+            "There should be only one message in log.");
 
-        if (count == 0) {
+        if (page.logMsg.size() == 0) {
             return;
         }
 
-        String loggedValue = selenium.getText(msgType).replaceAll(" *\\[.*\\]:$", "");
+        String loggedValue = page.msgType.get(0).getText().replaceAll(" *\\[.*\\]:$", "");
         assertEquals(loggedValue, logLevel.toString().toLowerCase(), "Message type in log.");
-        loggedValue = selenium.getText(msgContent);
-        assertEquals(loggedValue, logLevel.toString(), "Message content.");
+        assertEquals(page.msgContent.get(0).getText(), logLevel.toString(), "Message content.");
     }
 }
