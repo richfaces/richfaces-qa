3042a91bbb1754a9ffebe1ec772b1d71cc11105f
Pavol Pitonak <ppitonak@redhat.com>
2012-09-26 15:57:45 +0200

    group 4.Future removed from fixed tests

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richInplaceInput/TestRichInplaceInput.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richInplaceInput/TestRichInplaceInput.java
@@ -1,25 +1,24 @@
-/**
- * *****************************************************************************
- * JBoss, Home of Professional Open Source Copyright 2010-2012, Red Hat, Inc.
- * and individual contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2012, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
- * *****************************************************************************
- */
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.richInplaceInput;
 
 import static org.jboss.arquillian.ajocado.Graphene.guardNoRequest;
@@ -36,7 +35,9 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.net.URL;
+
 import javax.faces.event.PhaseId;
+
 import org.jboss.arquillian.ajocado.css.CssProperty;
 import org.jboss.arquillian.ajocado.dom.Attribute;
 import org.jboss.arquillian.ajocado.dom.Event;
@@ -46,13 +47,12 @@ import org.jboss.test.selenium.waiting.EventFiredCondition;
 import org.richfaces.tests.metamer.ftest.AbstractGrapheneTest;
 import org.richfaces.tests.metamer.ftest.annotations.IssueTracking;
 import org.richfaces.tests.metamer.ftest.annotations.RegressionTest;
-import org.richfaces.tests.metamer.ftest.annotations.Templates;
 import org.testng.annotations.Test;
 
 /**
  * Test case for page faces/components/richInplaceInput/simple.xhtml.
  *
- * @author <a href="mailto:ppitonak@redhat.com">Pavol Pitonak</a>
+ * @author <a href="https://community.jboss.org/people/ppitonak">Pavol Pitonak</a>
  * @version $Revision: 22788 $
  */
 public class TestRichInplaceInput extends AbstractGrapheneTest {
@@ -425,20 +425,8 @@ public class TestRichInplaceInput extends AbstractGrapheneTest {
     }
 
     @Test
-    @Templates(exclude = {"richPopupPanel"})
+    @RegressionTest("https://issues.jboss.org/browse/RF-10980")
     public void testTabindex() {
-        AttributeLocator<?> attr = input.getAttribute(new Attribute("tabindex"));
-
-        selenium.type(pjq("input[id$=tabindexInput]"), "47");
-        selenium.waitForPageToLoad();
-
-        assertTrue(selenium.getAttribute(attr).contains("47"), "Attribute tabindex should contain \"47\".");
-    }
-
-    @Test(groups = {"4.Future"})
-    @IssueTracking("https://issues.jboss.org/browse/RF-10980")
-    @Templates(value = {"richPopupPanel"})
-    public void testTabindexInPopupPanel() {
         AttributeLocator<?> attr = input.getAttribute(new Attribute("tabindex"));
 
         selenium.type(pjq("input[id$=tabindexInput]"), "47");
--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richInputNumberSpinner/TestRichSpinner.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richInputNumberSpinner/TestRichSpinner.java
@@ -32,7 +32,9 @@ import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 import java.net.URL;
+
 import javax.faces.event.PhaseId;
+
 import org.jboss.arquillian.ajocado.dom.Attribute;
 import org.jboss.arquillian.ajocado.dom.Event;
 import org.jboss.arquillian.ajocado.javascript.JavaScript;
@@ -47,7 +49,7 @@ import org.testng.annotations.Test;
 /**
  * Test case for page /faces/components/richInputNumberSpinner/simple.xhtml
  *
- * @author <a href="mailto:ppitonak@redhat.com">Pavol Pitonak</a>
+ * @author <a href="https://community.jboss.org/people/ppitonak">Pavol Pitonak</a>
  * @version $Revision: 22789 $
  */
 public class TestRichSpinner extends AbstractSpinnerTest {
@@ -463,14 +465,8 @@ public class TestRichSpinner extends AbstractSpinnerTest {
     }
 
     @Test
-    @Templates(exclude = { "richPopupPanel" })
+    @RegressionTest("https://issues.jboss.org/browse/RF-10980")
     public void testTabindex() {
-        testHtmlAttribute(input, "tabindex", "57");
-    }
-
-    @Test(groups = { "4.Future" })
-    @Templates(value = { "richPopupPanel" })
-    public void testTabindexInPopupPanel() {
         testHtmlAttribute(input, "tabindex", "57");
     }
 
