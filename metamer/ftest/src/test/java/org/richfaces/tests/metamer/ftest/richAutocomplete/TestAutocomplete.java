14076565d09e299e989b9dcfa60ab5f01c134763
Jan Papoušek <jan.papousek@gmail.com>
2012-10-15 13:23:14 +0200

    license headers

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richAutocomplete/TestAutocomplete.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richAutocomplete/TestAutocomplete.java
@@ -1,30 +1,47 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2012, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.richAutocomplete;
 
 import java.net.URL;
 import java.util.List;
-import org.jboss.arquillian.graphene.component.object.api.scrolling.ScrollingType;
-import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.Test;
 import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
 import org.jboss.arquillian.graphene.component.object.api.autocomplete.ClearType;
 import org.jboss.arquillian.graphene.component.object.api.autocomplete.Suggestion;
+import org.jboss.arquillian.graphene.component.object.api.scrolling.ScrollingType;
+import org.openqa.selenium.support.FindBy;
 import org.richfaces.tests.metamer.bean.Model;
 import org.richfaces.tests.metamer.ftest.annotations.Inject;
 import org.richfaces.tests.metamer.ftest.annotations.IssueTracking;
 import org.richfaces.tests.metamer.ftest.annotations.Use;
-import org.testng.annotations.BeforeMethod;
+import static org.richfaces.tests.metamer.ftest.webdriver.AttributeList.autocompleteAttributes;
+import org.richfaces.tests.metamer.model.Capital;
+import org.richfaces.tests.page.fragments.impl.autocomplete.AutocompleteComponentImpl;
+import org.richfaces.tests.page.fragments.impl.autocomplete.SuggestionImpl;
+import org.richfaces.tests.page.fragments.impl.autocomplete.TextSuggestionParser;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
-import org.richfaces.tests.metamer.model.Capital;
-import org.richfaces.tests.page.fragments.impl.autocomplete.AutocompleteComponentImpl;
-import org.richfaces.tests.page.fragments.impl.autocomplete.TextSuggestionParser;
-import static org.richfaces.tests.metamer.ftest.webdriver.AttributeList.autocompleteAttributes;
-import org.richfaces.tests.page.fragments.impl.autocomplete.SuggestionImpl;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 /**
  * @author <a href="mailto:jpapouse@redhat.com">Jan Papousek</a>
