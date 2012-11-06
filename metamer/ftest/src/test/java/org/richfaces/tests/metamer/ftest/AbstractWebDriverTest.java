13d1586be4d029179cfe3782ad0eb52dc9c1d882
Pavol Pitonak <ppitonak@redhat.com>
2012-11-06 16:52:08 +0100

    PhaseInfoWD removed, code moved to MetamerPage

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/AbstractWebDriverTest.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/AbstractWebDriverTest.java
@@ -67,7 +67,6 @@ public abstract class AbstractWebDriverTest<P extends MetamerPage> extends Abstr
     protected static final int TRIES = 20;//for guardListSize and expectedReturnJS
     private FieldDecorator fieldDecorator;
     protected DriverType driverType;
-    protected PhaseInfoWD phaseInfo = new PhaseInfoWD();
 
     public enum DriverType {
 
