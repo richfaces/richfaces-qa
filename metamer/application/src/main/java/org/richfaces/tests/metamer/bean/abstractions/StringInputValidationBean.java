e15c66e624314eae15312e40284e01964a8f4baa
Jiri Stefek <jstefek@redhat.com>
2012-11-20 15:12:00 +0100

    Metamer: some beans for input validation refactored

---
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/abstractions/StringInputValidationBean.java
@@ -0,0 +1,57 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.tests.metamer.bean.abstractions;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotEmpty;
+
+/**
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
+ */
+public interface StringInputValidationBean extends AttributesHolder {
+
+    String NOT_EMPTY_VALIDATION_MSG = "may not be empty";
+    String NOT_EMPTY_VALIDATION_MSG2 = "may not be null";
+    String NOT_EMPTY_VALIDATION_MSG3 = "size must be between 1 and 2147483647";
+    String REGEXP_VALIDATION_MSG = "must match \"[a-z].*\"";
+    String STRING_SIZE_VALIDATION_MSG = "size must be between 3 and 6";
+
+    String getCustomValue();
+
+    @NotEmpty(message = NOT_EMPTY_VALIDATION_MSG)
+    String getNotEmptyValue();
+
+    @Pattern(regexp = "[a-z].*", message = REGEXP_VALIDATION_MSG)
+    String getPatternValue();
+
+    @Size(min = 3, max = 6, message = STRING_SIZE_VALIDATION_MSG)
+    String getSizeValue();
+
+    void setCustomValue(String customValue);
+
+    void setNotEmptyValue(String notEmptyValue);
+
+    void setPatternValue(String patternValue);
+
+    void setSizeValue(String sizeValue);
+}
