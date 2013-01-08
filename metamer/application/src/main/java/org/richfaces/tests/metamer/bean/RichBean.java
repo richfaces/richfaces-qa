a819b3ba2f14accd9ce91450e6d28ae62669a946
Pavol Pitonak <ppitonak@redhat.com>
2013-01-08 14:31:22 +0100

    value change listener used in editor sample refactored

--- metamer/application/src/main/java/org/richfaces/tests/metamer/bean/RichBean.java
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/RichBean.java
@@ -41,7 +41,6 @@ import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.lang.StringEscapeUtils;
 import org.richfaces.event.ItemChangeEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -76,9 +75,7 @@ public class RichBean implements Serializable {
 
     public enum Skinning {
 
-        NONE,
-        SKINNING,
-        SKINNING_CLASSES
+        NONE, SKINNING, SKINNING_CLASSES
     }
 
     @PostConstruct
@@ -214,7 +211,8 @@ public class RichBean implements Serializable {
     /**
      * Setter for user's skin.
      *
-     * @param skin a RichFaces skin
+     * @param skin
+     *            a RichFaces skin
      */
     public void setSkin(String skin) {
         this.skin = skin;
@@ -373,7 +371,8 @@ public class RichBean implements Serializable {
      * Action that causes an error. Suitable for testing 'onerror' attribute.
      *
      * @return method never returns any value
-     * @throws FacesException thrown always
+     * @throws FacesException
+     *             thrown always
      */
     public String causeError() {
         throw new FacesException("Ajax request caused an error. This is intentional behavior.");
@@ -411,7 +410,8 @@ public class RichBean implements Serializable {
     /**
      * An action listener that does nothing.
      *
-     * @param event an event representing the activation of a user interface component (not used)
+     * @param event
+     *            an event representing the activation of a user interface component (not used)
      */
     public void dummyActionListener(ActionEvent event) {
         logToPage("* action listener invoked");
@@ -420,7 +420,8 @@ public class RichBean implements Serializable {
     /**
      * An action listener that does nothing.
      *
-     * @param event an event representing the activation of a user interface component (not used)
+     * @param event
+     *            an event representing the activation of a user interface component (not used)
      */
     public void actionListener(AjaxBehaviorEvent event) {
         logToPage("* action listener invoked");
@@ -429,7 +430,8 @@ public class RichBean implements Serializable {
     /**
      * An action listener that does nothing.
      *
-     * @param event an event representing the activation of a user interface component (not used)
+     * @param event
+     *            an event representing the activation of a user interface component (not used)
      */
     public void actionListener2(AjaxBehaviorEvent event) {
         logToPage("* action listener *2 invoked");
@@ -438,7 +440,8 @@ public class RichBean implements Serializable {
     /**
      * An item change listener that logs to the page old and new value.
      *
-     * @param event an event representing the activation of a user interface component
+     * @param event
+     *            an event representing the activation of a user interface component
      */
     public void itemChangeListener(ItemChangeEvent event) {
         logToPage("* item changed: " + (event.getOldItem() == null ? null : event.getOldItem().getId()) + " -> "
@@ -448,7 +451,8 @@ public class RichBean implements Serializable {
     /**
      * A value change listener that logs to the page old and new value.
      *
-     * @param event an event representing the activation of a user interface component
+     * @param event
+     *            an event representing the activation of a user interface component
      */
     public void valueChangeListener(ValueChangeEvent event) {
         logToPage("*1 value changed: " + event.getOldValue() + " -> " + event.getNewValue());
@@ -458,28 +462,35 @@ public class RichBean implements Serializable {
      * A change event listener that logs to the page old and new value. This is 2nd ValueChacgeListener. Use 2 different
      * listeners is useful when testing more than one listener definition for one component
      *
-     * @param event an event representing the activation of a user interface component
+     * @param event
+     *            an event representing the activation of a user interface component
      */
     public void changeEventListener(AjaxBehaviorEvent event) {
         logToPage("*2 value changed ");
     }
 
     /**
-     * A value change listener that logs to the page old and new value. But if event value was longer that 20 chars, only first
-     * 20 chars will be logged
+     * A value change listener that logs to the page old and new value. But if event value was longer that 20 chars,
+     * only first 20 chars will be logged
      *
-     * @param event an event representing the activation of a user interface component
+     * @param event
+     *            an event representing the activation of a user interface component
      */
     public void valueChangeListenerImproved(ValueChangeEvent event) {
         String oldVal = event.getOldValue() != null ? event.getOldValue().toString() : null;
         String newVal = event.getNewValue() != null ? event.getNewValue().toString() : null;
 
         // need to escape these values before logging to the page as it can contain e.g. \r \n
-        oldVal = StringEscapeUtils.escapeJavaScript(oldVal);
-        newVal = StringEscapeUtils.escapeJavaScript(newVal);
+        if (oldVal != null) {
+            oldVal = oldVal.replace("\r", "").replace("\n", "");
+        }
+        if (newVal != null) {
+            newVal = newVal.replace("\r", "").replace("\n", "");
+        }
 
         logToPage("*3 value changed: "
-            + (oldVal != null && oldVal.length() > 21 ? oldVal.substring(0, 20) : oldVal != null ? oldVal : "null") + " -> "
+            + (oldVal != null && oldVal.length() > 21 ? oldVal.substring(0, 20) : oldVal != null ? oldVal : "null")
+            + " -> "
             + (newVal != null && newVal.length() > 21 ? newVal.substring(0, 20) : newVal != null ? newVal : "null"));
     }
 
