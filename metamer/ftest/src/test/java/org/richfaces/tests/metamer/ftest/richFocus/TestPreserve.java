05689d2a64fd7b60a3f97d53634dc6bdfc57c08f
Pavol Pitonak <ppitonak@redhat.com>
2013-03-06 12:35:54 +0100

    added license header

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richFocus/TestPreserve.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richFocus/TestPreserve.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2013, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.richFocus;
 
 import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
@@ -12,6 +33,11 @@ import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
 import org.richfaces.tests.metamer.ftest.webdriver.AttributeList;
 import org.testng.annotations.Test;
 
+/**
+ * @author <a href="mailto:jhuska@redhat.com">Juraj Huska</a>
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
+ * @since 4.3.0.Final
+ */
 public class TestPreserve extends AbstractWebDriverTest {
 
     @Page
