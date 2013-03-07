0bf4881d34f66f1afe54ff90e713fb8d183d9612
Jan Papoušek <jan.papousek@gmail.com>
2013-03-07 08:53:59 +0100

    improved maven detection in archetype scripts

--- archetypes/apps/config.sh
+++ archetypes/apps/config.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-
-if [ -f "/qa/tools/opt/apache-maven-3.0.3/bin/mvn" ]; then
-    MAVEN="/qa/tools/opt/apache-maven-3.0.3/bin/mvn"
-else
-    MAVEN="mvn"
+if [ "$MAVEN"  == "" ]; then
+	if [ -f "/qa/tools/opt/apache-maven-3.0.4/bin/mvn" ]; then
+		MAVEN="/qa/tools/opt/apache-maven-3.0.4/bin/mvn"
+	else
+		MAVEN="mvn"
+	fi
 fi
+
+echo "--------------------------------------------------------------------------------";
+echo "Maven binary: ${MAVEN}" 
+echo "Maven version:"
+${MAVEN} -v;
+echo "--------------------------------------------------------------------------------";
 
 MAVEN_ARGS="--show-version -U"
