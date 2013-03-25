64a154202281e2bcf637b5acbd1dab90d315fa38
Ján Jamrich <jjamrich@redhat.com>
2013-03-25 13:05:24 +0100

    RFPL-2725: Metamer: rewrite rich:tooltip using webdriver

---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richTooltip/TooltipPage.java
@@ -0,0 +1,53 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.tests.metamer.ftest.richTooltip;
+
+import org.jboss.arquillian.graphene.enricher.findby.FindBy;
+import org.openqa.selenium.WebElement;
+import org.richfaces.tests.metamer.ftest.webdriver.MetamerPage;
+import org.richfaces.tests.page.fragments.impl.tooltip.RichFacesTooltip;
+
+/**
+ * @author <a href="jjamrich@redhat.com">Jan Jamrich</a>
+ *
+ */
+public class TooltipPage extends MetamerPage {
+
+    @FindBy(css = "div[id$=panel]")
+    public WebElement panel;
+
+    @FindBy(css = "div[id$=regular-div]")
+    public WebElement panel1;
+
+    @FindBy(css = "div[id$=jsf-div]")
+    public WebElement panel2;
+
+    @FindBy(css = "div[id$=panel_body]")
+    public WebElement panel3;
+
+    @FindBy(css = "span.rf-tt")
+    public RichFacesTooltip tooltip;
+
+    @FindBy(css = "table[id$='attributes:attributes']")
+    public WebElement attributesTable;
+
+}
