6b191188430a24fb39e3d55fba7dad6a0e50b5d2
Jiri Stefek <jstefek@redhat.com>
2013-06-19 12:44:18 +0200

    metamer: push+CDI tests (RFPL-1684)

--- metamer/application/src/main/java/org/richfaces/tests/metamer/bean/a4j/A4JPushBean.java
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/a4j/A4JPushBean.java
@@ -25,13 +25,18 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.annotation.PostConstruct;
+import javax.enterprise.event.Event;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import org.richfaces.application.push.MessageException;
 import org.richfaces.application.push.TopicKey;
 
 import org.richfaces.application.push.TopicsContext;
+import org.richfaces.cdi.push.Push;
 import org.richfaces.component.UIPush;
+import javax.inject.Inject;
+
+import org.joda.time.DateTime;
 import org.richfaces.tests.metamer.Attributes;
 import org.richfaces.tests.metamer.Message;
 import org.slf4j.Logger;
@@ -50,32 +55,35 @@ import static org.richfaces.demo.push.TopicsContextMessageProducer.PUSH_TOPICS_C
 public class A4JPushBean implements Serializable {
 
     private static final long serialVersionUID = 4810889475400649809L;
-    public static final String METAMER_SUBTOPIC = "xxx";
-
-    private static Logger logger;
+    public static final String DATE_PATTERN = "'day:' d', month:' M', time:' HH:mm:ss";
+    public static final String METAMER_SUBTOPIC = "";
+    public static final String METAMER_TOPIC_CDI = "topic2";
+    private static final Logger LOGGER = LoggerFactory.getLogger(A4JPushBean.class);
     private transient TopicsContext topicsContext;
     private Attributes attributes;
     private String username;
     private String message;
+
+    @Inject
+    @Push(topic = "sampleAddress1")
+    private Event<Date> messageProducerForAddress1;
+    @Inject
+    @Push(topic = "sampleAddress2")
+    private Event<Date> messageProducerForAddress2;
 
     /**
      * Initializes the managed bean.
      */
     @PostConstruct
     public void init() {
-        logger = LoggerFactory.getLogger(getClass());
-        logger.debug("initializing bean " + getClass().getName());
-
+        LOGGER.debug("initializing bean " + getClass().getName());
         // set up messaging
         // TODO JJa 2011-12-07: make sure if initialization is required even use initialization mechanism from showcase
-
         topicsContext = TopicsContext.lookup();
 
         attributes = Attributes.getComponentAttributesFromClass(UIPush.class, getClass());
-
         attributes.setAttribute("onerror", "alert('Error: ' + event.rf.data)");
         attributes.setAttribute("rendered", true);
-        // attributes.setAttribute("onsubscribed", "alert(this.id + ' subscribed')");
         attributes.remove("address");
         attributes.remove("ondataavailable");
     }
@@ -84,34 +92,43 @@ public class A4JPushBean implements Serializable {
      * just new date instance (current date)
      * @return
      */
-    public Date getDate() {
-        return new Date();
+    public String getDate() {
+        return new DateTime().toString(DATE_PATTERN);
+    }
+
+    public void pushCDI1() {
+        messageProducerForAddress1.fire(new Date());
+        LOGGER.debug("cdi push event 1");
+    }
+
+    public void pushCDI2() {
+        messageProducerForAddress2.fire(new Date());
+        LOGGER.debug("cdi push event 2");
     }
 
     /**
      * Trigger to start push topic
      * @throws MessageException
      */
-    public void push() throws MessageException {
+    public void pushJMS1() throws MessageException {
         TopicKey topicKey = new TopicKey("sampleAddress");
         TopicsContext topicsContext = TopicsContext.lookup();
 
-        topicsContext.publish(topicKey, new Date().toString());
-
-        System.out.println("push event");
+        topicsContext.publish(topicKey, new DateTime().toString(DATE_PATTERN));
+        LOGGER.debug("push event 1");
     }
 
     /**
      * Trigger to start push with another topic
      * @throws MessageException
      */
-    public void push2() throws MessageException {
+    public void pushJMS2() throws MessageException {
         TopicKey topicKey = new TopicKey("sampleAddress2");
         TopicsContext topicsContext = TopicsContext.lookup();
 
-        topicsContext.publish(topicKey, new Date().toString());
+        topicsContext.publish(topicKey, new DateTime().toString(DATE_PATTERN));
 
-        System.out.println("push event 2");
+        LOGGER.debug("push event 2");
     }
 
     /**
@@ -162,11 +179,11 @@ public class A4JPushBean implements Serializable {
 
     public void send() {
         try {
-            logger.info("sending message \"" + message + "\" by user " + username);
+            LOGGER.debug("sending message \"" + message + "\" by user " + username);
             Message msg = new Message(message, username, new Date().toString());
             getTopicsContext().publish(new TopicKey(PUSH_TOPICS_CONTEXT_TOPIC, METAMER_SUBTOPIC), msg);
         } catch (MessageException messageException) {
-            logger.error("Could not send message \"" + message + "\" by user " + username + ".", messageException);
+            LOGGER.error("Could not send message \"" + message + "\" by user " + username + ".", messageException);
         }
     }
 }
