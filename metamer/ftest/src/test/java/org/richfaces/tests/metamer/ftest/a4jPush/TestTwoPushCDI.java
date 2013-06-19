f13d9d5032c3e72eff93ed76de148d357bae3786
Jiri Stefek <jstefek@redhat.com>
2013-06-19 12:44:18 +0200

    metamer: push+CDI tests (RFPL-1684)

---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/a4jPush/TestTwoPushCDI.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2013, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
+package org.richfaces.tests.metamer.ftest.a4jPush;
+
+import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
+
+import java.net.URL;
+
+import org.testng.annotations.Test;
+
+public class TestTwoPushCDI extends TestTwoPush {
+
+    @Override
+    public URL getTestUrl() {
+        return buildUrl(contextPath, "faces/components/a4jPush/twoPushCDI.xhtml");
+    }
+
+    @Test
+    public void testBothPushes() {
+        super.testBothPushes();
+    }
+
+    @Test
+    public void testOnSubscribed() {
+        super.testOnSubscribed();
+    }
+
+    @Test
+    public void testPushEnable() {
+        super.testPushEnable();
+    }
+
+    @Test
+    public void testSimplePushEventReceive() {
+        super.testSimplePushEventReceive();
+    }
+}
