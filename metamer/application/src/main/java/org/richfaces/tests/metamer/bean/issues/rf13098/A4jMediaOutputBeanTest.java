f0886bc721238dd63e93c5f38a892a0606953ae8
manovotn <manovotn@dhcp129-149.brq.redhat.com>
2013-08-09 15:59:05 +0200

    RF-13098 Issue verification

---
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/issues/rf13098/A4jMediaOutputBeanTest.java
@@ -0,0 +1,112 @@
+package org.richfaces.tests.metamer.bean.issues.rf13098;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.imageio.ImageIO;
+
+import org.richfaces.tests.metamer.Attributes;
+import org.richfaces.ui.output.mediaOutput.UIMediaOutput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ManagedBean(name = "a4jMediaOutputBeanTest")
+@RequestScoped
+public class A4jMediaOutputBeanTest implements Serializable {
+    public static final String CSS_TEXT = "#mediaOutputMarked { color: red }";
+    public static final String HTML_TEXT = "<span id='htmlText' style='color: red'>SOME RED HTML TEXT</span>";
+    public static final String JAVASCRIPT_TEXT = "document.write('<span id=mediaOutputJavascriptText>JAVASCRIPT TEXT</span>')";
+    public static final String PLAIN_TEXT = "SOME PLAIN TEXT";
+    private static final long serialVersionUID = -1L;
+    private static Logger logger;
+    private Attributes attributes;
+
+    /**
+     * Initializes the managed bean.
+     */
+    @PostConstruct
+    public void init() {
+        logger = LoggerFactory.getLogger(getClass());
+        logger.debug("initializing bean " + getClass().getName());
+
+        attributes = Attributes.getComponentAttributesFromFacesConfig(UIMediaOutput.class, getClass());
+
+        attributes.setAttribute("session", true);
+        attributes.setAttribute("rendered", true);
+        attributes.remove("createContent");
+        attributes.remove("element");
+        attributes.remove("value");
+        attributes.remove("mimeType");
+        attributes.remove("uriAttribute");
+    }
+
+    /**
+     * Getter for attributes.
+     *
+     * @return A map containing all attributes of tested component. Name of the component is key in the map.
+     */
+    public Attributes getAttributes() {
+        return attributes;
+    }
+
+    /**
+     * Setter for attributes.
+     *
+     * @param attributes map containing all attributes of tested component. Name of the component is key in the map.
+     */
+    public void setAttributes(Attributes attributes) {
+        this.attributes = attributes;
+    }
+
+    public void createContentImagePng(OutputStream out, Object data) throws IOException {
+        if (data instanceof RF13098) {
+            RF13098 paintData = (RF13098) data;
+            BufferedImage img = new BufferedImage(paintData.getWidth(), paintData.getHeight(), BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = img.createGraphics();
+            graphics2D.clearRect(0, 0, paintData.getWidth(), paintData.getHeight());
+
+            graphics2D.setColor(Color.YELLOW);
+            graphics2D.fillRect(0, 0, paintData.getWidth() / 2, paintData.getHeight() / 2);
+
+            graphics2D.setColor(Color.RED);
+            graphics2D.fillRect(paintData.getWidth() / 2, 0, paintData.getWidth() / 2, paintData.getHeight() / 2);
+
+            graphics2D.setColor(Color.BLUE);
+            graphics2D.fillRect(0, paintData.getHeight() / 2, paintData.getWidth() / 2, paintData.getHeight() / 2);
+
+            graphics2D.setColor(Color.GREEN);
+            graphics2D.fillRect(paintData.getWidth() / 2, paintData.getHeight() / 2, paintData.getWidth() / 2,
+                paintData.getHeight() / 2);
+
+            ImageIO.write(img, "png", out);
+        }
+        if (data instanceof RF13098SerializableOnly) {
+            RF13098SerializableOnly paintData = (RF13098SerializableOnly) data;
+            BufferedImage img = new BufferedImage(paintData.getWidth(), paintData.getHeight(), BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = img.createGraphics();
+            graphics2D.clearRect(0, 0, paintData.getWidth(), paintData.getHeight());
+
+            graphics2D.setColor(Color.YELLOW);
+            graphics2D.fillRect(0, 0, paintData.getWidth() / 2, paintData.getHeight() / 2);
+
+            graphics2D.setColor(Color.RED);
+            graphics2D.fillRect(paintData.getWidth() / 2, 0, paintData.getWidth() / 2, paintData.getHeight() / 2);
+
+            graphics2D.setColor(Color.BLUE);
+            graphics2D.fillRect(0, paintData.getHeight() / 2, paintData.getWidth() / 2, paintData.getHeight() / 2);
+
+            graphics2D.setColor(Color.GREEN);
+            graphics2D.fillRect(paintData.getWidth() / 2, paintData.getHeight() / 2, paintData.getWidth() / 2,
+                paintData.getHeight() / 2);
+
+            ImageIO.write(img, "png", out);
+        }
+    }
+}
