89b80f8f4055b8c6d05217b8cd91ba9fcd73987d
Jiri Stefek <jstefek@redhat.com>
2013-09-02 14:28:02 +0200

    fragments + tests: toggleControl fragment deleted, tests refactored

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richToggleControl/AbstractTestToggleControl.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richToggleControl/AbstractTestToggleControl.java
@@ -1,25 +1,24 @@
-/**
- * *****************************************************************************
- * JBoss, Home of Professional Open Source Copyright 2010-2013, Red Hat, Inc.
- * and individual contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2013, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
- ******************************************************************************
- */
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.richToggleControl;
 
 import static org.richfaces.tests.metamer.ftest.webdriver.AttributeList.toggleControlAttributes;
@@ -29,7 +28,6 @@ import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.graphene.spi.annotations.Page;
 import org.openqa.selenium.WebElement;
 import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
-import org.richfaces.tests.page.fragments.impl.toggleControl.RichFacesToggleControl;
 
 /**
  * Abstract test case for rich:toggleControl.
@@ -42,9 +40,9 @@ public abstract class AbstractTestToggleControl extends AbstractWebDriverTest {
     @Page
     ToggleControlPage page;
 
-    private RichFacesToggleControl[] firstPanelBtns;
+    private WebElement[] firstPanelBtns;
 
-    private RichFacesToggleControl[] secondPanelBtns;
+    private WebElement[] secondPanelBtns;
 
     public void testSwitchFirstPanel(WebElement[] items) {
         testSwitching(getFirstPanelButtons(), items);
@@ -57,12 +55,12 @@ public abstract class AbstractTestToggleControl extends AbstractWebDriverTest {
     public void testTargetItem(WebElement[] items) {
         toggleControlAttributes.set(ToggleControlAttributes.targetItem, "item2");
 
-        Graphene.guardAjax(page.tcPanel1Item3).toggle();
+        Graphene.guardAjax(page.getTcPanel1Item3()).click();
         Graphene.waitGui().until().element(items[2]).is().visible();
         assertFalse(items[0].isDisplayed(), "Item 1 should not be visible.");
         assertFalse(items[1].isDisplayed(), "Item 2 should not be visible.");
 
-        Graphene.guardAjax(page.tcCustom).toggle();
+        Graphene.guardAjax(page.getTcCustom()).click();
         Graphene.waitGui().until().element(items[1]).is().visible();
         assertFalse(items[0].isDisplayed(), "Item 1 should not be visible.");
         assertFalse(items[2].isDisplayed(), "Item 3 should not be visible.");
@@ -71,45 +69,45 @@ public abstract class AbstractTestToggleControl extends AbstractWebDriverTest {
     public void testTargetPanel(WebElement[] items) {
         toggleControlAttributes.set(ToggleControlAttributes.targetPanel, "panel2");
 
-        Graphene.guardAjax(page.tcPanel2Item3).toggle();
+        Graphene.guardAjax(page.getTcPanel2Item3()).click();
         Graphene.waitGui().until().element(items[2]).is().visible();
         assertFalse(items[0].isDisplayed(), "Item 1 should not be visible.");
         assertFalse(items[1].isDisplayed(), "Item 2 should not be visible.");
 
-        Graphene.guardAjax(page.tcCustom).toggle();
+        Graphene.guardAjax(page.getTcCustom()).click();
         // waitGui.failWith("Item 1 is not displayed.").until(elementVisible.locator(items[0]));
         Graphene.waitGui().until().element(items[0]).is().visible();
         assertFalse(items[1].isDisplayed(), "Item 2 should not be visible.");
         assertFalse(items[2].isDisplayed(), "Item 3 should not be visible.");
     }
 
-    private void testSwitching(RichFacesToggleControl[] buttons, WebElement[] items) {
-        Graphene.guardAjax(buttons[2]).toggle();
+    private void testSwitching(WebElement[] buttons, WebElement[] items) {
+        Graphene.guardAjax(buttons[2]).click();
         Graphene.waitGui().until().element(items[2]).is().visible();
         assertFalse(items[0].isDisplayed(), "Item 1 should not be visible.");
         assertFalse(items[1].isDisplayed(), "Item 2 should not be visible.");
 
-        Graphene.guardAjax(buttons[1]).toggle();
+        Graphene.guardAjax(buttons[1]).click();
         Graphene.waitGui().until().element(items[1]).is().visible();
         assertFalse(items[0].isDisplayed(), "Item 1 should not be visible.");
         assertFalse(items[2].isDisplayed(), "Item 3 should not be visible.");
 
-        Graphene.guardAjax(buttons[0]).toggle();
+        Graphene.guardAjax(buttons[0]).click();
         Graphene.waitGui().until().element(items[0]).is().visible();
         assertFalse(items[1].isDisplayed(), "Item 2 should not be visible.");
         assertFalse(items[2].isDisplayed(), "Item 3 should not be visible.");
     }
 
-    public RichFacesToggleControl[] getFirstPanelButtons() {
-        if ( firstPanelBtns == null ) {
-            firstPanelBtns = new RichFacesToggleControl[] { page.tcPanel1Item1, page.tcPanel1Item2, page.tcPanel1Item3 };
+    public WebElement[] getFirstPanelButtons() {
+        if (firstPanelBtns == null) {
+            firstPanelBtns = new WebElement[]{ page.getTcPanel1Item1(), page.getTcPanel1Item2(), page.getTcPanel1Item3() };
         }
         return firstPanelBtns;
     }
 
-    public RichFacesToggleControl[] getSecondPanelButtons() {
-        if ( secondPanelBtns == null ) {
-            secondPanelBtns = new RichFacesToggleControl[] { page.tcPanel2Item1, page.tcPanel2Item2, page.tcPanel2Item3 };
+    public WebElement[] getSecondPanelButtons() {
+        if (secondPanelBtns == null) {
+            secondPanelBtns = new WebElement[]{ page.getTcPanel2Item1(), page.getTcPanel2Item2(), page.getTcPanel2Item3() };
         }
         return secondPanelBtns;
     }
