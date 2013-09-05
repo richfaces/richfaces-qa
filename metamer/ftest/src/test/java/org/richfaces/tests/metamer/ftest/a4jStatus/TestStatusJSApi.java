e13482ab0efb733636465c1ac2ac45856bc292d1
manovotn <manovotn@dhcp129-149.brq.redhat.com>
2013-09-05 14:06:06 +0200

    Added tests for RichTabPanel JS API and added copyright text to some test classes that were missing it

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/a4jStatus/TestStatusJSApi.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/a4jStatus/TestStatusJSApi.java
@@ -1,3 +1,25 @@
+/**
+ * *****************************************************************************
+ * JBoss, Home of Professional Open Source Copyright 2010-2013, Red Hat, Inc.
+ * and individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ * *****************************************************************************
+ */
 package org.richfaces.tests.metamer.ftest.a4jStatus;
 
 import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
@@ -9,6 +31,7 @@ import org.jboss.arquillian.graphene.findby.FindByJQuery;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
+import org.richfaces.tests.metamer.ftest.annotations.Templates;
 import org.testng.annotations.Test;
 
 /**
@@ -47,6 +70,7 @@ public class TestStatusJSApi extends AbstractWebDriverTest {
     }
 
     @Test
+    @Templates(value = { "plain" })
     public void testJsStart() {
         setAndAssertInitialState();
         testFireEvent(statusAttributes, StatusAttributes.onstart, new Action() {
@@ -59,6 +83,7 @@ public class TestStatusJSApi extends AbstractWebDriverTest {
     }
 
     @Test
+    @Templates(value = { "plain" })
     public void testJsStop() {
         setAndAssertInitialState();
         testFireEvent(statusAttributes, StatusAttributes.onstop, new Action() {
@@ -70,6 +95,7 @@ public class TestStatusJSApi extends AbstractWebDriverTest {
     }
 
     @Test
+    @Templates(value = { "plain" })
     public void testJsError() {
         setAndAssertInitialState();
         testFireEvent(statusAttributes, StatusAttributes.onerror, new Action() {
---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richTabPanel/TestRichTabPanelJSApi.java
@@ -0,0 +1,189 @@
+/**
+ * *****************************************************************************
+ * JBoss, Home of Professional Open Source Copyright 2010-2013, Red Hat, Inc.
+ * and individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ * *****************************************************************************
+ */
+package org.richfaces.tests.metamer.ftest.richTabPanel;
+
+import static org.jboss.arquillian.ajocado.utils.URLUtils.buildUrl;
+import static org.jboss.arquillian.graphene.Graphene.guardAjax;
+import static org.jboss.arquillian.graphene.Graphene.guardNoRequest;
+import static org.richfaces.tests.metamer.ftest.webdriver.AttributeList.tabPanelAttributes;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+import static org.testng.AssertJUnit.assertNull;
+import static org.testng.AssertJUnit.assertTrue;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jboss.arquillian.graphene.findby.FindByJQuery;
+import org.jboss.arquillian.graphene.page.Page;
+import org.openqa.selenium.WebElement;
+import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
+import org.richfaces.tests.metamer.ftest.annotations.Templates;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+public class TestRichTabPanelJSApi extends AbstractWebDriverTest {
+
+    @Override
+    public URL getTestUrl() {
+        return buildUrl(contextPath, "faces/components/richTabPanel/addTab2.xhtml");
+    }
+
+    @Page
+    private TabPanelSimplePage page;
+
+    @FindByJQuery("input[id*=switchButton]")
+    private List<WebElement> switchToButtons;
+
+    private String JsPrevItem() {
+        return (String) executeJS("return RichFaces.component('" + page.getPanelTabAsWebElement().getAttribute("id")
+            + "').prevItem()");
+    }
+
+    private String JsNextItem() {
+        return (String) executeJS("return RichFaces.component('" + page.getPanelTabAsWebElement().getAttribute("id")
+            + "').nextItem()");
+    }
+
+    @Test
+    @Templates(value = { "plain" })
+    public void testJsSwitchTo() {
+        // assert initial settings - 5 tabs, so 5 elements in list should be present
+        assertEquals(switchToButtons.size(), page.getPanelTab().getNumberOfTabs());
+
+        // switch to certain panel and assert it's content is visible
+        guardAjax(switchToButtons.get(4)).click();
+        assertVisible(page.getPanelTab().advanced().getAllTabContents().get(4), "Content of the last tab should be visible");
+
+        guardAjax(switchToButtons.get(0)).click();
+        assertVisible(page.getPanelTab().advanced().getAllTabContents().get(0), "Content of the first tab should be visible");
+
+        guardAjax(switchToButtons.get(2)).click();
+        assertVisible(page.getPanelTab().advanced().getAllTabContents().get(2), "Content of the third tab should be visible");
+
+        guardAjax(switchToButtons.get(1)).click();
+        assertVisible(page.getPanelTab().advanced().getAllTabContents().get(1), "Content of the second tab should be visible");
+
+        guardNoRequest(switchToButtons.get(3)).click();
+        assertNotVisible(page.getPanelTab().advanced().getAllTabContents().get(3),
+            "Content of the fourth tab should not be visible");
+    }
+
+    @Test
+    @Templates(value = { "plain" })
+    public void testJsGetItems() {
+        // Using JS API get IDs and isSelected attributes and assert
+        for (int i = 0; i < 5; i++) {
+            String id = (String) executeJS("return RichFaces.component('" + page.getPanelTabAsWebElement().getAttribute("id")
+                + "').getItems()['" + i + "'].id");
+            Boolean selected = (Boolean) executeJS("return RichFaces.component('"
+                + page.getPanelTabAsWebElement().getAttribute("id") + "').getItems()['" + i + "'].isSelected()");
+
+            assertTrue(page.getPanelTab().advanced().getAllActiveHeaders().get(i).getAttribute("id").contains(id));
+            if (i == 0) {
+                // first tab is selected
+                assertTrue(selected);
+            } else {
+                assertFalse(selected);
+            }
+        }
+    }
+
+    @Test
+    @Templates(value = { "plain" })
+    public void testJsGetItemsNames() {
+        @SuppressWarnings("unchecked")
+        List<String> result = (ArrayList<String>) executeJS("return RichFaces.component('"
+            + page.getPanelTabAsWebElement().getAttribute("id") + "').getItemsNames()");
+        assertTrue(result.size() == 5);
+        assertEquals("tab1, tab2, tab3, tab4, tab5", result.toString().substring(1, result.toString().length() - 1));
+    }
+
+    @Test
+    @Templates(value = { "plain" })
+    public void testJsFirstItem() {
+        String result = (String) executeJS("return RichFaces.component('" + page.getPanelTabAsWebElement().getAttribute("id")
+            + "').firstItem()");
+        assertEquals("tab1", result);
+    }
+
+    @Test
+    @Templates(value = { "plain" })
+    public void testJsPrevItem() {
+        // switch to first, use JS function and assert its null
+        page.getPanelTab().switchTo(0);
+        String result = JsPrevItem();
+        assertNull(result);
+
+        // move to second tab and assert tab1 is previous
+        page.getPanelTab().switchTo(1);
+        result = JsPrevItem();
+        assertEquals("tab1", result);
+
+        // turn on cycled switching
+        tabPanelAttributes.set(TabPanelAttributes.cycledSwitching, true);
+
+        // assert with cycled switching
+        page.getPanelTab().switchTo(0);
+        result = JsPrevItem();
+        assertEquals("tab5", result);
+
+        page.getPanelTab().switchTo(2);
+        result = JsPrevItem();
+        assertEquals("tab2", result);
+    }
+
+    @Test
+    @Templates(value = { "plain" })
+    public void testJsNextItem() {
+        page.getPanelTab().switchTo(0);
+        String result = JsNextItem();
+        assertEquals("tab2", result);
+
+        page.getPanelTab().switchTo(4);
+        result = JsNextItem();
+        assertNull(result);
+
+        // turn on cycled switching
+        tabPanelAttributes.set(TabPanelAttributes.cycledSwitching, true);
+
+        result = JsNextItem();
+        assertEquals("tab1", result);
+
+        page.getPanelTab().switchTo(1);
+        result = JsNextItem();
+        assertEquals("tab3", result);
+    }
+
+    @Test
+    @Templates(value = { "plain" })
+    public void testJsLastItem() {
+        String result = (String) executeJS("return RichFaces.component('" + page.getPanelTabAsWebElement().getAttribute("id")
+            + "').lastItem()");
+        assertEquals("tab5", result);
+    }
+}
