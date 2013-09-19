ba6cf63c7d2320141c4862e9cb50679d30b56a98
Jiri Stefek <jstefek@redhat.com>
2013-09-19 10:33:32 +0200

    fragments: refactoring in pickers

--- page-fragments/src/test/java/org/richfaces/tests/page/fragments/test/choicePicker/TestChoicePickerByIndex.java
+++ page-fragments/src/test/java/org/richfaces/tests/page/fragments/test/choicePicker/TestChoicePickerByIndex.java
@@ -1,6 +1,10 @@
 package org.richfaces.tests.page.fragments.test.choicePicker;
 
-import static org.testng.Assert.fail;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
 
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.testng.Arquillian;
@@ -12,6 +16,8 @@ import org.richfaces.tests.page.fragments.impl.utils.picker.ChoicePickerHelper.B
 import org.richfaces.tests.page.fragments.test.choicePicker.fragments.MyPageFragment;
 import org.testng.annotations.Test;
 
+import com.google.common.collect.Lists;
+
 public class TestChoicePickerByIndex extends Arquillian {
 
     @Drone
@@ -20,16 +26,68 @@ public class TestChoicePickerByIndex extends Arquillian {
     @FindBy(tagName = "body")
     private MyPageFragment myFragment;
 
+    private List<String> getStringsFromElements(List<WebElement> list) {
+        List<String> result = Lists.newArrayList();
+        for (WebElement webElement : list) {
+            result.add(webElement.getText());
+        }
+        return result;
+    }
+
     @Test
-    public void testPickNonExistingElement() {
+    public void testPickEvery2nd() {
+        browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
+
+        ByIndexChoicePicker picker = ChoicePickerHelper.byIndex().everyNth(2);
+        List<WebElement> pickMultiple = picker.pickMultiple(myFragment.getDivs());
+        assertEquals(getStringsFromElements(pickMultiple), Lists.newArrayList("1", "3", "5"));
+    }
+
+    @Test
+    public void testPickEvery2ndFromSecond() {
+        browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
+
+        ByIndexChoicePicker picker = ChoicePickerHelper.byIndex().everyNth(2, 1);
+        List<WebElement> pickMultiple = picker.pickMultiple(myFragment.getDivs());
+        assertEquals(getStringsFromElements(pickMultiple), Lists.newArrayList("2", "4", "6"));
+    }
+
+    @Test
+    public void testPickEvery2ndFromTooHighIndex() {
+        browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
+
+        ByIndexChoicePicker picker = ChoicePickerHelper.byIndex().everyNth(2, 6);
+        List<WebElement> pickMultiple = picker.pickMultiple(myFragment.getDivs());
+        assertTrue(pickMultiple.isEmpty());
+    }
+
+    @Test
+    public void testPickMultiple() {
+        browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
+
+        ByIndexChoicePicker picker = ChoicePickerHelper.byIndex().indexes(1, 2).first().last().beforeLast(1);
+        List<WebElement> pickMultiple = picker.pickMultiple(myFragment.getDivs());
+        assertEquals(getStringsFromElements(pickMultiple), Lists.newArrayList("2", "3", "1", "6", "5"));
+    }
+
+    @Test
+    public void testPickMultipleTimesSameIndexes() {
+        browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
+
+        ByIndexChoicePicker picker = ChoicePickerHelper.byIndex()
+            .first().index(0)// the first element
+            .index(1).indexes(0, 1)// the first two elements
+            .last().beforeLast(0).index(5);// the last element
+
+        List<WebElement> pickMultiple = picker.pickMultiple(myFragment.getDivs());
+        assertEquals(getStringsFromElements(pickMultiple), Lists.newArrayList("1", "2", "6"));
+    }
+
+    @Test
+    public void testPickNotExistingElement() {
         browser.get(TestChoicePickerByVisibleText.class.getResource("choicePickerByVisibleText.html").toExternalForm());
 
         ByIndexChoicePicker picker = ChoicePickerHelper.byIndex().index(15);
-        try {
-            WebElement element = picker.pick(myFragment.getDivs());
-            fail("IndexOutOfBounds should be thrown!");
-        } catch (IndexOutOfBoundsException ex) {
-            // expected
-        }
+        assertNull(picker.pick(myFragment.getDivs()));
     }
 }
