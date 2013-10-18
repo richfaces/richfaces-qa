bf0f6bc1ac4eda0b9c57a55d90ba91230b83a585
Pavol Pitonak <ppitonak@redhat.com>
2013-10-18 15:29:43 +0200

    TestNG listeners in metamer/ftest-legacy refactored

--- metamer/ftest-legacy/src/test/java/org/richfaces/tests/metamer/ftest/MetamerFailureLoggingTestListener.java
+++ metamer/ftest-legacy/src/test/java/org/richfaces/tests/metamer/ftest/MetamerFailureLoggingTestListener.java
@@ -23,35 +23,68 @@ package org.richfaces.tests.metamer.ftest;
 
 import static java.util.Arrays.asList;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
-import org.jboss.test.selenium.listener.FailureLoggingTestListener;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.jboss.arquillian.ajocado.browser.BrowserType;
+import org.jboss.arquillian.ajocado.framework.GrapheneConfigurationContext;
+import org.jboss.arquillian.ajocado.framework.GrapheneSelenium;
+import org.jboss.arquillian.ajocado.framework.GrapheneSeleniumContext;
+import org.jboss.arquillian.ajocado.network.NetworkTrafficType;
+import org.jboss.test.selenium.utils.testng.TestInfo;
+import org.jboss.test.selenium.utils.testng.TestLoggingUtils;
 import org.richfaces.tests.metamer.ftest.annotations.IssueTracking;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+import com.thoughtworks.selenium.SeleniumException;
 
 /**
+ * Test listener which provides the methods injected in lifecycle of test case to catch the additional information in
+ * context of test failure.
+ *
  * @author <a href="mailto:lfryc@redhat.com">Lukas Fryc</a>
- * @author <a href="https://community.jboss.org/people/ppitonak">Pavol Pitonak</a>
- * @version $Revision: 22728 $
+ * @authot <a href="https://community.jboss.org/people/ppitonak">Pavol Pitonak</a>
+ * @version $Revision$
  */
-public class MetamerFailureLoggingTestListener extends FailureLoggingTestListener {
+public class MetamerFailureLoggingTestListener extends TestListenerAdapter {
+
+//    protected static final Logger LOGGER = Logger.getLogger(FailureLoggingTestListener.class.getName());
+    protected File mavenProjectBuildDirectory = new File(System.getProperty("maven.project.build.directory",
+        "./target/"));
+    protected File failuresOutputDir = new File(mavenProjectBuildDirectory, "failures");
+    protected GrapheneSelenium selenium = GrapheneSeleniumContext.getProxy();
+
     @Override
-    protected String getFilenameIdentification(ITestResult result) {
-        return MetamerTestInfo.getAssociatedFilename(result);
+    public void onStart(ITestContext testContext) {
+        try {
+            FileUtils.forceMkdir(failuresOutputDir);
+            // FIXME it should clean directory only if it is the first test suite
+            // FileUtils.cleanDirectory(failuresOutputDir);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    protected String getSeleniumLogIdentification(ITestResult result) {
-        String id = super.getSeleniumLogIdentification(result);
-        return id + " " + MetamerTestInfo.getConfigurationInfoInParenthesses();
+    public void onConfigurationFailure(ITestResult result) {
+        onFailure(result);
     }
 
     @Override
+    public void onTestFailure(ITestResult result) {
+        onFailure(result);
+    }
+
     public void onFailure(ITestResult result) {
         onFailureForSelenium1(result);
 
@@ -77,4 +110,70 @@ public class MetamerFailureLoggingTestListener extends FailureLoggingTestListene
             }
         }
     }
+
+    public void onFailureForSelenium1(ITestResult result) {
+        if (!selenium.isStarted()) {
+            return;
+        }
+
+        Throwable throwable = result.getThrowable();
+        String stacktrace = null;
+
+        if (throwable != null) {
+            stacktrace = ExceptionUtils.getStackTrace(throwable);
+        }
+
+        String filenameIdentification = getFilenameIdentification(result);
+
+        String traffic;
+        try {
+            traffic = selenium.captureNetworkTraffic(NetworkTrafficType.PLAIN).getTraffic();
+        } catch (SeleniumException e) {
+            traffic = ExceptionUtils.getFullStackTrace(e);
+        }
+
+        BrowserType browser = GrapheneConfigurationContext.getProxy().getBrowser().getType();
+        BufferedImage screenshot = null;
+
+        if (browser == BrowserType.FIREFOX) {
+            screenshot = selenium.captureEntirePageScreenshot();
+        }
+
+        String htmlSource = selenium.getHtmlSource();
+
+        File stacktraceOutputFile = new File(failuresOutputDir, filenameIdentification + "/stacktrace.txt");
+        File imageOutputFile = new File(failuresOutputDir, filenameIdentification + "/screenshot.png");
+        File trafficOutputFile = new File(failuresOutputDir, filenameIdentification + "/network-traffic.txt");
+        // File logOutputFile = new File(failuresOutputDir, filenameIdentification + "/selenium-log.txt");
+        File htmlSourceOutputFile = new File(failuresOutputDir, filenameIdentification + "/html-source.html");
+
+        try {
+            File directory = imageOutputFile.getParentFile();
+            FileUtils.forceMkdir(directory);
+
+            FileUtils.writeStringToFile(stacktraceOutputFile, stacktrace);
+            if (browser == BrowserType.FIREFOX) {
+                ImageIO.write(screenshot, "PNG", imageOutputFile);
+            }
+            FileUtils.writeStringToFile(trafficOutputFile, traffic);
+            // FileUtils.writeLines(logOutputFile, methodLog);
+            FileUtils.writeStringToFile(htmlSourceOutputFile, htmlSource);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected String getSeleniumLogIdentification(ITestResult result) {
+        final String failure = TestInfo.STATUSES.get(result.getStatus()).toUpperCase();
+        final String started = TestInfo.STATUSES.get(ITestResult.STARTED).toUpperCase();
+        String testDescription = TestLoggingUtils.getTestDescription(result);
+        testDescription = testDescription.replaceFirst(failure, started);
+        testDescription = testDescription.replaceFirst("\\[[^\\]]+\\] ", "");
+        return testDescription + " " + MetamerTestInfo.getConfigurationInfoInParenthesses();
+    }
+
+    protected String getFilenameIdentification(ITestResult result) {
+        return MetamerTestInfo.getAssociatedFilename(result);
+    }
+
 }
