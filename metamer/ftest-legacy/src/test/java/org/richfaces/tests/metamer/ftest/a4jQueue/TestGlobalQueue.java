5ebde3e9a71a2d106e95239db30e5de284f5e890
Jiri Stefek <jstefek@redhat.com>
2014-03-06 13:14:34 +0100

    metamer-legacy: queue: testRequestDelay: delays changed

--- metamer/ftest-legacy/src/test/java/org/richfaces/tests/metamer/ftest/a4jQueue/TestGlobalQueue.java
+++ metamer/ftest-legacy/src/test/java/org/richfaces/tests/metamer/ftest/a4jQueue/TestGlobalQueue.java
@@ -1,25 +1,24 @@
-/**
- * *****************************************************************************
- * JBoss, Home of Professional Open Source Copyright 2010-2014, Red Hat, Inc.
- * and individual contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
+/*******************************************************************************
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2014, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
- * *****************************************************************************
- */
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *******************************************************************************/
 package org.richfaces.tests.metamer.ftest.a4jQueue;
 
 import static org.jboss.arquillian.ajocado.Graphene.retrieveText;
@@ -30,8 +29,8 @@ import static org.richfaces.tests.metamer.ftest.a4jQueue.QueueAttributes.rendere
 import static org.richfaces.tests.metamer.ftest.a4jQueue.QueueAttributes.timeout;
 import static org.testng.Assert.assertEquals;
 
-import com.thoughtworks.selenium.SeleniumException;
 import java.net.URL;
+
 import org.jboss.arquillian.ajocado.locator.JQueryLocator;
 import org.jboss.cheiron.halt.XHRHalter;
 import org.jboss.cheiron.halt.XHRState;
@@ -42,6 +41,8 @@ import org.richfaces.tests.metamer.ftest.annotations.RegressionTest;
 import org.richfaces.tests.metamer.ftest.annotations.Use;
 import org.richfaces.tests.metamer.ftest.attributes.Attributes;
 import org.testng.annotations.Test;
+
+import com.thoughtworks.selenium.SeleniumException;
 
 /**
  * @author <a href="mailto:lfryc@redhat.com">Lukas Fryc</a>
@@ -65,7 +66,7 @@ public class TestGlobalQueue extends AbstractGrapheneTest {
      * request (begin).
      */
     @Test
-    @Use(field = "requestDelay", ints = { 4000, 900, 150 })
+    @Use(field = "requestDelay", ints = { 3000, 1200})
     public void testRequestDelay() {
         attributes.set(QueueAttributes.requestDelay, requestDelay);
 
