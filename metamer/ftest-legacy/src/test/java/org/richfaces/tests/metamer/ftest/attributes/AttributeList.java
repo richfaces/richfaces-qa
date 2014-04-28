8adf149cce931fc65a279958da631dfcd0065a4a
Juraj Huska <jhuska@redhat.com>
2014-04-28 17:35:34 +0200

    ftest-legacy: removed redundant files
    - as table tests are ported removed all abstractions and model classes & iteration

--- metamer/ftest-legacy/src/test/java/org/richfaces/tests/metamer/ftest/attributes/AttributeList.java
+++ metamer/ftest-legacy/src/test/java/org/richfaces/tests/metamer/ftest/attributes/AttributeList.java
@@ -8,8 +8,6 @@ import org.richfaces.tests.metamer.ftest.BasicAttributes;
 import org.richfaces.tests.metamer.ftest.MetamerAttributes;
 import org.richfaces.tests.metamer.ftest.a4jAttachQueue.AttachQueueAttributes;
 import org.richfaces.tests.metamer.ftest.a4jQueue.QueueAttributes;
-import org.richfaces.tests.metamer.ftest.abstractions.DataTableFacets;
-import org.richfaces.tests.metamer.ftest.richDataTable.DataTableAttributes;
 
 public class AttributeList {
 
@@ -19,8 +17,6 @@ public class AttributeList {
     public static Attributes<AttachQueueAttributes> attachQueueAttrs2 = new Attributes<AttachQueueAttributes>(
         pjq("table.attributes[id$=attributes2]"));
     public static Attributes<BasicAttributes> basicAttributes = new Attributes<BasicAttributes>();
-    public static Attributes<DataTableAttributes> dataTableAttributes = new Attributes<DataTableAttributes>();
-    public static Attributes<DataTableFacets> dataTableFacets = new Attributes<DataTableFacets>(RequestType.XHR); // for facets
     public static Attributes<MetamerAttributes> metamerAttributes = new Attributes<MetamerAttributes>(
         jq("form[id$=displayControlsForm]"));
     public static Attributes<QueueAttributes> queueAttributes = new Attributes<QueueAttributes>(
