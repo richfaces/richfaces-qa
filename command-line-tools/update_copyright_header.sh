60926c82cbec3759ee15853327882aacb7a3984c
Jiri Stefek <jstefek@redhat.com>
2014-06-02 11:02:28 +0200

    command-line-tools: update_copyright_header script updated, all copyright headers are now involved

--- command-line-tools/update_copyright_header.sh
+++ command-line-tools/update_copyright_header.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-find . -type f -name '*.java'  -exec sed -i 's/Copyright 2010-2013,/Copyright 2010-2014,/g' {} \;
-find . -type f -name '*.xhtml' -exec sed -i 's/Copyright 2010-2013,/Copyright 2010-2014,/g' {} \;
-find . -type f -name '*.xml'   -exec sed -i 's/Copyright 2010-2013,/Copyright 2010-2014,/g' {} \;
+# include all headers with a single year (4 numbers, e.g. 2010) or with a range of years (4 numbers - 4numbers,
+# e.g. 2010-2011) and replace it with a range of years $START-$END.
+START=2010
+END=2015
+TEMPLATE="s/Copyright [0-9]\{4\}\(-[0-9]\{4\}\)\?,/Copyright ${START}-${END},/g"
+
+find . -type f -name '*.java'  -exec sed -i "${TEMPLATE}" {} \;
+find . -type f -name '*.xhtml' -exec sed -i "${TEMPLATE}" {} \;
+find . -type f -name '*.xml'   -exec sed -i "${TEMPLATE}" {} \;
