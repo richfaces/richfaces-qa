bae80f56fd44edb6d69ac19fba3a8ed3bdbeae23
Jiri Stefek <jstefek@redhat.com>
2014-06-18 08:06:36 +0200

    metamer: matrixConfigurator: do not unconfigure when there is no configuration

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/AbstractWebDriverTest.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/AbstractWebDriverTest.java
@@ -196,7 +196,9 @@ public abstract class AbstractWebDriverTest extends AbstractMetamerTest {
 
     @AfterMethod(alwaysRun = true)
     public void unconfigure() {
-        currentConfiguration.unconfigure();
+        if (currentConfiguration != null) {
+            currentConfiguration.unconfigure();
+        }
     }
 
     /**
