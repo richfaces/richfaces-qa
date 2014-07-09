4ffcd4c6902982b413d44258e68e444b264c28c8
Matej Novotny <manovotn@redhat.com>
2014-07-09 16:19:27 +0200

    Added tests for chart attributes

---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richChart/AbstractChartTest.java
@@ -0,0 +1,77 @@
+/******************************************************************************
+ * JBoss, Home of Professional Open Source Copyright 2010-2014, Red Hat, Inc.
+ * and individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ ******************************************************************************/
+package org.richfaces.tests.metamer.ftest.richChart;
+
+import static org.jboss.arquillian.graphene.Graphene.waitAjax;
+import static org.testng.AssertJUnit.assertTrue;
+
+import org.jboss.arquillian.graphene.page.Page;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
+import org.richfaces.tests.metamer.ftest.webdriver.Attributes;
+import org.testng.annotations.Test;
+
+/**
+ * Abstract class for chart tests. Allows to keep common tests altogether while like chart can have extra tests for zooming etc.
+ */
+public abstract class AbstractChartTest extends AbstractWebDriverTest {
+
+    private final Attributes<ChartAttributes> chartAttributes = getAttributes();
+
+    @Page
+    ChartSimplePage page;
+
+    public void testRendered() {
+        assertPresent(page.getChartElement(), "Chart should be rendered.");
+        chartAttributes.set(ChartAttributes.rendered, false);
+        assertNotPresent(page.getChartElement(), "Chart should not be rendered.");
+    }
+
+    public void testTitle() {
+        // assert title is present
+        assertPresent(page.getChartTitleElement(), "Title should be rendered");
+
+        // change title and assert
+        final String newTitle = "SomeNewAwesomeAndSlightlyLongTitle";
+        chartAttributes.set(ChartAttributes.title, newTitle);
+        waitAjax(driver).until().element(page.getChartTitleElement()).text().equalTo(newTitle);
+    }
+
+    public void testStyleClass() {
+        final String newStyleClass = "newStyleClass";
+        chartAttributes.set(ChartAttributes.styleClass, newStyleClass);
+        assertTrue(page.getChartElement().getAttribute("class").contains(newStyleClass));
+    }
+
+    @Test
+    public void testOnmouseout() {
+        testFireEvent(chartAttributes, ChartAttributes.onmouseout, new Action() {
+            @Override
+            public void perform() {
+                // move to chart element
+                new Actions(driver).moveToElement(page.getChartElement()).build().perform();
+                // move out of chart element area
+                new Actions(driver).moveToElement(page.getPlotClickMessage()).build().perform();
+            }
+        });
+    }
+}
---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richChart/ChartSimplePage.java
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * JBoss, Home of Professional Open Source Copyright 2010-2014, Red Hat, Inc.
+ * and individual contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ ******************************************************************************/
+package org.richfaces.tests.metamer.ftest.richChart;
+
+import org.jboss.arquillian.graphene.findby.FindByJQuery;
+import org.openqa.selenium.WebElement;
+import org.richfaces.tests.metamer.ftest.webdriver.MetamerPage;
+
+public class ChartSimplePage extends MetamerPage {
+
+    @FindByJQuery(value = "span[id$='frm:msg']")
+    private WebElement serverSideMessage;
+
+    @FindByJQuery(value = "span[id$='clickInfo']")
+    private WebElement plotClickMessage;
+
+    @FindByJQuery(value = "span[id$='hoverInfo']")
+    private WebElement plotHoverMessage;
+
+    @FindByJQuery(value = "div[id$='frm:chart']")
+    private WebElement chartElement;
+
+    @FindByJQuery(value = "div[id$='frm:chart'] div.chart-title")
+    private WebElement chartTitleElement;
+
+    public WebElement getChartTitleElement() {
+        return chartTitleElement;
+    }
+
+    public WebElement getServerSideMessage() {
+        return serverSideMessage;
+    }
+
+    public WebElement getPlotClickMessage() {
+        return plotClickMessage;
+    }
+
+    public WebElement getPlotHoverMessage() {
+        return plotHoverMessage;
+    }
+
+    public WebElement getChartElement() {
+        return chartElement;
+    }
+}
