bbb552cf9f3a8b924fe0baab12e719200452e64f
Matej Novotny <manovotn@redhat.com>
2014-11-12 14:17:55 +0100

    Modified some scripts to work on Solaris as well

--- command-line-tools/failBuildWhenTooManySkips.sh
+++ command-line-tools/failBuildWhenTooManySkips.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
+#replacement for readlink command which is not available on Solaris
+canonicalpath() {
+  if [ -d "$1" ]; then
+    pushd $1 > /dev/null 2>&1
+    echo $PWD
+  elif [ -f "$1" ]; then
+    pushd $(dirname $1) > /dev/null 2>&1
+    echo $PWD/$(basename $1)
+  else
+    echo "Invalid path $1"
+  fi
+  popd > /dev/null 2>&1
+}
+
 SCRIPT_DIR=`dirname $BASH_SOURCE`;
-SCRIPT_DIR=`readlink -f $SCRIPT_DIR`;
+SCRIPT_DIR=`canonicalpath $SCRIPT_DIR`;
 
 # first argument = path to testng-results.xml file [optional], default '../metamer/ftest/target/surefire-reports/testng-results.xml'
 # second argument = threshold number of skips after which the build will fail [optional], default '2'
 failWhenTooManySkips(){
-  if [[ ! -z "$1" ]];then
+  # check if value in $1 points to an existing regular file, if so, it is set as path to file; otherwise find is used starting from qa repo
+  if [ -f "$1" ];then
      XML_FILE=$1;
   else
-     XML_FILE="../metamer/ftest/target/surefire-reports/testng-results.xml";
+     # this will locate the testng xml file based on script directory - workaround for solaris where path could not be found
+     XML_FILE=$(find $( dirname ${SCRIPT_DIR}) -name testng-results.xml);
   fi
 
-#  echo "XML_FILE=${XML_FILE}"
-
-  cd ${SCRIPT_DIR}
-
-  if [ ! -f ${XML_FILE} ];then
+  if [ ! -f "${XML_FILE}" ];then
      echo "No such file ${XML_FILE}. Exiting...";
      return 1;
   fi
@@ -30,7 +42,7 @@ failWhenTooManySkips(){
 #  echo "MAX_SKIPS_FOR_SUCCESS=${MAX_SKIPS_FOR_SUCCESS}"
 
 
-  SKIPS=`head ${XML_FILE} -n 2 | tail -n 1 | sed -n 's/.* skipped="\([^"]\+\).*/\1/p'`
+  SKIPS=`head -2 ${XML_FILE} | tail -1 | sed -n 's/.* skipped="\([^"]\+\).*/\1/p'`;
   echo "Found: ${SKIPS} skips";
   if [ ${SKIPS} -gt ${MAX_SKIPS_FOR_SUCCESS} ]; then
      echo "Too many skips, FAILING the build";
