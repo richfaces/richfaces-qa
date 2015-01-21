a1f721b88ba0639aa9b84b9955bf107ea33d915e
Jiri Stefek <jstefek@redhat.com>
2015-01-21 10:40:14 +0100

    command-line-tools: download script improved

--- command-line-tools/download.sh
+++ command-line-tools/download.sh
@@ -7,4 +7,9 @@ download(){
      exit 1;
   fi
   wget ${URL} --no-check-certificate -c
+  if [ "$?" -ne "0" ];then
+     # Mac workaround
+     echo "Trying to download the file without option '--no-check-certificate'"
+     wget ${URL} -c
+  fi
 }
