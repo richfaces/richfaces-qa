d23d79a25a434ee0798466adc0cca365183023bf
Jiri Stefek <jstefek@redhat.com>
2015-02-23 13:09:27 +0100

    metamer: added test and reproducer for RF-13963

---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/uiRepeat/TestRF13963.java
@@ -0,0 +1,57 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2015, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.tests.metamer.ftest.uiRepeat;
+
+import static org.jboss.test.selenium.support.url.URLUtils.buildUrl;
+
+import java.net.URL;
+
+import org.jboss.arquillian.graphene.Graphene;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.richfaces.tests.metamer.bean.issues.RF13963;
+import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
+import org.richfaces.tests.metamer.ftest.annotations.RegressionTest;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
+ */
+public class TestRF13963 extends AbstractWebDriverTest {
+
+    private static final String UNEXPECTED_MESSAGE = RF13963.MESSAGE.substring(2);
+
+    @FindBy(css = "[id$='ajaxSubmit']")
+    private WebElement ajaxSubmit;
+
+    @Override
+    public URL getTestUrl() {
+        return buildUrl(contextPath, "faces/components/uiRepeat/rf-13963.xhtml");
+    }
+
+    @Test
+    @RegressionTest("https://issues.jboss.org/browse/RF-13963")
+    public void testUnrenderedUIRepeatIsNotVisited() {
+        Graphene.guardAjax(ajaxSubmit).click();
+        getMetamerPage().assertNoListener(UNEXPECTED_MESSAGE);
+    }
+}
