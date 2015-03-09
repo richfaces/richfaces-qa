c8f496d2de379747f901cd4c1433f7cc29c945dc
Jiri Stefek <jstefek@redhat.com>
2015-03-09 09:24:31 +0100

    metamer: fixed reproducer and test for RF-13780 and RF-13988 to work in a4j:region

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richCollapsiblePanel/TestRF13780.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richCollapsiblePanel/TestRF13780.java
@@ -33,6 +33,7 @@ import org.richfaces.fragment.collapsiblePanel.TextualRichFacesCollapsiblePanel;
 import org.richfaces.fragment.inplaceInput.RichFacesInplaceInput;
 import org.richfaces.fragment.log.Log.LogEntryLevel;
 import org.richfaces.fragment.log.RichFacesLog;
+import org.richfaces.tests.metamer.bean.issues.RF13780;
 import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
 import org.richfaces.tests.metamer.ftest.annotations.RegressionTest;
 import org.testng.annotations.Test;
@@ -42,10 +43,8 @@ import org.testng.annotations.Test;
  */
 public class TestRF13780 extends AbstractWebDriverTest {
 
-    @FindBy(css = "[id$=input1]")
-    private RichFacesInplaceInput input1;
-    @FindBy(css = "[id$=input2]")
-    private RichFacesInplaceInput input2;
+    @FindBy(css = "[id$=input]")
+    private RichFacesInplaceInput input;
     @FindBy(css = "[id$=log]")
     private RichFacesLog log;
     @FindBy(css = "[id$=output]")
@@ -53,12 +52,12 @@ public class TestRF13780 extends AbstractWebDriverTest {
     @FindBy(css = "[id$=panel]")
     private TextualRichFacesCollapsiblePanel panel;
 
-    private void checkInput1(String text) {
-        Graphene.waitGui().withMessage("Input 1 should be updated.").until().element(input1.advanced().getRootElement()).text().equalTo(text);
+    private void assertNoErrorsInLog() {
+        assertEquals(log.getLogEntries().size(), 0, "There should be no errors in a4j:log.");
     }
 
-    private void checkInput2(String text) {
-        Graphene.waitGui().withMessage("Input 2 should be updated.").until().element(input2.advanced().getRootElement()).text().equalTo(text);
+    private void changeInputTextTo(String text) {
+        Graphene.guardAjax(input.type(text)).confirm();
     }
 
     private void checkOutput(String text) {
@@ -76,30 +75,27 @@ public class TestRF13780 extends AbstractWebDriverTest {
         String text = "1";
         testCollapsedCollapsiblePanelWillNotBeVisited();
         checkOutput(text);
-        checkInput2(text);
 
-        text += "2";
-        Graphene.guardAjax(input2.type(text)).confirm();
+        text = "12";
+        changeInputTextTo(text);
         checkOutput(text);
-        checkInput1(text);
-        assertEquals(log.getLogEntries().size(), 0, "There should be no errors in a4j:log.");
+        assertNoErrorsInLog();
 
-        text += "3";
-        Graphene.guardAjax(input1.type(text)).confirm();
-        checkOutput(text);
-        checkInput2(text);
-        assertEquals(log.getLogEntries().size(), 0, "There should be no errors in a4j:log.");
+        text = "";
+        changeInputTextTo(text);
+        checkOutput(RF13780.DEFAULT_VALUE);
+        assertNoErrorsInLog();
     }
 
     @Test
     @RegressionTest("https://issues.jboss.org/browse/RF-13780")
     public void testCollapsedCollapsiblePanelWillNotBeVisited() {
         log.changeLevel(LogEntryLevel.ERROR);
-        assertEquals(log.getLogEntries().size(), 0, "There should be no errors in a4j:log.");
+        assertNoErrorsInLog();
         Graphene.guardAjax(panel).collapse();
-        assertEquals(log.getLogEntries().size(), 0, "There should be no errors in a4j:log.");
+        assertNoErrorsInLog();
 
-        Graphene.guardAjax(input1.type("1")).confirm();
-        assertEquals(log.getLogEntries().size(), 0, "There should be no errors in a4j:log.");
+        changeInputTextTo("1");
+        assertNoErrorsInLog();
     }
 }
