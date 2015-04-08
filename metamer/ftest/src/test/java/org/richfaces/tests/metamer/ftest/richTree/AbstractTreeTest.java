79c987bca26648fa1e9c2111183f6fd5b908b23e
Jiri Stefek <jstefek@redhat.com>
2015-04-08 08:34:35 +0200

    metamer: tree: RichFacesTreeNode indexing unification, tests for tree selection updated, rowKeyConverter fixed (RF-11766)

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richTree/AbstractTreeTest.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richTree/AbstractTreeTest.java
@@ -1,6 +1,6 @@
-/*******************************************************************************
+/*
  * JBoss, Home of Professional Open Source
- * Copyright 2010-2014, Red Hat, Inc. and individual contributors
+ * Copyright 2010-2015, Red Hat, Inc. and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -18,7 +18,7 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *******************************************************************************/
+ */
 package org.richfaces.tests.metamer.ftest.richTree;
 
 import static org.jboss.test.selenium.support.url.URLUtils.buildUrl;
@@ -42,6 +42,7 @@ public abstract class AbstractTreeTest extends AbstractWebDriverTest {
     @FindBy(css = "div[id$=richTree]")
     protected RichFacesTree tree;
 
+    protected final String[] ALL_NODES = { "simpleSwingTreeNode", "simpleRichFacesTreeDataModel", "simpleRichFacesTreeNode" };
     protected static final String IMAGE_URL = "/resources/images/loading.gif";
     protected final Attributes<TreeAttributes> treeAttributes = getAttributes();
     protected final Attributes<TreeNodeAttributes> firstNodeAttributes = getAttributes("treeNode1Attributes");
