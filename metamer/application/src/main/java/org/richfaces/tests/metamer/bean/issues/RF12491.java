e49d0158b574cd5ed3e6cb8d20db813e90a394e1
Jiri Stefek <jstefek@redhat.com>
2015-04-29 13:28:59 +0200

    metamer: tabPanel: added test and reproducer for RF-12491

---
+++ metamer/application/src/main/java/org/richfaces/tests/metamer/bean/issues/RF12491.java
@@ -0,0 +1,142 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2015, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.tests.metamer.bean.issues;
+
+import java.io.Serializable;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
+
+import org.richfaces.component.SwitchType;
+import org.richfaces.tests.metamer.bean.Model;
+import org.richfaces.tests.metamer.model.Capital;
+
+import com.google.common.collect.Maps;
+
+@SessionScoped
+@ManagedBean(name = "rf12491")
+public class RF12491 implements Serializable {
+
+    private static final List<Capital> capitals = Model.unmarshallCapitals();
+    private static final long serialVersionUID = 1L;
+
+    private Map<PhaseId, Integer> getterForTable1InvokedInPhases;
+    private Map<PhaseId, Integer> getterForTable2InvokedInPhases;
+    private SwitchType switchType;
+    private Set<SwitchType> switchTypes;
+    private List<Capital> table1;
+    private List<Capital> table2;
+
+    private String createStringFromMap(Map<PhaseId, Integer> map) {
+        StringBuilder sb = new StringBuilder();
+        for (PhaseId key : map.keySet()) {
+            sb.append(map.get(key))
+                .append('x')
+                .append(key.getName())
+                .append('(')
+                .append(key.getOrdinal())
+                .append("), ");
+        }
+        if (sb.length() > 0) {
+            return sb.substring(0, sb.length() - 2);
+        } else {
+            return "";
+        }
+    }
+
+    public String getGetterForTable1InvokedInPhases() {
+        return createStringFromMap(getterForTable1InvokedInPhases);
+    }
+
+    public String getGetterForTable2InvokedInPhases() {
+        return createStringFromMap(getterForTable2InvokedInPhases);
+    }
+
+    public SwitchType getSwitchType() {
+        return switchType;
+    }
+
+    public Set<SwitchType> getSwitchTypes() {
+        return switchTypes;
+    }
+
+    public List<Capital> getTable1() {
+        updateMap(getterForTable1InvokedInPhases);
+        return table1;
+    }
+
+    public List<Capital> getTable2() {
+        updateMap(getterForTable2InvokedInPhases);
+        return table2;
+    }
+
+    @PostConstruct
+    public void init() {
+        table1 = capitals.subList(0, 10);
+        table2 = capitals.subList(11, 20);
+        switchTypes = EnumSet.allOf(SwitchType.class);
+        switchType = SwitchType.ajax;
+        resetInvokedPhases();
+    }
+
+    public void resetAll() {
+        resetInvokedPhases();
+    }
+
+    public void resetInvokedPhases() {
+        this.getterForTable1InvokedInPhases = Maps.newHashMap();
+        this.getterForTable2InvokedInPhases = Maps.newHashMap();
+    }
+
+    public void setSwitchType(SwitchType switchType) {
+        this.switchType = switchType;
+    }
+
+    public void setSwitchTypes(Set<SwitchType> switchTypes) {
+        this.switchTypes = switchTypes;
+    }
+
+    public void setTable1(List<Capital> table1) {
+        this.table1 = table1;
+    }
+
+    public void setTable2(List<Capital> table2) {
+        this.table2 = table2;
+    }
+
+    private void updateMap(Map<PhaseId, Integer> map) {
+        PhaseId id = FacesContext.getCurrentInstance().getCurrentPhaseId();
+        boolean containsKey = map.containsKey(id);
+        if (containsKey) {
+            map.put(id, map.get(id) + 1);
+        } else {
+            map.put(id, 1);
+        }
+    }
+}
---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richTabPanel/TestRF12491.java
@@ -0,0 +1,67 @@
+package org.richfaces.tests.metamer.ftest.richTabPanel;
+
+import static org.jboss.test.selenium.support.url.URLUtils.buildUrl;
+import static org.testng.Assert.assertEquals;
+
+import java.net.URL;
+
+import org.jboss.arquillian.graphene.Graphene;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.richfaces.fragment.common.NullFragment;
+import org.richfaces.fragment.dataTable.RichFacesDataTable;
+import org.richfaces.fragment.tabPanel.RichFacesTabPanel;
+import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
+import org.richfaces.tests.metamer.ftest.annotations.RegressionTest;
+import org.richfaces.tests.metamer.ftest.webdriver.Attributes;
+import org.testng.annotations.Test;
+
+public class TestRF12491 extends AbstractWebDriverTest {
+
+    private final Attributes<TabPanelAttributes> attributes = getAttributes();
+
+    @FindBy(css = "[id$='firstGetterInvokedPhases']")
+    private WebElement firstGetterInvokedPhases;
+    @FindBy(css = "[id$='resetCounts']")
+    private WebElement resetCounts;
+    @FindBy(css = "[id$='refreshCounts']")
+    private WebElement refreshCounts;
+    @FindBy(css = "[id$='secondGetterInvokedPhases']")
+    private WebElement secondGetterInvokedPhases;
+    @FindBy(css = "div[id$='tabPanel']")
+    private RichFacesTabPanel tabPanel;
+    @FindBy(css = "[id$='table1']")
+    private SimpleDT table1;
+    @FindBy(css = "[id$='table2']")
+    private SimpleDT table2;
+
+    @Override
+    public URL getTestUrl() {
+        return buildUrl(contextPath, "faces/components/richTabPanel/rf-12491.xhtml");
+    }
+
+    private void refreshCounts() {
+        Graphene.guardAjax(refreshCounts).click();
+    }
+
+    @Test
+    @RegressionTest("https://issues.jboss.org/browse/RF-12491")
+    public void testGetterForOtherDataTableIsNotInvoked_switchTypeAjax() {
+        attributes.set(TabPanelAttributes.switchType, "ajax");
+        refreshCounts();
+        assertEquals(firstGetterInvokedPhases.getText(), "1xRENDER_RESPONSE(6)");
+        assertEquals(secondGetterInvokedPhases.getText(), "", "Second getter should not be invoked at all!");
+    }
+
+    @Test
+    @RegressionTest("https://issues.jboss.org/browse/RF-12491")
+    public void testGetterForOtherDataTableIsNotInvoked_switchTypeServer() {
+        attributes.set(TabPanelAttributes.switchType, "server");
+        refreshCounts();
+        assertEquals(firstGetterInvokedPhases.getText(), "2xRENDER_RESPONSE(6)");
+        assertEquals(secondGetterInvokedPhases.getText(), "", "Second getter should not be invoked at all!");
+    }
+
+    private class SimpleDT extends RichFacesDataTable<NullFragment, NullFragment, NullFragment> {
+    }
+}
