36c9817d265962d7b83e7e8204b267741d191a66
Jiri Stefek <jstefek@redhat.com>
2015-06-11 11:12:46 +0200

    metamer: edt: put passing tests back to suite

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richExtendedDataTable/TestExtendedDataTableSortingUsingComponentControl.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richExtendedDataTable/TestExtendedDataTableSortingUsingComponentControl.java
@@ -26,9 +26,7 @@ import org.richfaces.tests.metamer.ftest.abstractions.DataTableSortingTest;
 import org.richfaces.tests.metamer.ftest.annotations.IssueTracking;
 import org.richfaces.tests.metamer.ftest.annotations.RegressionTest;
 import org.richfaces.tests.metamer.ftest.extension.attributes.coverage.annotations.CoversAttributes;
-import org.richfaces.tests.metamer.ftest.extension.configurator.skip.BecauseOf;
 import org.richfaces.tests.metamer.ftest.extension.configurator.skip.annotation.Skip;
-import org.richfaces.tests.metamer.ftest.extension.configurator.templates.annotation.Templates;
 import org.richfaces.tests.metamer.ftest.richExtendedDataTable.fragment.SortingEDT;
 import org.testng.annotations.Test;
 
@@ -46,94 +44,39 @@ public class TestExtendedDataTableSortingUsingComponentControl extends DataTable
     private SortingEDT table;
 
     @Override
-    protected SortingEDT getTable() {
-        return table;
-    }
-
-    @Override
     public String getComponentTestPagePath() {
         return "richExtendedDataTable/sorting-using-component-control.xhtml";
     }
 
+    @Override
+    protected SortingEDT getTable() {
+        return table;
+    }
+
     @Test
     @CoversAttributes("sortMode")
-    @Templates(exclude = { "richExtendedDataTable", "uiRepeat" })
-    @Override
+    @RegressionTest({ "https://issues.jboss.org/browse/RF-13881", "https://issues.jboss.org/browse/RF-13690" })
     public void testSortModeMulti() {
         super.testSortModeMulti();
     }
 
     @Test
     @CoversAttributes("sortMode")
-    @Templates(exclude = { "richExtendedDataTable", "uiRepeat" })
-    @Override
+    @RegressionTest({ "https://issues.jboss.org/browse/RF-13881", "https://issues.jboss.org/browse/RF-13690" })
     public void testSortModeMultiFullPageRefresh() {
         super.testSortModeMultiFullPageRefresh();
     }
 
     @Test
     @CoversAttributes("sortMode")
-    @RegressionTest("https://issues.jboss.org/browse/RF-13881")
-    @Templates(value = "richExtendedDataTable")
-    public void testSortModeMultiFullPageRefreshInEDT() {
-        super.testSortModeMultiFullPageRefresh();
-    }
-
-    @Test // fails with JSF 2.2
-    @Skip(BecauseOf.UIRepeatSetIndexIssue.class)
-    @CoversAttributes("sortMode")
-    @IssueTracking("https://issues.jboss.org/browse/RF-13690")
-    @Templates(value = "uiRepeat")
-    public void testSortModeMultiFullPageRefreshInUiRepeat() {
-        super.testSortModeMultiFullPageRefresh();
-    }
-
-    @Test
-    @CoversAttributes("sortMode")
-    @RegressionTest("https://issues.jboss.org/browse/RF-13881")
-    @Templates(value = "richExtendedDataTable")
-    public void testSortModeMultiInEDT() {
-        super.testSortModeMulti();
-    }
-
-    @Test // fails with JSF 2.2
-    @Skip(BecauseOf.UIRepeatSetIndexIssue.class)
-    @CoversAttributes("sortMode")
-    @IssueTracking("https://issues.jboss.org/browse/RF-13690")
-    @Templates(value = "uiRepeat")
-    public void testSortModeMultiInUiRepeat() {
-        super.testSortModeMulti();
-    }
-
-    @Test
-    @CoversAttributes("sortMode")
-    @Templates(exclude = { "richExtendedDataTable", "uiRepeat" })
-    @Override
+    @RegressionTest({ "https://issues.jboss.org/browse/RF-13881", "https://issues.jboss.org/browse/RF-13690" })
     public void testSortModeMultiReplacingOldOccurences() {
-        super.testSortModeMultiReplacingOldOccurences();
-    }
-
-    @Test
-    @CoversAttributes("sortMode")
-    @RegressionTest("https://issues.jboss.org/browse/RF-13881")
-    @Templates(value = "richExtendedDataTable")
-    public void testSortModeMultiReplacingOldOccurencesInEDT() {
-        super.testSortModeMultiReplacingOldOccurences();
-    }
-
-    @Test // fails with JSF 2.2
-    @Skip(BecauseOf.UIRepeatSetIndexIssue.class)
-    @CoversAttributes("sortMode")
-    @Templates(value = "uiRepeat")
-    @IssueTracking("https://issues.jboss.org/browse/RF-13690")
-    public void testSortModeMultiReplacingOldOccurencesInUiRepeat() {
         super.testSortModeMultiReplacingOldOccurences();
     }
 
     @Test
     @Skip
     @CoversAttributes("sortMode")
-    @Override
     @IssueTracking({ "https://issues.jboss.org/browse/RF-9932",
         "http://java.net/jira/browse/JAVASERVERFACES_SPEC_PUBLIC-790" })
     public void testSortModeMultiRerenderAll() {
@@ -142,83 +85,28 @@ public class TestExtendedDataTableSortingUsingComponentControl extends DataTable
 
     @Test
     @CoversAttributes("sortMode")
-    @Templates(exclude = { "richExtendedDataTable", "uiRepeat" })
-    @Override
+    @RegressionTest({ "https://issues.jboss.org/browse/RF-13881", "https://issues.jboss.org/browse/RF-13690" })
     public void testSortModeMultiReverse() {
         super.testSortModeMultiReverse();
     }
 
     @Test
     @CoversAttributes("sortMode")
-    @RegressionTest("https://issues.jboss.org/browse/RF-13881")
-    @Templates(value = "richExtendedDataTable")
-    public void testSortModeMultiReverseInEDT() {
-        super.testSortModeMultiReverse();
-    }
-
-    @Test // fails with JSF 2.2
-    @Skip(BecauseOf.UIRepeatSetIndexIssue.class)
-    @CoversAttributes("sortMode")
-    @Templates(value = "uiRepeat")
-    @IssueTracking("https://issues.jboss.org/browse/RF-13690")
-    public void testSortModeMultiReverseInUiRepeat() {
-        super.testSortModeMultiReverse();
-    }
-
-    @Test
-    @CoversAttributes("sortMode")
-    @Templates(exclude = { "richExtendedDataTable", "uiRepeat" })
-    @Override
+    @RegressionTest({ "https://issues.jboss.org/browse/RF-13881", "https://issues.jboss.org/browse/RF-13690" })
     public void testSortModeSingle() {
         super.testSortModeSingle();
     }
 
     @Test
     @CoversAttributes("sortMode")
-    @Templates(exclude = { "richExtendedDataTable", "uiRepeat" })
-    @Override
+    @RegressionTest({ "https://issues.jboss.org/browse/RF-13881", "https://issues.jboss.org/browse/RF-13690" })
     public void testSortModeSingleFullPageRefresh() {
         super.testSortModeSingleFullPageRefresh();
     }
 
     @Test
-    @CoversAttributes("sortMode")
-    @RegressionTest("https://issues.jboss.org/browse/RF-13881")
-    @Templates(value = "richExtendedDataTable")
-    public void testSortModeSingleFullPageRefreshInEDT() {
-        super.testSortModeSingleFullPageRefresh();
-    }
-
-    @Test // fails with JSF 2.2
-    @Skip(BecauseOf.UIRepeatSetIndexIssue.class)
-    @CoversAttributes("sortMode")
-    @IssueTracking("https://issues.jboss.org/browse/RF-13690")
-    @Templates(value = "uiRepeat")
-    public void testSortModeSingleFullPageRefreshInUiRepeat() {
-        super.testSortModeSingleFullPageRefresh();
-    }
-
-    @Test
-    @CoversAttributes("sortMode")
-    @RegressionTest("https://issues.jboss.org/browse/RF-13881")
-    @Templates(value = "richExtendedDataTable")
-    public void testSortModeSingleInEDT() {
-        super.testSortModeSingle();
-    }
-
-    @Test // fails with JSF 2.2
-    @Skip(BecauseOf.UIRepeatSetIndexIssue.class)
-    @CoversAttributes("sortMode")
-    @Templates(value = "uiRepeat")
-    @IssueTracking("https://issues.jboss.org/browse/RF-13690")
-    public void testSortModeSingleInUiRepeat() {
-        super.testSortModeSingle();
-    }
-
-    @Test
     @Skip
     @CoversAttributes("sortMode")
-    @Override
     @IssueTracking({ "https://issues.jboss.org/browse/RF-9932",
         "http://java.net/jira/browse/JAVASERVERFACES_SPEC_PUBLIC-790" })
     public void testSortModeSingleRerenderAll() {
@@ -227,26 +115,8 @@ public class TestExtendedDataTableSortingUsingComponentControl extends DataTable
 
     @Test
     @CoversAttributes("sortMode")
-    @Templates(exclude = { "richExtendedDataTable", "uiRepeat" })
-    @Override
+    @RegressionTest({ "https://issues.jboss.org/browse/RF-13881", "https://issues.jboss.org/browse/RF-13690" })
     public void testSortModeSingleReverse() {
-        super.testSortModeSingleReverse();
-    }
-
-    @Test
-    @CoversAttributes("sortMode")
-    @RegressionTest("https://issues.jboss.org/browse/RF-13881")
-    @Templates(value = "richExtendedDataTable")
-    public void testSortModeSingleReverseInEDT() {
-        super.testSortModeSingleReverse();
-    }
-
-    @Test
-    @Skip(BecauseOf.UIRepeatSetIndexIssue.class)
-    @CoversAttributes("sortMode")
-    @Templates(value = "uiRepeat")
-    @IssueTracking(value = "https://issues.jboss.org/browse/RF-13690")
-    public void testSortModeSingleReverseInUiRepeat() {
         super.testSortModeSingleReverse();
     }
 }
