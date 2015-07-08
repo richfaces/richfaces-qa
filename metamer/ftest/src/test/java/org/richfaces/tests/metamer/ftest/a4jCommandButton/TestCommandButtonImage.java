1c1986d8da52be211ed972bdb9632befc0f06126
Jiri Stefek <jstefek@redhat.com>
2015-07-08 13:00:25 +0200

    metamer: commandButton: added test using commandButton with @image

---
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/a4jCommandButton/TestCommandButtonImage.java
@@ -0,0 +1,62 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2015, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.richfaces.tests.metamer.ftest.a4jCommandButton;
+
+import static org.testng.Assert.assertTrue;
+
+import org.jboss.arquillian.graphene.page.Page;
+import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
+import org.richfaces.tests.metamer.ftest.extension.attributes.coverage.annotations.CoversAttributes;
+import org.richfaces.tests.metamer.ftest.extension.configurator.templates.annotation.Templates;
+import org.testng.annotations.Test;
+
+/**
+ * @author <a href="mailto:jstefek@redhat.com">Jiri Stefek</a>
+ */
+public class TestCommandButtonImage extends AbstractWebDriverTest {
+
+    @Page
+    private CommandButtonLinkPage page;
+
+    @Override
+    public String getComponentTestPagePath() {
+        return "a4jCommandButton/image.xhtml";
+    }
+
+    @Test
+    @Templates("plain")
+    @CoversAttributes("image")
+    public void testImage() {
+        assertTrue(page.getButtonElement().getAttribute("src").endsWith("/metamer/resources/images/star.png"));
+    }
+
+    @Test
+    public void testSimpleClickUnicode() {
+        page.typeToInput(CommandButtonLinkPage.STRING_UNICODE1);
+        page.submitByButton();
+        page.waitUntilOutput1Changes(CommandButtonLinkPage.STRING_UNICODE1);
+
+        page.verifyOutput1Text(CommandButtonLinkPage.STRING_UNICODE1);
+        page.verifyOutput2Text(CommandButtonLinkPage.STRING_UNICODE2);
+        page.verifyOutput3Text(CommandButtonLinkPage.STRING_UNICODE3);
+    }
+}
