826856bd6d6978633d9b83bbb76b3ef5681b9a32
Jiri Stefek <jstefek@redhat.com>
2015-10-13 09:49:42 +0200

    metamer: attributes starting with 'on' or ending with 'Class' have attached JavaScript fuction on double click, for filling its value with common used test values

--- metamer/application/src/main/webapp/resources/script/attributesOndblclick.js
+++ metamer/application/src/main/webapp/resources/script/attributesOndblclick.js
@@ -1,8 +1,20 @@
 function attributesOndblclick(elem, key, klass) {
-    var $this = $(elem);
-    var valToSet = 'alert(\'' + key + ' triggered (' + klass + ')\')';
-    if (valToSet !== $this.val()) {
-        $this.val(valToSet).change();
+    var endsWithClassRegExp = /.*Class/;
+    var startsWithOnRegExp = /on.*/;
+
+    function setValue(input, valToSet) {
+        var $input = $(input);
+        if (valToSet !== $input.val()) {
+            $input.val(valToSet).change();
+        }
+    }
+
+    if (startsWithOnRegExp.test(key)) {
+        var valueToSet = 'alert(\'' + key + ' triggered (' + klass + ')\')';
+        setValue(elem, valueToSet);
+    } else if (endsWithClassRegExp.test(key)) {
+        var valueToSet = 'metamer-ftest-class';
+        setValue(elem, valueToSet);
     }
 }
 
