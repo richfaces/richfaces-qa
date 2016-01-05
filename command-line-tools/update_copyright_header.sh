893be77702a60fbcd0897a0d4cafda93eae79411
Jiri Stefek <jstefek@redhat.com>
2016-01-05 09:18:49 +0100

    command line tools: updated end date in update_copyright_header script

--- command-line-tools/update_copyright_header.sh
+++ command-line-tools/update_copyright_header.sh
@@ -3,7 +3,7 @@
 # include all headers with a single year (4 numbers, e.g. 2010) or with a range of years (4 numbers - 4numbers,
 # e.g. 2010-2011) and replace it with a range of years $START-$END.
 START=2010
-END=2015
+END=2016
 TEMPLATE="s/Copyright [0-9]\{4\}\(-[0-9]\{4\}\)\?,/Copyright ${START}-${END},/g"
 
 find . -type f -name '*.java'  -exec sed -i "${TEMPLATE}" {} \;
