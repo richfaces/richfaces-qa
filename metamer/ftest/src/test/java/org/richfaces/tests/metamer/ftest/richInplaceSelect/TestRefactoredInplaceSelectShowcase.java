0eceb3ad95b933cbfa314d2a7cdd05451b0d9e80
Jiri Stefek <jstefek@redhat.com>
2016-01-06 15:17:47 +0100

    metamer: inplaceSelect: TestRefactoredInplaceSelectShowcase: refactored with guards and multiple attribute setter

--- metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richInplaceSelect/TestRefactoredInplaceSelectShowcase.java
+++ metamer/ftest/src/test/java/org/richfaces/tests/metamer/ftest/richInplaceSelect/TestRefactoredInplaceSelectShowcase.java
@@ -1,7 +1,29 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010-2016, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.richfaces.tests.metamer.ftest.richInplaceSelect;
 
 import static org.testng.Assert.assertEquals;
 
+import org.jboss.arquillian.graphene.Graphene;
 import org.openqa.selenium.support.FindBy;
 import org.richfaces.fragment.inplaceSelect.RichFacesInplaceSelect;
 import org.richfaces.tests.metamer.ftest.AbstractWebDriverTest;
@@ -22,21 +44,23 @@ public class TestRefactoredInplaceSelectShowcase extends AbstractWebDriverTest {
 
     @Test
     public void testInplaceSelectCancelConfirm() {
-        inplaceSelect.select(2);
+        Graphene.guardAjax(inplaceSelect).select(2);
 
         String selectedValue = inplaceSelect.getTextInput().getStringValue();
         assertEquals(selectedValue, "Arizona");
 
-        inplaceSelectAttributes.set(InplaceSelectAttributes.saveOnBlur, false);
-        inplaceSelectAttributes.set(InplaceSelectAttributes.saveOnSelect, false);
+        attsSetter()
+            .setAttribute(InplaceSelectAttributes.saveOnBlur).toValue(false)
+            .setAttribute(InplaceSelectAttributes.saveOnSelect).toValue(false)
+            .asSingleAction().perform();
 
-        inplaceSelect.select(2).confirm();
+        Graphene.guardAjax(inplaceSelect.select(4)).confirm();
         inplaceSelect.select(3).cancel();
         selectedValue = inplaceSelect.getTextInput().getStringValue();
-        assertEquals(selectedValue, "Arizona");
+        assertEquals(selectedValue, "California");
 
         inplaceSelectAttributes.set(InplaceSelectAttributes.showControls, true);
-        inplaceSelect.select(3).confirmByControlls();
+        Graphene.guardAjax(inplaceSelect.select(3)).confirmByControlls();
         selectedValue = inplaceSelect.getTextInput().getStringValue();
         assertEquals(selectedValue, "Arkansas");
     }
