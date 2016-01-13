df0c2a3b178584b53207c8bf602dcfa630ed9e72
Jiri Stefek <jstefek@redhat.com>
2016-01-13 15:31:29 +0100

    WebDriverUtils: ElementIsFocused condition synced with the one used in RF framework

--- common/webdriver-utils/src/main/java/org/jboss/test/selenium/support/ui/ElementIsFocused.java
+++ common/webdriver-utils/src/main/java/org/jboss/test/selenium/support/ui/ElementIsFocused.java
@@ -27,12 +27,10 @@ import org.openqa.selenium.WebElement;
 
 import com.google.common.base.Predicate;
 
-/**
- * @author <a href="mailto:jhuska@redhat.com">Juraj Huska</a>
- */
 public class ElementIsFocused implements Predicate<WebDriver> {
 
-    private WebElement element;
+    private WebElement activeElement;
+    private final WebElement element;
 
     /**
      * Provide element to wait to gain focus or null if you want to fail for no element having focus
@@ -44,12 +42,19 @@ public class ElementIsFocused implements Predicate<WebDriver> {
     @Override
     public boolean apply(WebDriver browser) {
         try {
+            activeElement = FocusRetriever.retrieveActiveElement(browser);
             if (element == null) {
-                return FocusRetriever.retrieveActiveElement(browser) == null;
+                return activeElement == null;
             }
-            return element.equals(FocusRetriever.retrieveActiveElement(browser));
+            return activeElement.equals(element);
         } catch (StaleElementReferenceException e) {
             return false;
         }
+    }
+
+    @Override
+    public String toString() {
+        String focused = (activeElement == null) ? null : ((activeElement.getAttribute("id") != null) ? activeElement.getAttribute("id") : activeElement.toString());
+        return String.format("waiting for the focus on '%s' failed - last focused element: %s", element, focused);
     }
 }
