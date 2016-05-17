a5a6f9229136364a1629bfb78ab3943a99456499
Jiri Stefek <jstefek@redhat.com>
2016-05-17 09:57:52 +0200

    archetypes: scripts: paths to pom fixed, setup maven home

--- archetypes/apps/config.sh
+++ archetypes/apps/config.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 if [ "$MAVEN"  == "" ]; then
     if [ -f "/qa/tools/opt/apache-maven-3.2.5/bin/mvn" ]; then
-	    MAVEN="/qa/tools/opt/apache-maven-3.2.5/bin/mvn"
+        MAVEN="/qa/tools/opt/apache-maven-3.2.5/bin/mvn"
     elif [ -f "t:/opt/apache-maven-3.2.5/bin/mvn.bat" ]; then
-	    MAVEN="t:/opt/apache-maven-3.2.5/bin/mvn.bat"
-    else
+        MAVEN="t:/opt/apache-maven-3.2.5/bin/mvn"
+        M2_HOME="t:/opt/apache-maven-3.2.5"
+     else
         MAVEN="mvn"
     fi
 fi
