23f130fe656c5ea3c8dd855fc6a59897717e056b
Jan Kasik <honza.kasik1@seznam.cz>
2016-08-16 15:39:07 +0200

    Removed hardcoded URLs targeting mw jenkins (#18)
    * Removed hardcoded URLs targeting mw jenkins
    * Removed unnecessary options

--- command-line-tools/getLatestGitRepository.sh
+++ command-line-tools/getLatestGitRepository.sh
@@ -2,6 +2,15 @@
 # use sh getLatestGitRepository.sh ; to download repository from latest snapshots
 # use sh getLatestGitRepository.sh githubBranch; to download repository from latest snapshots and to switch to a specific branch/tag
 
+while getopts ":s" opt; do
+    case $opt in
+        s)
+            URL_SNAPSHOTS="$OPTARG"
+            ;;
+    esac
+done
+
+
 SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd );
 
 # add additional functions
@@ -10,36 +19,35 @@ SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd );
 . ${SCRIPT_DIR}/download.sh;
 
 getLatestQAGitRepository(){
-  URL_SNAPSHOTS=http://jenkins.mw.lab.eng.bos.redhat.com/hudson/view/RichFaces/view/4.5/job/richfaces-4.5-metamer-repositories-packer/lastSuccessfulBuild/artifact/git-repository.zip;
+    download "$URL_SNAPSHOTS"
 
-  download ${URL_SNAPSHOTS}
+    if [ $? -gt 0 ];then
+        echo "downloading was unsuccessful. Exiting."
+        exit 1;
+    fi
 
-  if [ $? -gt 0 ];then
-    echo "downloading was unsuccessful. Exiting."
-    exit 1;
-  fi
+    extract git-repository.zip
 
-  extract git-repository.zip
-
-  cd qa
-  checkoutToBranchAndUpdate $1
+    cd qa
+    checkoutToBranchAndUpdate $1
 }
 
 checkoutToBranchAndUpdate(){
-  if [ ! -z "$1" ];then
-    BRANCH=$1;
-  else
-    BRANCH=master
-  fi
+    if [ ! -z "$1" ]; then
+        BRANCH="$1";
+    else
+        BRANCH=master
+    fi
 
-#  echo "BRANCH=${BRANCH}"
+    #  echo "BRANCH=${BRANCH}"
 
-  git stash save
-  git checkout master
-  git pull
-  if [[ ! "$BRANCH" == "master"  ]];then
-    git checkout $BRANCH
-  fi
+    git stash save
+    git checkout master
+    git pull
+
+    if [[ ! "$BRANCH" == "master"  ]];then
+        git checkout "$BRANCH"
+    fi
 }
 
 getLatestQAGitRepository $1
--- command-line-tools/getLatestMavenRepository.sh
+++ command-line-tools/getLatestMavenRepository.sh
@@ -2,6 +2,21 @@
 # use sh getLatestMavenRepository.sh ; to download repository from latest snapshots
 # use sh getLatestMavenRepository.sh release ; to download repository from latest release
 
+while getopts ":s:r:w" opt
+    do
+        case $opt in
+            s)
+                URL_SNAPSHOTS="$OPTARG"
+                ;;
+            r)
+                URL_RELEASE="$OPTARG"
+                ;;
+            w)
+                URL_WFK="$OPTARG"
+                ;;
+        esac
+    done
+
 SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd );
 
 # add additional functions
@@ -10,33 +25,30 @@ SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd );
 . ${SCRIPT_DIR}/download.sh;
 
 getLatestMavenRepository(){
-  URL_SNAPSHOTS=http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-build-richfaces-4.5-tests/lastSuccessfulBuild/artifact/maven-repository.zip;
-  URL_RELEASE=http://jenkins.mw.lab.eng.bos.redhat.com/hudson/view/RichFaces/view/Release/job/richfaces-4.5-release-metamer-repositories-packer/lastSuccessfulBuild/artifact/maven-repository.zip;
-  URL_WFK=http://jenkins.mw.lab.eng.bos.redhat.com/hudson/view/RichFaces/view/RF-Prod/job/richfaces-wfk-2.7-tests/lastSuccessfulBuild/artifact/maven-repository.zip;
 
-  if [ "$1" == "release" ] ;then
-     URL=${URL_RELEASE};
-  elif [ "$1" == "wfk" ] ;then
-     URL=${URL_WFK};
-  else
-     URL=${URL_SNAPSHOTS};
-  fi
-
-  download ${URL}
-
-  if [ $? -gt 0 ];then
-    echo "Downloading was unsuccessful. Will delete the downloaded file and retry the download from scratch."
-    rm -rf maven-repository.zip
-
-    download ${URL}
-
-    if [ $? -gt 0 ];then
-      echo "Downloading was unsuccessful. Exiting."
-      exit 1;
+    if [ -n "$URL_RELEASE" ]; then
+        URL="$URL_RELEASE"
+    elif [ -n "$URL_WFK" ]; then
+        URL="$URL_WFK"
+    else
+        URL="$URL_SNAPSHOTS";
     fi
-  fi
 
-  extract maven-repository.zip
+    download "$URL"
+
+    if [ $? -gt 0 ]; then
+        echo "Downloading was unsuccessful. Will delete the downloaded file and retry the download from scratch."
+        rm -rf maven-repository.zip
+
+        download "$URL"
+
+        if [ $? -gt 0 ]; then
+            echo "Downloading was unsuccessful. Exiting."
+            exit 1;
+        fi
+    fi
+
+    extract maven-repository.zip
 }
 
-getLatestMavenRepository $1
+getLatestMavenRepository
